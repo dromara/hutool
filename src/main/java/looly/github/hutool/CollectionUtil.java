530fe635b8a5deffd4d73514b9ad75e413c0750e
looly <loolly@gmail.com>
2014-05-05 11:50:01 +0800

    Add zip() method

--- src/main/java/looly/github/hutool/CollectionUtil.java
+++ src/main/java/looly/github/hutool/CollectionUtil.java
@@ -455,6 +455,22 @@ public class CollectionUtil {
 	/**
 	 * 映射键值（参考Python的zip()函数）<br>
 	 * 例如：<br>
+	 * 		keys =    a,b,c,d<br>
+	 *		values = 1,2,3,4<br>
+	 *		delimiter = ,
+	 * 则得到的Map是 {a=1, b=2, c=3, d=4}<br>
+	 * 如果两个数组长度不同，则只对应最短部分
+	 * @param keys 键列表
+	 * @param values 值列表
+	 * @return Map
+	 */
+	public static Map<String, String> zip(String keys, String values, String delimiter) {
+		return zip(StrUtil.split(keys, delimiter), StrUtil.split(values, delimiter));
+	}
+	
+	/**
+	 * 映射键值（参考Python的zip()函数）<br>
+	 * 例如：<br>
 	 * 		keys =    [a,b,c,d]<br>
 	 *		values = [1,2,3,4]<br>
 	 * 则得到的Map是 {a=1, b=2, c=3, d=4}<br>
