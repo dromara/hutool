4cf64ecce800f671f1affe8fc05b44e1174fefab
looly <loolly@gmail.com>
2014-09-12 12:51:55 +0800

    Add method toDBC and toSBC with notConvertSet

--- src/main/java/com/xiaoleilu/hutool/Conver.java
+++ src/main/java/com/xiaoleilu/hutool/Conver.java
@@ -1,6 +1,7 @@
 package com.xiaoleilu.hutool;
 
 import java.math.BigDecimal;
+import java.util.Set;
 
 import com.xiaoleilu.hutool.exceptions.UtilException;
 
@@ -269,8 +270,24 @@ public class Conver {
 	 * @return 全角字符串.
 	 */
 	public static String toSBC(String input) {
+		return toSBC(input, null);
+	}
+	
+	/**
+	 * 半角转全角
+	 * 
+	 * @param input String
+	 * @param notConvertSet 不替换的字符集合
+	 * @return 全角字符串.
+	 */
+	public static String toSBC(String input, Set<Character> notConvertSet) {
 		char c[] = input.toCharArray();
 		for (int i = 0; i < c.length; i++) {
+			if(null != notConvertSet && notConvertSet.contains(c[i])) {
+				//跳过不替换的字符
+				continue;
+			}
+			
 			if (c[i] == ' ') {
 				c[i] = '\u3000';
 			} else if (c[i] < '\177') {
@@ -288,18 +305,31 @@ public class Conver {
 	 * @return 半角字符串
 	 */
 	public static String toDBC(String input) {
-
-		char c[] = input.toCharArray();
+		return toDBC(input, null);
+	}
+	
+	/**
+	 * 替换全角为半角
+	 * @param text 文本
+	 * @param notConvertSet 不替换的字符集合
+	 * @return 替换后的字符
+	 */
+	public static String toDBC(String text, Set<Character> notConvertSet) {
+		char c[] = text.toCharArray();
 		for (int i = 0; i < c.length; i++) {
+			if(null != notConvertSet && notConvertSet.contains(c[i])) {
+				//跳过不替换的字符
+				continue;
+			}
+			
 			if (c[i] == '\u3000') {
 				c[i] = ' ';
 			} else if (c[i] > '\uFF00' && c[i] < '\uFF5F') {
 				c[i] = (char) (c[i] - 65248);
-
 			}
 		}
 		String returnString = new String(c);
-
+		
 		return returnString;
 	}
 }
