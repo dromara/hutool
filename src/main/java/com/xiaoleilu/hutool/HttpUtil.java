52e83ee41f2ad472b19e7ef34f75eb9f804b5cc4
looly <loolly@gmail.com>
2015-01-25 16:43:19 +0800

    Add decodeParams and update to 2.1.1

--- src/main/java/com/xiaoleilu/hutool/HttpUtil.java
+++ src/main/java/com/xiaoleilu/hutool/HttpUtil.java
@@ -9,8 +9,13 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLDecoder;
 import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.regex.Pattern;
 
 import javax.servlet.http.HttpServletRequest;
@@ -26,9 +31,9 @@ public class HttpUtil {
 
 	/** 未知的标识 */
 	public final static String UNKNOW = "unknown";
-	
-	public final static Pattern CHARSET_PATTERN =  Pattern.compile("charset=(.*?)\"");
-	
+
+	public final static Pattern CHARSET_PATTERN = Pattern.compile("charset=(.*?)\"");
+
 	private static Map<String, String> cookies = new HashMap<String, String>();
 
 	/**
@@ -106,30 +111,30 @@ public class HttpUtil {
 		final URL url = new URL(urlString);
 		final String host = url.getHost();
 		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-		
+
 		conn.addRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/21.0.1180.83 Safari/537.1");
 		final String cookie = cookies.get(host);
-		if(cookie != null) conn.addRequestProperty("Cookie", cookie);
+		if (cookie != null) conn.addRequestProperty("Cookie", cookie);
 
 		conn.setRequestMethod("GET");
 		conn.setDoInput(true);
-		
-		if(conn.getResponseCode() != 200) {
-			if(! isPassCodeError) {
+
+		if (conn.getResponseCode() != 200) {
+			if (!isPassCodeError) {
 				throw new IOException("Status code not 200!");
 			}
 		}
-		
+
 		final String setCookie = conn.getHeaderField("Set-Cookie");
-		if(StrUtil.isBlank(setCookie) == false) {
+		if (StrUtil.isBlank(setCookie) == false) {
 			Log.debug("Set cookie: [{}]", setCookie);
 			cookies.put(host, setCookie);
 		}
-		
+
 		/* 获取内容 */
 		String charset = getCharsetFromConn(conn);
 		boolean isGetCharsetFromContent = false;
-		if(StrUtil.isBlank(charset)) {
+		if (StrUtil.isBlank(charset)) {
 			charset = customCharset;
 			isGetCharsetFromContent = true;
 		}
@@ -138,9 +143,10 @@ public class HttpUtil {
 
 		return content;
 	}
-	
+
 	/**
 	 * 发送post请求
+	 * 
 	 * @param urlString 网址
 	 * @param paramMap post表单数据
 	 * @param customCharset 自定义请求字符集，发送时使用此字符集，获取返回内容如果字符集获取不到，使用此字符集
@@ -151,9 +157,10 @@ public class HttpUtil {
 	public static String post(String urlString, Map<String, Object> paramMap, String customCharset, boolean isPassCodeError) throws IOException {
 		return post(urlString, toParams(paramMap), customCharset, isPassCodeError);
 	}
-	
+
 	/**
 	 * 发送post请求
+	 * 
 	 * @param urlString 网址
 	 * @param params post表单数据
 	 * @param customCharset 自定义请求字符集，发送时使用此字符集，获取返回内容如果字符集获取不到，使用此字符集
@@ -164,20 +171,20 @@ public class HttpUtil {
 	public static String post(String urlString, String params, String customCharset, boolean isPassCodeError) throws IOException {
 		URL url = new URL(urlString);
 		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-		
+
 		conn.setRequestMethod("POST");
 		conn.setDoOutput(true);
 		conn.setDoInput(true);
 		conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
-		
+
 		IoUtil.write(conn.getOutputStream(), customCharset, true, params);
-		
-		if(conn.getResponseCode() != 200) {
-			if(! isPassCodeError) {
+
+		if (conn.getResponseCode() != 200) {
+			if (!isPassCodeError) {
 				throw new IOException("Status code not 200!");
 			}
 		}
-		
+
 		/* 获取内容 */
 		String charset = getCharsetFromConn(conn);
 		String content = IoUtil.getString(conn.getInputStream(), StrUtil.isBlank(charset) ? customCharset : charset);
@@ -185,9 +192,10 @@ public class HttpUtil {
 
 		return content;
 	}
-	
+
 	/**
 	 * 获得远程String
+	 * 
 	 * @param url 请求的url
 	 * @param customCharset 自定义的字符集
 	 * @return 文本
@@ -197,17 +205,67 @@ public class HttpUtil {
 		InputStream inputStream = new URL(url).openStream();
 		return IoUtil.getString(inputStream, customCharset);
 	}
-	
+
 	/**
 	 * 将Map形式的Form表单数据转换为Url参数形式
+	 * 
 	 * @param paramMap 表单数据
 	 * @return url参数
 	 */
 	public static String toParams(Map<String, Object> paramMap) {
 		return CollectionUtil.join(paramMap.entrySet(), "&");
 	}
-	
-	//----------------------------------------------------------------------------------------- Private method start
+
+	public static Map<String, List<String>> decodeParams(String paramsStr, String charset) {
+		if (StrUtil.isBlank(paramsStr)) {
+			return Collections.emptyMap();
+		}
+
+		// 去掉Path部分
+		int pathEndPos = paramsStr.indexOf('?');
+		if (pathEndPos > 0) {
+			paramsStr = StrUtil.subSuf(paramsStr, pathEndPos + 1);
+		}
+		paramsStr = decode(paramsStr, charset);
+
+		final Map<String, List<String>> params = new LinkedHashMap<String, List<String>>();
+		String name = null;
+		int pos = 0; // 未处理字符开始位置
+		int i; // 未处理字符结束位置
+		char c; // 当前字符
+		for (i = 0; i < paramsStr.length(); i++) {
+			c = paramsStr.charAt(i);
+			if (c == '=' && name == null) { // 键值对的分界点
+				if (pos != i) {
+					name = paramsStr.substring(pos, i);
+				}
+				pos = i + 1;
+			} else if (c == '&' || c == ';') { // 参数对的分界点
+				if (name == null && pos != i) {
+					// 对于像&a&这类无参数值的字符串，我们将name为a的值设为""
+					addParam(params, paramsStr.substring(pos, i), StrUtil.EMPTY);
+				} else if (name != null) {
+					addParam(params, name, paramsStr.substring(pos, i));
+					name = null;
+				}
+				pos = i + 1;
+			}
+		}
+
+		if (pos != i) {
+			if (name == null) {
+				addParam(params, paramsStr.substring(pos, i), StrUtil.EMPTY);
+			} else {
+				addParam(params, name, paramsStr.substring(pos, i));
+			}
+		} else if (name != null) {
+			addParam(params, name, StrUtil.EMPTY);
+		}
+
+		return params;
+	}
+
+	// ----------------------------------------------------------------------------------------- Private method start
 	/**
 	 * 从Http连接的头信息中获得字符集
 	 * 
@@ -232,7 +290,7 @@ public class HttpUtil {
 	private static boolean isUnknow(String checkString) {
 		return StrUtil.isBlank(checkString) || UNKNOW.equalsIgnoreCase(checkString);
 	}
-	
+
 	/**
 	 * 从流中读取内容
 	 * 
@@ -249,9 +307,9 @@ public class HttpUtil {
 		String line = null;
 		while ((line = reader.readLine()) != null) {
 			content.append(line).append('\n');
-			if(isGetCharsetFromContent) {
+			if (isGetCharsetFromContent) {
 				String charsetInContent = ReUtil.get(CHARSET_PATTERN, line, 1);
-				if(StrUtil.isBlank(charsetInContent) == false) {
+				if (StrUtil.isBlank(charsetInContent) == false) {
 					charset = charsetInContent;
 					reader = new BufferedReader(new InputStreamReader(in, charset));
 					isGetCharsetFromContent = true;
@@ -261,5 +319,24 @@ public class HttpUtil {
 
 		return content.toString();
 	}
-	//----------------------------------------------------------------------------------------- Private method start end
+
+	/**
+	 * 将键值对加入到值为List类型的Map中
+	 * 
+	 * @param params 参数
+	 * @param name key
+	 * @param value value
+	 * @return 是否成功
+	 */
+	private static boolean addParam(Map<String, List<String>> params, String name, String value) {
+		List<String> values = params.get(name);
+		if (values == null) {
+			values = new ArrayList<String>(1); // 一般是一个参数
+			params.put(name, values);
+		}
+		values.add(value);
+		return true;
+	}
+
+	// ----------------------------------------------------------------------------------------- Private method start end
 }
