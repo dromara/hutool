64759fdc9f0a056c9340759c3037f790f6af99fc
looly <loolly@gmail.com>
2015-01-25 16:44:11 +0800

    Add decodeParams and update to 2.1.1

--- src/main/java/com/xiaoleilu/hutool/HttpUtil.java
+++ src/main/java/com/xiaoleilu/hutool/HttpUtil.java
@@ -15,7 +15,6 @@ import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
 import java.util.regex.Pattern;
 
 import javax.servlet.http.HttpServletRequest;
