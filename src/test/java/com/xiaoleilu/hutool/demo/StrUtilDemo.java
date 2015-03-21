bf9eaecdfc40ce755de660c2b54f9f51370d580a
looly <loolly@gmail.com>
2015-03-21 11:55:22 +0800

    Add License

---
+++ src/test/java/com/xiaoleilu/hutool/demo/StrUtilDemo.java
@@ -0,0 +1,10 @@
+package com.xiaoleilu.hutool.demo;
+
+import com.xiaoleilu.hutool.StrUtil;
+
+public class StrUtilDemo {
+	public static void main(String[] args) {
+		String underlineCase = StrUtil.toUnderlineCase("HelloWorldCCDn");
+		System.out.println(underlineCase);
+	}
+}
