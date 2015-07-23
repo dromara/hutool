26dd66977c03b314d840001c5b2ffaaf20c595f5
Looly <loolly@gmail.com>
2015-07-23 10:40:51 +0800

    Add HandleHelper.handleRow

--- src/main/java/com/xiaoleilu/hutool/db/handler/HandleHelper.java
+++ src/main/java/com/xiaoleilu/hutool/db/handler/HandleHelper.java
@@ -17,10 +17,28 @@ public class HandleHelper {
 	 * @param columnCount 列数
 	 * @param meta ResultSetMetaData
 	 * @param rs 数据集
-	 * @return 每一行的Map
+	 * @return 每一行的Entity
 	 * @throws SQLException
 	 */
 	public static Entity handleRow(int columnCount, ResultSetMetaData meta, ResultSet rs) throws SQLException {
+		final Entity row = Entity.create(meta.getTableName(1));
+		String columnLabel;
+		for (int i = 1; i <= columnCount; i++) {
+			columnLabel = meta.getColumnLabel(i);
+			row.put(columnLabel, rs.getObject(columnLabel));
+		}
+		return row;
+	}
+	
+	/**
+	 * 处理单条数据
+	 * @param rs 数据集
+	 * @return 每一行的Entity
+	 * @throws SQLException
+	 */
+	public static Entity handleRow(ResultSet rs) throws SQLException {
+		final ResultSetMetaData meta = rs.getMetaData();
+		final int columnCount = meta.getColumnCount();
 		final Entity row = Entity.create(meta.getTableName(1));
 		String columnLabel;
 		for (int i = 1; i <= columnCount; i++) {
