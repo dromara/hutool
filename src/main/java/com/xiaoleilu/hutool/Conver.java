b070e86f6bb280942a92b6403b03ebeadd28e32a
Looly <loolly@gmail.com>
2015-12-31 16:50:03 +0800

    Add degistUpercase method

--- src/main/java/com/xiaoleilu/hutool/Conver.java
+++ src/main/java/com/xiaoleilu/hutool/Conver.java
@@ -570,4 +570,38 @@ public class Conver {
 			return str;
 		}
 	}
+	
+	/**
+	 * 数字金额大写转换
+	 * 先写个完整的然后将如零拾替换成零
+	 * @param n 数字
+	 * @return 中文大写数字
+	 */
+	public static String digitUppercase(double n) {
+		String fraction[] = { "角", "分" };
+		String digit[] = { "零", "壹", "贰", "叁", "肆", "伍", "陆", "柒", "捌", "玖" };
+		String unit[][] = { { "元", "万", "亿" }, { "", "拾", "佰", "仟" } };
+
+		String head = n < 0 ? "负" : "";
+		n = Math.abs(n);
+
+		String s = "";
+		for (int i = 0; i < fraction.length; i++) {
+			s += (digit[(int) (Math.floor(n * 10 * Math.pow(10, i)) % 10)] + fraction[i]).replaceAll("(零.)+", "");
+		}
+		if (s.length() < 1) {
+			s = "整";
+		}
+		int integerPart = (int) Math.floor(n);
+
+		for (int i = 0; i < unit[0].length && integerPart > 0; i++) {
+			String p = "";
+			for (int j = 0; j < unit[1].length && n > 0; j++) {
+				p = digit[integerPart % 10] + unit[1][j] + p;
+				integerPart = integerPart / 10;
+			}
+			s = p.replaceAll("(零.)*零$", "").replaceAll("^$", "零") + unit[0][i] + s;
+		}
+		return head + s.replaceAll("(零.)*零元", "元").replaceFirst("(零.)+", "").replaceAll("(零.)+", "零").replaceAll("^整$", "零元整");
+	}
 }
