1e79a1523535954ca63f2dc54ff0bacbee7140b7
Looly <loolly@gmail.com>
2016-01-15 20:11:35 +0800

    Update to 2.14.0，Change package and add Filter

--- src/main/java/com/xiaoleilu/hutool/util/CollectionUtil.java
+++ src/main/java/com/xiaoleilu/hutool/util/CollectionUtil.java
@@ -9,17 +9,17 @@ import java.util.Comparator;
 import java.util.Deque;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
-
-import com.xiaoleilu.hutool.lang.BoundedPriorityQueue;
-import com.xiaoleilu.hutool.lang.Filter;
-
 import java.util.Set;
 import java.util.Stack;
 import java.util.TreeSet;
+
+import com.xiaoleilu.hutool.lang.BoundedPriorityQueue;
+import com.xiaoleilu.hutool.lang.Filter;
 
 /**
  * 集合相关工具类，包括数组
@@ -28,11 +28,11 @@ import java.util.TreeSet;
  * 
  */
 public class CollectionUtil {
-	
+
 	private CollectionUtil() {
 		// 静态类不可实例化
 	}
-	
+
 	/**
 	 * 以 conjunction 为分隔符将集合转换为字符串
 	 * 
@@ -54,7 +54,7 @@ public class CollectionUtil {
 		}
 		return sb.toString();
 	}
-	
+
 	/**
 	 * 以 conjunction 为分隔符将数组转换为字符串
 	 * 
@@ -76,9 +76,10 @@ public class CollectionUtil {
 		}
 		return sb.toString();
 	}
-	
+
 	/**
 	 * 将多个集合排序并显示不同的段落（分页）
+	 * 
 	 * @param pageNo 页码
 	 * @param numPerPage 每页的条目数
 	 * @param comparator 比较器
@@ -91,20 +92,21 @@ public class CollectionUtil {
 		for (Collection<T> coll : colls) {
 			result.addAll(coll);
 		}
-		
+
 		Collections.sort(result, comparator);
-		
-		//第一页且数目少于第一页显示的数目
-		if(pageNo <=1 && result.size() <= numPerPage) {
+
+		// 第一页且数目少于第一页显示的数目
+		if (pageNo <= 1 && result.size() <= numPerPage) {
 			return result;
 		}
-		
+
 		final int[] startEnd = PageUtil.transToStartEnd(pageNo, numPerPage);
 		return result.subList(startEnd[0], startEnd[1]);
 	}
-	
+
 	/**
 	 * 将多个集合排序并显示不同的段落（分页）
+	 * 
 	 * @param pageNo 页码
 	 * @param numPerPage 每页的条目数
 	 * @param comparator 比较器
@@ -117,12 +119,12 @@ public class CollectionUtil {
 		for (Collection<T> coll : colls) {
 			queue.addAll(coll);
 		}
-		
-		//第一页且数目少于第一页显示的数目
-		if(pageNo <=1 && queue.size() <= numPerPage) {
+
+		// 第一页且数目少于第一页显示的数目
+		if (pageNo <= 1 && queue.size() <= numPerPage) {
 			return queue.toList();
 		}
-		
+
 		final int[] startEnd = PageUtil.transToStartEnd(pageNo, numPerPage);
 		return queue.toList().subList(startEnd[0], startEnd[1]);
 	}
@@ -139,10 +141,10 @@ public class CollectionUtil {
 
 			@Override
 			public int compare(Entry<Long, Long> o1, Entry<Long, Long> o2) {
-				if (o1.getValue() > o2.getValue()){
+				if (o1.getValue() > o2.getValue()) {
 					return 1;
 				}
-				if (o1.getValue() < o2.getValue()){
+				if (o1.getValue() < o2.getValue()) {
 					return -1;
 				}
 				return 0;
@@ -160,7 +162,7 @@ public class CollectionUtil {
 	 * @return 切取出的数据或null
 	 */
 	public static <T> List<T> popPart(Stack<T> surplusAlaDatas, int partSize) {
-		if (surplusAlaDatas == null || surplusAlaDatas.size() <= 0){
+		if (surplusAlaDatas == null || surplusAlaDatas.size() <= 0) {
 			return null;
 		}
 
@@ -178,7 +180,7 @@ public class CollectionUtil {
 		}
 		return currentAlaDatas;
 	}
-	
+
 	/**
 	 * 切取部分数据
 	 * 
@@ -188,7 +190,7 @@ public class CollectionUtil {
 	 * @return 切取出的数据或null
 	 */
 	public static <T> List<T> popPart(Deque<T> surplusAlaDatas, int partSize) {
-		if (surplusAlaDatas == null || surplusAlaDatas.size() <= 0){
+		if (surplusAlaDatas == null || surplusAlaDatas.size() <= 0) {
 			return null;
 		}
 
@@ -215,14 +217,15 @@ public class CollectionUtil {
 	public static <T, K> HashMap<T, K> newHashMap() {
 		return new HashMap<T, K>();
 	}
-	
+
 	/**
 	 * 新建一个HashMap
+	 * 
 	 * @param size 初始大小，由于默认负载因子0.75，传入的size会实际初始大小为size / 0.75
 	 * @return HashMap对象
 	 */
 	public static <T, K> HashMap<T, K> newHashMap(int size) {
-		return new HashMap<T, K>((int)(size / 0.75));
+		return new HashMap<T, K>((int) (size / 0.75));
 	}
 
 	/**
@@ -233,7 +236,7 @@ public class CollectionUtil {
 	public static <T> HashSet<T> newHashSet() {
 		return new HashSet<T>();
 	}
-	
+
 	/**
 	 * 新建一个HashSet
 	 * 
@@ -256,7 +259,7 @@ public class CollectionUtil {
 	public static <T> ArrayList<T> newArrayList() {
 		return new ArrayList<T>();
 	}
-	
+
 	/**
 	 * 新建一个ArrayList
 	 * 
@@ -266,7 +269,7 @@ public class CollectionUtil {
 	public static <T> ArrayList<T> newArrayList(T... values) {
 		return new ArrayList<T>(Arrays.asList(values));
 	}
-	
+
 	/**
 	 * 将新元素添加到已有数组中<br/>
 	 * 添加新元素会生成一个新的数组，不影响原数组
@@ -294,9 +297,10 @@ public class CollectionUtil {
 		System.arraycopy(buffer, 0, newArray, 0, buffer.length >= newSize ? newSize : buffer.length);
 		return newArray;
 	}
-	
+
 	/**
 	 * 新建一个空数组
+	 * 
 	 * @param componentType 元素类型
 	 * @param newSize 大小
 	 * @return 空数组
@@ -330,10 +334,10 @@ public class CollectionUtil {
 		if (arrays.length == 1) {
 			return arrays[0];
 		}
-		
+
 		int length = 0;
 		for (T[] array : arrays) {
-			if(array == null) {
+			if (array == null) {
 				continue;
 			}
 			length += array.length;
@@ -342,7 +346,7 @@ public class CollectionUtil {
 
 		length = 0;
 		for (T[] array : arrays) {
-			if(array == null) {
+			if (array == null) {
 				continue;
 			}
 			System.arraycopy(array, 0, result, length, array.length);
@@ -353,6 +357,7 @@ public class CollectionUtil {
 
 	/**
 	 * 克隆数组
+	 * 
 	 * @param array 被克隆的数组
 	 * @return 新数组
 	 */
@@ -362,20 +367,22 @@ public class CollectionUtil {
 		}
 		return array.clone();
 	}
-	
+
 	/**
 	 * 生成一个数字列表<br>
 	 * 自动判定正序反序
+	 * 
 	 * @param excludedEnd 结束的数字（不包含）
 	 * @return 数字列表
 	 */
 	public static int[] range(int excludedEnd) {
 		return range(0, excludedEnd, 1);
 	}
-	
+
 	/**
 	 * 生成一个数字列表<br>
 	 * 自动判定正序反序
+	 * 
 	 * @param includedStart 开始的数字（包含）
 	 * @param excludedEnd 结束的数字（不包含）
 	 * @return 数字列表
@@ -383,193 +390,223 @@ public class CollectionUtil {
 	public static int[] range(int includedStart, int excludedEnd) {
 		return range(includedStart, excludedEnd, 1);
 	}
-	
+
 	/**
 	 * 生成一个数字列表<br>
 	 * 自动判定正序反序
+	 * 
 	 * @param includedStart 开始的数字（包含）
 	 * @param excludedEnd 结束的数字（不包含）
 	 * @param step 步进
 	 * @return 数字列表
 	 */
 	public static int[] range(int includedStart, int excludedEnd, int step) {
-		if(includedStart > excludedEnd) {
+		if (includedStart > excludedEnd) {
 			int tmp = includedStart;
 			includedStart = excludedEnd;
 			excludedEnd = tmp;
 		}
-		
-		if(step <=0) {
+
+		if (step <= 0) {
 			step = 1;
 		}
-		
+
 		int deviation = excludedEnd - includedStart;
 		int length = deviation / step;
-		if(deviation % step != 0) {
+		if (deviation % step != 0) {
 			length += 1;
 		}
 		int[] range = new int[length];
-		for(int i = 0; i < length; i++) {
+		for (int i = 0; i < length; i++) {
 			range[i] = includedStart;
 			includedStart += step;
 		}
 		return range;
 	}
-	
+
 	/**
 	 * 截取数组的部分
+	 * 
 	 * @param list 被截取的数组
 	 * @param start 开始位置（包含）
 	 * @param end 结束位置（不包含）
 	 * @return 截取后的数组，当开始位置超过最大时，返回null
 	 */
 	public static <T> List<T> sub(List<T> list, int start, int end) {
-		if(list == null || list.isEmpty()) {
+		if (list == null || list.isEmpty()) {
 			return null;
 		}
-		
-		if(start < 0) {
+
+		if (start < 0) {
 			start = 0;
 		}
-		if(end < 0) {
+		if (end < 0) {
 			end = 0;
 		}
-		
-		if(start > end) {
+
+		if (start > end) {
 			int tmp = start;
 			start = end;
 			end = tmp;
 		}
-		
+
 		final int size = list.size();
-		if(end > size) {
-			if(start >= size) {
+		if (end > size) {
+			if (start >= size) {
 				return null;
 			}
 			end = size;
 		}
-		
+
 		return list.subList(start, end);
 	}
-	
+
 	/**
 	 * 截取集合的部分
+	 * 
 	 * @param list 被截取的数组
 	 * @param start 开始位置（包含）
 	 * @param end 结束位置（不包含）
 	 * @return 截取后的数组，当开始位置超过最大时，返回null
 	 */
 	public static <T> List<T> sub(Collection<T> list, int start, int end) {
-		if(list == null || list.isEmpty()) {
+		if (list == null || list.isEmpty()) {
 			return null;
 		}
-		
+
 		return sub(new ArrayList<T>(list), start, end);
 	}
-	
+
 	/**
 	 * 过滤
+	 * 
 	 * @param array 数组
 	 * @param filter 过滤器接口
 	 * @return 过滤后的数组
 	 */
-	public static <T> T[] filter(T[] array, Filter<T> filter){
+	public static <T> T[] filter(T[] array, Filter<T> filter) {
 		ArrayList<T> list = new ArrayList<T>();
 		for (T t : array) {
-			if(filter.accept(t)){
+			if (filter.accept(t)) {
 				list.add(t);
 			}
 		}
 		return list.toArray(Arrays.copyOf(array, list.size()));
 	}
-	
+
+	/**
+	 * 过滤<br>
+	 * 过滤会改变原集合的内容
+	 * 
+	 * @param collection 集合
+	 * @param filter 过滤器接口
+	 * @return 过滤后的数组
+	 */
+	public static <T> Collection<T> filter(Collection<T> collection, Filter<T> filter) {
+		Iterator<T> it = collection.iterator();
+		while (it.hasNext()) {
+			T t = it.next();
+			if (false == filter.accept(t)) {
+				it.remove();
+			}
+		}
+		return collection;
+	}
+
 	/**
 	 * 数组是否为空
+	 * 
 	 * @param array 数组
 	 * @return 是否为空
 	 */
 	public static <T> boolean isEmpty(T[] array) {
 		return array == null || array.length == 0;
 	}
-	
+
 	/**
 	 * 数组是否为非空
+	 * 
 	 * @param array 数组
 	 * @return 是否为非空
 	 */
 	public static <T> boolean isNotEmpty(T[] array) {
 		return false == isEmpty(array);
 	}
-	
+
 	/**
 	 * 集合是否为空
+	 * 
 	 * @param collection 集合
 	 * @return 是否为空
 	 */
 	public static boolean isEmpty(Collection<?> collection) {
 		return collection == null || collection.isEmpty();
 	}
-	
+
 	/**
 	 * 集合是否为非空
+	 * 
 	 * @param collection 集合
 	 * @return 是否为非空
 	 */
 	public static boolean isNotEmpty(Collection<?> collection) {
 		return false == isEmpty(collection);
 	}
-	
+
 	/**
 	 * Map是否为空
+	 * 
 	 * @param map 集合
 	 * @return 是否为空
 	 */
 	public static boolean isEmpty(Map<?, ?> map) {
 		return map == null || map.isEmpty();
 	}
-	
+
 	/**
 	 * Map是否为非空
+	 * 
 	 * @param map 集合
 	 * @return 是否为非空
 	 */
 	public static <T> boolean isNotEmpty(Map<?, ?> map) {
 		return false == isEmpty(map);
 	}
-	
+
 	/**
 	 * 映射键值（参考Python的zip()函数）<br>
 	 * 例如：<br>
-	 * 		keys =    [a,b,c,d]<br>
-	 *		values = [1,2,3,4]<br>
+	 * keys = [a,b,c,d]<br>
+	 * values = [1,2,3,4]<br>
 	 * 则得到的Map是 {a=1, b=2, c=3, d=4}<br>
 	 * 如果两个数组长度不同，则只对应最短部分
+	 * 
 	 * @param keys 键列表
 	 * @param values 值列表
 	 * @return Map
 	 */
 	public static <T, K> Map<T, K> zip(T[] keys, K[] values) {
-		if(isEmpty(keys) || isEmpty(values)) {
+		if (isEmpty(keys) || isEmpty(values)) {
 			return null;
 		}
-		
+
 		final int size = Math.min(keys.length, values.length);
-		final Map<T, K> map = new HashMap<T, K>((int)(size / 0.75));
-		for(int i = 0; i < size; i++) {
+		final Map<T, K> map = new HashMap<T, K>((int) (size / 0.75));
+		for (int i = 0; i < size; i++) {
 			map.put(keys[i], values[i]);
 		}
-		
+
 		return map;
 	}
-	
+
 	/**
 	 * 映射键值（参考Python的zip()函数）<br>
 	 * 例如：<br>
-	 * 		keys =    a,b,c,d<br>
-	 *		values = 1,2,3,4<br>
-	 *		delimiter = ,
-	 * 则得到的Map是 {a=1, b=2, c=3, d=4}<br>
+	 * keys = a,b,c,d<br>
+	 * values = 1,2,3,4<br>
+	 * delimiter = , 则得到的Map是 {a=1, b=2, c=3, d=4}<br>
 	 * 如果两个数组长度不同，则只对应最短部分
+	 * 
 	 * @param keys 键列表
 	 * @param values 值列表
 	 * @return Map
@@ -577,37 +614,39 @@ public class CollectionUtil {
 	public static Map<String, String> zip(String keys, String values, String delimiter) {
 		return zip(StrUtil.split(keys, delimiter), StrUtil.split(values, delimiter));
 	}
-	
+
 	/**
 	 * 映射键值（参考Python的zip()函数）<br>
 	 * 例如：<br>
-	 * 		keys =    [a,b,c,d]<br>
-	 *		values = [1,2,3,4]<br>
+	 * keys = [a,b,c,d]<br>
+	 * values = [1,2,3,4]<br>
 	 * 则得到的Map是 {a=1, b=2, c=3, d=4}<br>
 	 * 如果两个数组长度不同，则只对应最短部分
+	 * 
 	 * @param keys 键列表
 	 * @param values 值列表
 	 * @return Map
 	 */
 	public static <T, K> Map<T, K> zip(Collection<T> keys, Collection<K> values) {
-		if(isEmpty(keys) || isEmpty(values)) {
+		if (isEmpty(keys) || isEmpty(values)) {
 			return null;
 		}
-		
+
 		final List<T> keyList = new ArrayList<T>(keys);
 		final List<K> valueList = new ArrayList<K>(values);
-		
+
 		final int size = Math.min(keys.size(), values.size());
-		final Map<T, K> map = new HashMap<T, K>((int)(size / 0.75));
-		for(int i = 0; i < size; i++) {
+		final Map<T, K> map = new HashMap<T, K>((int) (size / 0.75));
+		for (int i = 0; i < size; i++) {
 			map.put(keyList.get(i), valueList.get(i));
 		}
-		
+
 		return map;
 	}
-	
+
 	/**
 	 * 数组中是否包含元素
+	 * 
 	 * @param array 数组
 	 * @param value 被检查的元素
 	 * @return 是否包含
@@ -615,177 +654,189 @@ public class CollectionUtil {
 	public static <T> boolean contains(T[] array, T value) {
 		final Class<?> componetType = array.getClass().getComponentType();
 		boolean isPrimitive = false;
-		if(null != componetType) {
+		if (null != componetType) {
 			isPrimitive = componetType.isPrimitive();
 		}
 		for (T t : array) {
-			if(t == value) {
+			if (t == value) {
 				return true;
-			}else if(false == isPrimitive && null != value && value.equals(t)) {
+			} else if (false == isPrimitive && null != value && value.equals(t)) {
 				return true;
 			}
 		}
 		return false;
 	}
-	
+
 	/**
 	 * 将Entry集合转换为HashMap
+	 * 
 	 * @param entryCollection entry集合
 	 * @return Map
 	 */
 	public static <T, K> HashMap<T, K> toMap(Collection<Entry<T, K>> entryCollection) {
-		HashMap<T,K> map = new HashMap<T, K>();
+		HashMap<T, K> map = new HashMap<T, K>();
 		for (Entry<T, K> entry : entryCollection) {
 			map.put(entry.getKey(), entry.getValue());
 		}
 		return map;
 	}
-	
+
 	/**
 	 * 将集合转换为排序后的TreeSet
+	 * 
 	 * @param collection 集合
 	 * @param comparator 比较器
 	 * @return treeSet
 	 */
-	public static <T> TreeSet<T> toTreeSet(Collection<T> collection, Comparator<T> comparator){
+	public static <T> TreeSet<T> toTreeSet(Collection<T> collection, Comparator<T> comparator) {
 		final TreeSet<T> treeSet = new TreeSet<T>(comparator);
 		for (T t : collection) {
 			treeSet.add(t);
 		}
 		return treeSet;
 	}
-	
+
 	/**
 	 * 排序集合
+	 * 
 	 * @param collection 集合
 	 * @param comparator 比较器
 	 * @return treeSet
 	 */
-	public static <T> List<T> sort(Collection<T> collection, Comparator<T> comparator){
+	public static <T> List<T> sort(Collection<T> collection, Comparator<T> comparator) {
 		List<T> list = new ArrayList<T>(collection);
 		Collections.sort(list, comparator);
 		return list;
 	}
-	
-	//------------------------------------------------------------------- 基本类型的数组转换为包装类型数组
+
+	// ------------------------------------------------------------------- 基本类型的数组转换为包装类型数组
 	/**
 	 * 将基本类型数组包装为包装类型
+	 * 
 	 * @param values 基本类型数组
 	 * @return 包装类型数组
 	 */
-	public static Integer[] wrap(int... values){
+	public static Integer[] wrap(int... values) {
 		final int length = values.length;
 		Integer[] array = new Integer[length];
-		for(int i = 0; i < length; i++){
+		for (int i = 0; i < length; i++) {
 			array[i] = values[i];
 		}
 		return array;
 	}
-	
+
 	/**
 	 * 将基本类型数组包装为包装类型
+	 * 
 	 * @param values 基本类型数组
 	 * @return 包装类型数组
 	 */
-	public static Long[] wrap(long... values){
+	public static Long[] wrap(long... values) {
 		final int length = values.length;
 		Long[] array = new Long[length];
-		for(int i = 0; i < length; i++){
+		for (int i = 0; i < length; i++) {
 			array[i] = values[i];
 		}
 		return array;
 	}
-	
+
 	/**
 	 * 将基本类型数组包装为包装类型
+	 * 
 	 * @param values 基本类型数组
 	 * @return 包装类型数组
 	 */
-	public static Character[] wrap(char... values){
+	public static Character[] wrap(char... values) {
 		final int length = values.length;
 		Character[] array = new Character[length];
-		for(int i = 0; i < length; i++){
+		for (int i = 0; i < length; i++) {
 			array[i] = values[i];
 		}
 		return array;
 	}
-	
+
 	/**
 	 * 将基本类型数组包装为包装类型
+	 * 
 	 * @param values 基本类型数组
 	 * @return 包装类型数组
 	 */
-	public static Byte[] wrap(byte... values){
+	public static Byte[] wrap(byte... values) {
 		final int length = values.length;
 		Byte[] array = new Byte[length];
-		for(int i = 0; i < length; i++){
+		for (int i = 0; i < length; i++) {
 			array[i] = values[i];
 		}
 		return array;
 	}
-	
+
 	/**
 	 * 将基本类型数组包装为包装类型
+	 * 
 	 * @param values 基本类型数组
 	 * @return 包装类型数组
 	 */
-	public static Short[] wrap(short... values){
+	public static Short[] wrap(short... values) {
 		final int length = values.length;
 		Short[] array = new Short[length];
-		for(int i = 0; i < length; i++){
+		for (int i = 0; i < length; i++) {
 			array[i] = values[i];
 		}
 		return array;
 	}
-	
+
 	/**
 	 * 将基本类型数组包装为包装类型
+	 * 
 	 * @param values 基本类型数组
 	 * @return 包装类型数组
 	 */
-	public static Float[] wrap(float... values){
+	public static Float[] wrap(float... values) {
 		final int length = values.length;
 		Float[] array = new Float[length];
-		for(int i = 0; i < length; i++){
+		for (int i = 0; i < length; i++) {
 			array[i] = values[i];
 		}
 		return array;
 	}
-	
+
 	/**
 	 * 将基本类型数组包装为包装类型
+	 * 
 	 * @param values 基本类型数组
 	 * @return 包装类型数组
 	 */
-	public static Double[] wrap(double... values){
+	public static Double[] wrap(double... values) {
 		final int length = values.length;
 		Double[] array = new Double[length];
-		for(int i = 0; i < length; i++){
+		for (int i = 0; i < length; i++) {
 			array[i] = values[i];
 		}
 		return array;
 	}
-	
+
 	/**
 	 * 将基本类型数组包装为包装类型
+	 * 
 	 * @param values 基本类型数组
 	 * @return 包装类型数组
 	 */
-	public static Boolean[] wrap(boolean... values){
+	public static Boolean[] wrap(boolean... values) {
 		final int length = values.length;
 		Boolean[] array = new Boolean[length];
-		for(int i = 0; i < length; i++){
+		for (int i = 0; i < length; i++) {
 			array[i] = values[i];
 		}
 		return array;
 	}
-	
+
 	/**
 	 * 判定给定对象是否为数组类型
+	 * 
 	 * @param obj 对象
 	 * @return 是否为数组类型
 	 */
-	public static boolean isArray(Object obj){
+	public static boolean isArray(Object obj) {
 		return obj.getClass().isArray();
 	}
 }
