a80c122c3aaf63b0c4dc7456246c1ad8415865c9
Looly <loolly@gmail.com>
2017-02-05 19:32:42 +0800

    delete pre file

--- src/test/java/com/xiaoleilu/hutool/demo/SystemUtilDemo.java
+++
@@ -1,9 +0,0 @@
-package com.xiaoleilu.hutool.demo;
-
-import com.xiaoleilu.hutool.system.SystemUtil;
-
-public class SystemUtilDemo {
-	public static void main(String[] args) {
-		SystemUtil.dumpSystemInfo();
-	}
-}
