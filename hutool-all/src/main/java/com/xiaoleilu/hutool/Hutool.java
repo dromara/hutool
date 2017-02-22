a96b46bde8c5eb58f3fa6bdaaff953157de672ac
Looly <loolly@gmail.com>
2017-02-22 21:50:51 +0800

    add source jar

---
+++ hutool-all/src/main/java/com/xiaoleilu/hutool/Hutool.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (C) 2017 hutool.cn
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.xiaoleilu.hutool;
+
+/**
+ * Hutool是一个工具包，我日常写项目的一些积累，广泛学习其它开源项目精髓，<br>
+ * 经过自己整理修改，希望你看了之后会有所启发或者能给你工作中带来帮助。<br>
+ * 叫做Hutool也有“糊涂”之意，表示很多功能糊里糊涂就实现了。
+ * 
+ * @author Looly
+ *
+ */
+public class Hutool {
+	private Hutool() {}
+}
