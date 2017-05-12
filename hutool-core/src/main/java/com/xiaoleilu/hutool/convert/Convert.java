1db1f70718d342d181532e256238bc22419a19e3
Looly <loolly@gmail.com>
2017-05-12 12:14:00 +0800

    fix blank

--- hutool-core/src/main/java/com/xiaoleilu/hutool/convert/Convert.java
+++ hutool-core/src/main/java/com/xiaoleilu/hutool/convert/Convert.java
@@ -502,8 +502,8 @@ public final class Convert {
 				continue;
 			}
 
-			if (c[i] == '\u3000' || c[i] == '\u00a0') {
-				//\u3000是中文全角空格，\u00a0是不间断空格
+			if (c[i] == '\u3000' || c[i] == '\u00a0' || c[i] == '\u2007' || c[i] == '\u202F') {
+				//\u3000是中文全角空格，\u00a0、\u2007、\u202F是不间断空格
 				c[i] = ' ';
 			} else if (c[i] > '\uFF00' && c[i] < '\uFF5F') {
 				c[i] = (char) (c[i] - 65248);
