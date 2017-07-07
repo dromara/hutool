c602e876d8ec3ba13680a82338cb420f240b30f7
Kun <kuncuit@gmail.com>
2017-07-07 23:02:31 +0800

    Fix array object empty check.

--- hutool-core/src/test/java/com/xiaoleilu/hutool/core/util/ArrayUtilTest.java
+++ hutool-core/src/test/java/com/xiaoleilu/hutool/core/util/ArrayUtilTest.java
@@ -19,6 +19,7 @@ public class ArrayUtilTest {
 	public void isEmptyTest(){
 		int[] a = {};
 		Assert.assertTrue(ArrayUtil.isEmpty(a));
+		Assert.assertTrue(ArrayUtil.isEmpty((Object) a));
 		int[] b = null;
 		Assert.assertTrue(ArrayUtil.isEmpty(b));
 		Object c = null;
