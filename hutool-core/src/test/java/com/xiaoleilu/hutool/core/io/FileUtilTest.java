907bbcf8287f5c403fcdb8dc351acdab39fee608
xyz327 <xyz327@outlook.com>
2017-07-14 20:33:27 +0800

    fix: FileUtil#equals(File,File)方法当其中有一个文件不存在时会报错

--- hutool-core/src/test/java/com/xiaoleilu/hutool/core/io/FileUtilTest.java
+++ hutool-core/src/test/java/com/xiaoleilu/hutool/core/io/FileUtilTest.java
@@ -1,5 +1,6 @@
 package com.xiaoleilu.hutool.core.io;
 
+import java.io.File;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -11,5 +12,14 @@ public class FileUtilTest {
 	public void getAbsolutePathTest(){
 		String absolutePath = FileUtil.getAbsolutePath("aaa");
 		Assert.assertNotNull(absolutePath);
+	}
+
+	@Test
+	public void copyTest() throws Exception {
+		String dir = FileUtilTest.class.getResource("/").getFile();
+		File srcFile = FileUtil.file(dir, "hutool.jpg");
+		File destFile = FileUtil.file(dir, "hutool.copy.jpg");
+
+		FileUtil.copy(srcFile, destFile, true);
 	}
 }
