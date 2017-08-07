deb8a6002957202c0b3dc3dc46fd301677ee2ade
Looly <loolly@gmail.com>
2017-08-07 11:15:45 +0800

    fix ArrayUtil.isEmpty bug

--- hutool-core/src/main/java/com/xiaoleilu/hutool/util/ArrayUtil.java
+++ hutool-core/src/main/java/com/xiaoleilu/hutool/util/ArrayUtil.java
@@ -48,7 +48,14 @@ public class ArrayUtil {
 	 * @return 是否为空
 	 */
 	public static boolean isEmpty(final Object array) {
-		return array == null || (false == isArray(array)) || Array.getLength(array) == 0;
+		if(null == array) {
+			return true;
+		}else if(false == isArray(array)) {
+			//非数组，理解为此对象为数组的第一个元素
+			return false;
+		}else {
+			return 0 == Array.getLength(array);
+		}
 	}
 
 	/**
