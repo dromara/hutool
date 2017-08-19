f0d7262712bafcd67d78649d9dda568268ca59ae
Looly <loolly@gmail.com>
2017-08-19 10:53:54 +0800

    remove invalid sample

--- hutool-json/src/test/java/com/xiaoleilu/hutool/json/JSONUtilTest.java
+++ hutool-json/src/test/java/com/xiaoleilu/hutool/json/JSONUtilTest.java
@@ -32,25 +32,4 @@ public class JSONUtilTest {
 		String str = JSONUtil.toJsonStr(map);
 		Console.log(str);
 	}
-	
-	@Test
-	@Ignore
-	public void toJsonStrTest2() {
-		UserA a1 = new UserA();
-		a1.setA("aaaa");
-		a1.setDate(DateUtil.date());
-		a1.setName("AAAAName");
-		UserA a2 = new UserA();
-		a2.setA("aaaa222");
-		a2.setDate(DateUtil.date());
-		a2.setName("AAAA222Name");
-		
-		ArrayList<UserA> list = CollectionUtil.newArrayList(a1, a2);
-		HashMap<String, Object> map = CollectionUtil.newHashMap();
-		map.put("total", 13);
-		map.put("rows", list);
-		
-		String str = JSONUtil.toJsonStr(map);
-		Console.log(str);
-	}
 }
