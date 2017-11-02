2e95edae43063035a2b62f581b44920f769c9efe
looly <loolly@aliyun.com>
2017-11-02 14:03:45 +0100

    add method

--- hutool-core/src/main/java/com/xiaoleilu/hutool/io/FileUtil.java
+++ hutool-core/src/main/java/com/xiaoleilu/hutool/io/FileUtil.java
@@ -1582,6 +1582,19 @@ public class FileUtil {
 	public static byte[] readBytes(File file) throws IORuntimeException {
 		return FileReader.create(file).readBytes();
 	}
+	
+	/**
+	 * 读取文件所有数据<br>
+	 * 文件的长度不能超过Integer.MAX_VALUE
+	 * 
+	 * @param filePath 文件路径
+	 * @return 字节码
+	 * @throws IORuntimeException IO异常
+	 * @since 3.2.0
+	 */
+	public static byte[] readBytes(String filePath) throws IORuntimeException {
+		return readBytes(file(filePath));
+	}
 
 	/**
 	 * 读取文件内容
