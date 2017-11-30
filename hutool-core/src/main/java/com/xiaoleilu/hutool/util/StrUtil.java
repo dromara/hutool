3553e74e881c04f32826ccfd06f57fa996ebd263
looly <loolly@aliyun.com>
2017-11-30 12:18:39 +0000

    fix comment

--- hutool-core/src/main/java/com/xiaoleilu/hutool/util/StrUtil.java
+++ hutool-core/src/main/java/com/xiaoleilu/hutool/util/StrUtil.java
@@ -1313,13 +1313,13 @@ public class StrUtil {
 	}
 
 	/**
-	 * 截取字符串,从指定位置开始,截取指定长度的字符串
+	 * 截取字符串,从指定位置开始,截取指定长度的字符串<br>
+	 * author weibaohui
 	 * 
 	 * @param input 原始字符串
 	 * @param fromIndex 开始的index,包括
 	 * @param length 要截取的长度
 	 * @return 截取后的字符串
-	 * @author weibaohui
 	 */
 	public static String subWithLength(String input, int fromIndex, int length) {
 		return sub(input, fromIndex, fromIndex + length);
