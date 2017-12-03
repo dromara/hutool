988c683a9de2413b89f86fcc2adc9831b20daef0
looly <loolly@aliyun.com>
2017-12-03 09:01:03 +0000

    fix color

--- push_master.sh
+++ push_master.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-echo 'checkout to master'
+echo -e "\033[32m Checkout to master\033[0m"
 git checkout master
 
-echo 'Merge dev branch'
+echo -e "\033[32m Merge dev branch\033[0m"
 git merge dev
 
-echo 'Push to origin master'
+echo -e "\033[32m Push to origin master\033[0m"
 git push origin master
-echo 'Push to osc master'
+echo -e "\033[32m Push to osc master\033[0m"
 git push osc master
