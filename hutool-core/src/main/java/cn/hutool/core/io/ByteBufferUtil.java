12bd66e0da9f1407d056b01e6f7ba10a386a764b
looly <loolly@aliyun.com>
2018-01-08 16:57:32 +0000

    add ByteBufferUtil

---
+++ hutool-core/src/main/java/cn/hutool/core/io/ByteBufferUtil.java
@@ -0,0 +1,187 @@
+package cn.hutool.core.io;
+
+import java.nio.ByteBuffer;
+import java.nio.charset.Charset;
+
+import cn.hutool.core.util.StrUtil;
+
+/**
+ * {@link ByteBuffer} 工具类<br>
+ * 此工具来自于 t-io 项目以及其它项目的相关部分收集
+ * TODO 缺少单元测试
+ * 
+ * @author tanyaowu, looly
+ * @since 4.0.0
+ *
+ */
+public class ByteBufferUtil {
+
+	/**
+	 * ByteBuffer中数据的长度
+	 * 
+	 * @param byteBuffer {@link ByteBuffer}
+	 * @return 数据长度
+	 */
+	public static int size(ByteBuffer byteBuffer) {
+		return byteBuffer.limit() - byteBuffer.position();
+	}
+
+	/**
+	 * 混合两个{@link ByteBuffer} 为一个
+	 * 
+	 * @param byteBuffer1 ByteBuffer1
+	 * @param byteBuffer2 ByteBuffer2
+	 * @return 混合后的ByteBuffer
+	 */
+	public static ByteBuffer composite(ByteBuffer byteBuffer1, ByteBuffer byteBuffer2) {
+		int capacity = size(byteBuffer1) + size(byteBuffer2);
+		final ByteBuffer ret = ByteBuffer.allocate(capacity);
+
+		ret.put(byteBuffer1);
+		ret.put(byteBuffer2);
+
+		return ret;
+	}
+
+	/**
+	 * 拷贝到一个新的ByteBuffer
+	 * 
+	 * @param src 源ByteBuffer
+	 * @param start 起始位置（包括）
+	 * @param end 结束位置（不包括）
+	 * @return 新的ByteBuffer
+	 */
+	public static ByteBuffer copy(ByteBuffer src, int start, int end) {
+		return copy(src, ByteBuffer.allocate(end - start));
+	}
+
+	/**
+	 * 拷贝ByteBuffer
+	 * 
+	 * @param src 源ByteBuffer
+	 * @param dest 目标ByteBuffer
+	 * @return 目标ByteBuffer
+	 */
+	public static ByteBuffer copy(ByteBuffer src, ByteBuffer dest) {
+		return copy(src, dest, Math.min(size(src), size(dest)));
+	}
+
+	/**
+	 * 拷贝ByteBuffer
+	 * 
+	 * @param src 源ByteBuffer
+	 * @param dest 目标ByteBuffer
+	 * @param length 长度
+	 * @return 目标ByteBuffer
+	 */
+	public static ByteBuffer copy(ByteBuffer src, ByteBuffer dest, int length) {
+		return copy(src, src.position(), dest, dest.position(), length);
+	}
+
+	/**
+	 * 拷贝ByteBuffer
+	 * 
+	 * @param src 源ByteBuffer
+	 * @param srcStart 源开始的位置
+	 * @param dest 目标ByteBuffer
+	 * @param destStart 目标开始的位置
+	 * @param length 长度
+	 * @return 目标ByteBuffer
+	 */
+	public static ByteBuffer copy(ByteBuffer src, int srcStart, ByteBuffer dest, int destStart, int length) {
+		System.arraycopy(src.array(), srcStart, dest.array(), destStart, length);
+		return dest;
+	}
+
+	/**
+	 * 读取指定长度的bytes
+	 * 
+	 * @param buffer ByteBuffer
+	 * @param length 长度
+	 * @return bytes
+	 */
+	public static byte[] readBytes(ByteBuffer buffer, int length) {
+		byte[] ab = new byte[length];
+		buffer.get(ab);
+		return ab;
+	}
+	
+	/**
+	 * 读取指定区间的数据
+	 * 
+	 * @param buffer {@link ByteBuffer}
+	 * @param start 开始位置
+	 * @param end 结束位置
+	 * @return bytes
+	 */
+	public static byte[] readBytes(ByteBuffer buffer, int start, int end) {
+		byte[] bs = new byte[end - start];
+		System.arraycopy(buffer.array(), start, bs, 0, bs.length);
+		return bs;
+	}
+	
+	/**
+	 * 一行的末尾位置，查找位置时位移ByteBuffer到结束位置
+	 *
+	 * @param buffer {@link ByteBuffer}
+	 * @return 末尾位置，未找到或达到最大长度返回-1
+	 */
+	public static int lineEnd(ByteBuffer buffer) {
+		return lineEnd(buffer, buffer.remaining());
+	}
+
+	/**
+	 * 一行的末尾位置，查找位置时位移ByteBuffer到结束位置
+	 *
+	 * @param buffer {@link ByteBuffer}
+	 * @param maxLength 读取最大长度
+	 * @return 末尾位置，未找到或达到最大长度返回-1
+	 */
+	public static int lineEnd(ByteBuffer buffer, int maxLength) {
+		int primitivePosition = buffer.position();
+		boolean canEnd = false;
+		int count = 0;
+		byte b;
+		while (buffer.hasRemaining()) {
+			b = buffer.get();
+			if (b == StrUtil.C_CR) {
+				canEnd = true;
+			} else if (b == StrUtil.C_LF) {
+				return canEnd ? count - 1 : count;
+			} else {
+				// 只有\r无法确认换行
+				canEnd = false;
+			}
+			// 继续查找
+			count++;
+			if (count > maxLength) {
+				//查找到尽头，未找到，还原位置
+				buffer.position(primitivePosition);
+				throw new IndexOutOfBoundsException(StrUtil.format("Position is out of maxLength: {}", maxLength));
+			}
+		}
+		
+		//读到结束位置
+		return -1;
+	}
+
+	/**
+	 * 读取一行
+	 * @param buffer ByteBuffer
+	 * @param charset 编码
+	 * @return 一行
+	 */
+	public static String readLine(ByteBuffer buffer, Charset charset) {
+		final int startPosition = buffer.position();
+		final int endPosition = lineEnd(buffer);
+
+		if (endPosition > startPosition) {
+			byte[] bs = readBytes(buffer, startPosition, endPosition);
+			return StrUtil.str(bs, charset);
+		} else if (endPosition == startPosition) {
+			return StrUtil.EMPTY;
+		}
+		
+		return null;
+	}
+}
