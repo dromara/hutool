8d94f6982b86a032b24e1187be76c749b228b0e5
looly <loolly@aliyun.com>
2018-01-16 06:54:25 +0000

    fix sh

--- bin/push_master.sh
+++ bin/push_master.sh
@@ -4,7 +4,7 @@ echo -e "\033[32mCheckout to v4-master\033[0m"
 git checkout v4-master
 
 echo -e "\033[32mMerge v4-dev branch\033[0m"
-git merger v4-dev -m 'Prepare release'
+git merge v4-dev -m 'Prepare release'
 
 echo -e "\033[32mPush to origin v4-master\033[0m"
 git push origin v4-master
