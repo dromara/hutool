38a87d8d347b4114007f91ed34ca2c9c8bbb85e5
looly <loolly@aliyun.com>
2018-03-25 07:09:57 +0100

    change test

--- hutool-extra/src/test/java/cn/hutool/extra/mail/MailTest.java
+++ hutool-extra/src/test/java/cn/hutool/extra/mail/MailTest.java
@@ -23,13 +23,6 @@ public class MailTest {
 	
 	@Test
 	@Ignore
-	public void sendTest2() {
-		MailUtil.send("hutool@foxmail.com", "测试", "<h1>邮件来自Hutool测试</h1>", true, //
-				FileUtil.file("D:\\Documents\\CTMP_CASE_MGT\\src\\main\\resources\\upload\\channel\\一阶段平台建设周报2018.3.12-3.16.xlsx"));
-	}
-	
-	@Test
-	@Ignore
 	public void sendHtmlTest() {
 		MailUtil.send("hutool@foxmail.com", "测试", "<h1>邮件来自Hutool测试</h1>", true);
 	}
