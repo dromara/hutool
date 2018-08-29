47fdd0be91c9da99d4237953a7586f1fb78bb5e2
looly <loolly@aliyun.com>
2018-08-29 12:12:13 +0100

    fix null bug

--- hutool-json/src/main/java/cn/hutool/json/InternalJSONUtil.java
+++ hutool-json/src/main/java/cn/hutool/json/InternalJSONUtil.java
@@ -297,6 +297,7 @@ final class InternalJSONUtil {
 		// 非标准转换格式
 		if (value instanceof JSONObject) {
 			if(BeanUtil.isBean(rowType)) {
+				//目标为Bean
 				targetValue = ((JSONObject) value).toBean(type, ignoreError);
 			}
 		} else if (value instanceof JSONArray) {
@@ -311,7 +312,7 @@ final class InternalJSONUtil {
 		// 标准格式转换
 		if (null == targetValue) {
 			try {
-				targetValue = ConverterRegistry.getInstance().convert(rowType, value);
+				targetValue = ConverterRegistry.getInstance().convert(type, value);
 			} catch (ConvertException e) {
 				if (ignoreError) {
 					return null;
