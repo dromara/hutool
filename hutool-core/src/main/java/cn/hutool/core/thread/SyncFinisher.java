ec2c9c615d50637ebe8907133a7a7a19afb2fdcc
looly <loolly@aliyun.com>
2018-10-04 06:13:12 +0100

    add test

--- hutool-core/src/main/java/cn/hutool/core/thread/SyncFinisher.java
+++ hutool-core/src/main/java/cn/hutool/core/thread/SyncFinisher.java
@@ -1,4 +1,4 @@
-package cn.hutool.core.lang;
+package cn.hutool.core.thread;
 
 import java.util.LinkedHashSet;
 import java.util.Set;
@@ -10,22 +10,24 @@ import cn.hutool.core.exceptions.NotInitedException;
  * 线程同步结束器<br>
  * 在完成一组正在其他线程中执行的操作之前，它允许一个或多个线程一直等待。<br>
  * 不能保证同时开始
- * @author xiaoleilu
- *
+ * 
+ * @author Looly
+ * @since 4.1.15
  */
-public class SyncFinisher{
+public class SyncFinisher {
 	private CountDownLatch countDownLatch;
-	
+
 	private Set<Worker> workers = new LinkedHashSet<Worker>();
-	
+
 	/**
 	 * 增加工作线程
+	 * 
 	 * @param worker 工作线程
 	 */
 	synchronized public void addWorker(Worker worker) {
 		workers.add(worker);
 	}
-	
+
 	/**
 	 * 开始工作
 	 */
@@ -35,35 +37,37 @@ public class SyncFinisher{
 			worker.start();
 		}
 	}
-	
+
 	/**
 	 * 等待所有Worker工作结束，否则阻塞
+	 * 
 	 * @throws InterruptedException 用户中断
 	 */
 	public void await() throws InterruptedException {
-		if(countDownLatch == null) {
+		if (countDownLatch == null) {
 			throw new NotInitedException("Please call start() method first!");
 		}
-		
+
 		countDownLatch.await();
 	}
-	
+
 	/**
 	 * 清空工作线程对象
 	 */
 	public void clearWorker() {
 		workers.clear();
 	}
-	
+
 	/**
 	 * @return 并发数
 	 */
 	public long count() {
 		return countDownLatch.getCount();
 	}
-	
+
 	/**
 	 * 工作者，为一个线程
+	 * 
 	 * @author xiaoleilu
 	 *
 	 */
@@ -77,7 +81,7 @@ public class SyncFinisher{
 				countDownLatch.countDown();
 			}
 		}
-		
+
 		public abstract void work();
 	}
 }
--- hutool-core/src/test/java/cn/hutool/core/util/IdUtilTest.java
+++ hutool-core/src/test/java/cn/hutool/core/util/IdUtilTest.java
@@ -1,15 +1,20 @@
 package cn.hutool.core.util;
 
+import java.util.Set;
 import java.util.UUID;
+import java.util.concurrent.CountDownLatch;
 
 import org.junit.Assert;
 import org.junit.Ignore;
 import org.junit.Test;
 
+import cn.hutool.core.collection.ConcurrentHashSet;
 import cn.hutool.core.date.DateUtil;
 import cn.hutool.core.date.TimeInterval;
+import cn.hutool.core.exceptions.UtilException;
 import cn.hutool.core.lang.Console;
 import cn.hutool.core.lang.Snowflake;
+import cn.hutool.core.thread.ThreadUtil;
 
 /**
  * {@link IdUtil} 单元测试
@@ -58,5 +63,39 @@ public class IdUtilTest {
 		Snowflake snowflake = IdUtil.createSnowflake(1, 1);
 		long id = snowflake.nextId();
 		Assert.assertTrue(id > 0);
+	}
+	
+	@Test
+	public void snowflakeBenchTest() {
+		final Set<Long> set = new ConcurrentHashSet<>();
+		final Snowflake snowflake = IdUtil.createSnowflake(1, 1);
+		
+		//线程数
+		int threadCount = 100;
+		//每个线程生成的ID数
+		final int idCountPerThread = 10000;
+		final CountDownLatch latch = new CountDownLatch(threadCount);
+		for(int i =0; i < threadCount; i++) {
+			ThreadUtil.execute(new Runnable() {
+				
+				@Override
+				public void run() {
+					for(int i =0; i < idCountPerThread; i++) {
+						long id = snowflake.nextId();
+						set.add(id);
+//						Console.log("Add new id: {}", id);
+					}
+					latch.countDown();
+				}
+			});
+		}
+		
+		//等待全部线程结束
+		try {
+			latch.await();
+		} catch (InterruptedException e) {
+			throw new UtilException(e);
+		}
+		Assert.assertEquals(threadCount * idCountPerThread, set.size());
 	}
 }
