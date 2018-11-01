4e04bd976843637cd4bfa8a7a5c383ec6c247ce9
diffblue-assistant <contributions@diffblue.com>
2018-11-01 12:03:56 +0000

    Add unit tests to ThreadUtilTest.java
    Add tests for newExecutorByBlockingCoefficient() and safeSleep()

--- hutool-core/src/test/java/cn/hutool/core/thread/ThreadUtilTest.java
+++ hutool-core/src/test/java/cn/hutool/core/thread/ThreadUtilTest.java
@@ -2,8 +2,12 @@ package cn.hutool.core.thread;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.Rule;
 
 public class ThreadUtilTest {
+
+	@Rule public ExpectedException thrown = ExpectedException.none();
 	
 	@Test
 	public void executeTest() {
@@ -17,5 +21,44 @@ public class ThreadUtilTest {
 			}
 		});
 		
+	}
+
+	@Test
+	public void newExecutorByBlockingCoefficientInputPositiveOutputIllegalArgumentException() {
+
+		// Arrange
+		final float blockingCoefficient = 2.15625f;
+
+		// Act
+		thrown.expect(IllegalArgumentException.class);
+		ThreadUtil.newExecutorByBlockingCoefficient(blockingCoefficient);
+
+		// Method is not expected to return due to exception thrown
+	}
+
+	@Test
+	public void newExecutorByBlockingCoefficientInputNegativeOutputIllegalArgumentException() {
+  
+	  // Arrange
+	  final float blockingCoefficient = -1.3125f;
+  
+	  // Act
+	  thrown.expect(IllegalArgumentException.class);
+	  ThreadUtil.newExecutorByBlockingCoefficient(blockingCoefficient);
+  
+	  // Method is not expected to return due to exception thrown
+	}
+
+	@Test
+	public void safeSleepInputZeroOutputTrue() {
+  
+	  // Arrange
+	  final Number millis = 0;
+  
+	  // Act
+	  final boolean retval = ThreadUtil.safeSleep(millis);
+  
+	  // Assert result
+	  Assert.assertEquals(true, retval);
 	}
 }
