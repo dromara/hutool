f954fda982d022beb5ef7e1ff319e6d9c0e4300f
looly <loolly@aliyun.com>
2019-01-03 12:56:58 +0000

    fix test

--- hutool-core/src/test/java/cn/hutool/core/util/NumberUtilTest.java
+++ hutool-core/src/test/java/cn/hutool/core/util/NumberUtilTest.java
@@ -185,7 +185,7 @@ public class NumberUtilTest {
 		int v1 = NumberUtil.parseInt("0xFF");
 		Assert.assertEquals(255, v1);
 		int v2 = NumberUtil.parseInt("010");
-		Assert.assertEquals(8, v2);
+		Assert.assertEquals(10, v2);
 		int v3 = NumberUtil.parseInt("10");
 		Assert.assertEquals(10, v3);
 		int v4 = NumberUtil.parseInt("   ");
@@ -201,7 +201,7 @@ public class NumberUtilTest {
 		long v1 = NumberUtil.parseLong("0xFF");
 		Assert.assertEquals(255L, v1);
 		long v2 = NumberUtil.parseLong("010");
-		Assert.assertEquals(8L, v2);
+		Assert.assertEquals(10L, v2);
 		long v3 = NumberUtil.parseLong("10");
 		Assert.assertEquals(10L, v3);
 		long v4 = NumberUtil.parseLong("   ");
