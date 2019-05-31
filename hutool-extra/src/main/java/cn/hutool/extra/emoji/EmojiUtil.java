a54af0597fb2da3ba9eb7336d328fcc4dc67e674
looly <loolly@gmail.com>
2019-05-31 17:29:42 +0800

    a

--- hutool-extra/src/main/java/cn/hutool/extra/emoji/EmojiUtil.java
+++ hutool-extra/src/main/java/cn/hutool/extra/emoji/EmojiUtil.java
@@ -16,7 +16,7 @@ import com.vdurmont.emoji.EmojiParser.FitzpatrickAction;
  * emoji-java文档以及别名列表见：<a href="https://github.com/vdurmont/emoji-java">https://github.com/vdurmont/emoji-java</a>
  * 
  * @author looly
- *@since 4.2.1
+ * @since 4.2.1
  */
 public class EmojiUtil {
 
@@ -29,29 +29,33 @@ public class EmojiUtil {
 	public static boolean isEmoji(String str) {
 		return EmojiManager.isEmoji(str);
 	}
-	
+
 	/**
 	 * 是否包含Emoji表情的Unicode符
 	 * 
 	 * @param str 被测试的字符串
 	 * @return 是否包含Emoji表情的Unicode符
+	 * @since 4.5.11
 	 */
 	public static boolean containsEmoji(String str) {
-		if (str == null) return false;
-		char[] chars = str.toCharArray();
+		if (str == null) {
+			return false;
+		}
+		final char[] chars = str.toCharArray();
+		EmojiTrie.Matches status;
 		for (int i = 0; i < chars.length; i++) {
 			for (int j = i + 1; j <= chars.length; j++) {
-				EmojiTrie.Matches status = EmojiManager.isEmoji(Arrays.copyOfRange(chars, i, j));
+				status = EmojiManager.isEmoji(Arrays.copyOfRange(chars, i, j));
 				if (status.impossibleMatch()) {
 					break;
-				}else if (status.exactMatch()) {
+				} else if (status.exactMatch()) {
 					return true;
 				}
 			}
 		}
 		return false;
 	}
-	
+
 	/**
 	 * 通过tag方式获取对应的所有Emoji表情
 	 * 
