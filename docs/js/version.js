9a3aa24e27a0ed42bd3a2ad9a0804f85a7f0af02
looly <loolly@gmail.com>
2019-07-18 23:03:44 +0800

    prepare 4.5.18

--- docs/js/version.js
+++ docs/js/version.js
@@ -1,1 +1,1 @@
-var version = '4.5.17'
+var version = '4.5.18'
--- hutool-extra/src/test/java/cn/hutool/extra/MailTest.java
+++
@@ -1,9 +0,0 @@
-package cn.hutool.extra;
-
-import cn.hutool.extra.mail.MailUtil;
-
-public class MailTest {
-	public static void main(String[] args) {
-		MailUtil.sendText("hutool@foxmail.com", "title test", "mail content test");
-	}
-}
