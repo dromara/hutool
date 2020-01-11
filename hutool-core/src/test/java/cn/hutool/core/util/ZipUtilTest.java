56fc14d91ebbd362335667d09174117d1eac05cd
Looly <loolly@gmail.com>
2020-01-11 21:01:28 +0800

    fix test

--- hutool-core/src/test/java/cn/hutool/core/util/ZipUtilTest.java
+++ hutool-core/src/test/java/cn/hutool/core/util/ZipUtilTest.java
@@ -31,7 +31,7 @@ public class ZipUtilTest {
 	}
 	
 	@Test
-//	@Ignore
+	@Ignore
 	public void unzipTest2() {
 		File unzip = ZipUtil.unzip("f:/test/各种资源.zip", "f:/test/各种资源", CharsetUtil.CHARSET_GBK);
 		Console.log(unzip);
