01cd586cfaa7862dfbcdd13a25b1908fa3de9007
xielei <lei.xie@welian.com>
2020-03-13 11:02:55 +0800

    添加获取当前文件夹名称方法

--- hutool-core/src/test/java/cn/hutool/core/io/file/TailerTest.java
+++ hutool-core/src/test/java/cn/hutool/core/io/file/TailerTest.java
@@ -1,23 +1,31 @@
 package cn.hutool.core.io.file;
 
+import cn.hutool.core.io.FileUtil;
+import cn.hutool.core.util.CharsetUtil;
+import lombok.Data;
 import org.junit.Ignore;
 import org.junit.Test;
 
-import cn.hutool.core.io.FileUtil;
-import cn.hutool.core.util.CharsetUtil;
-
 public class TailerTest {
-	
-	@Test
-	@Ignore
-	public void tailTest() {
-		FileUtil.tail(FileUtil.file("e:/tail.txt"), CharsetUtil.CHARSET_GBK);
-	}
-	
-	@Test
-	@Ignore
-	public void tailWithLinesTest() {
-		Tailer tailer = new Tailer(FileUtil.file("f:/test/test.log"), Tailer.CONSOLE_HANDLER, 2);
-		tailer.start();
-	}
+
+    @Test
+    @Ignore
+    public void tailTest() {
+        FileUtil.tail(FileUtil.file("e:/tail.txt"), CharsetUtil.CHARSET_GBK);
+    }
+
+    @Test
+    @Ignore
+    public void tailWithLinesTest() {
+        Tailer tailer = new Tailer(FileUtil.file("f:/test/test.log"), Tailer.CONSOLE_HANDLER, 2);
+        tailer.start();
+    }
+
+    @Test
+    @Ignore
+    public void testDir() {
+        System.out.println(FileUtil.getCurrentDir(FileUtil.class));
+        System.out.println(FileUtil.getCurrentDir(Data.class));
+        System.out.println(FileUtil.getCurrentDir(Test.class));
+    }
 }
