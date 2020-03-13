5c7caf5a58c3181606b9839ec11585d6ccecad63
Looly <loolly@gmail.com>
2020-03-13 17:23:09 +0800

    fix import bug

--- hutool-extra/src/main/java/cn/hutool/extra/template/engine/TemplateFactory.java
+++ hutool-extra/src/main/java/cn/hutool/extra/template/engine/TemplateFactory.java
@@ -7,7 +7,6 @@ import cn.hutool.extra.template.TemplateConfig;
 import cn.hutool.extra.template.TemplateEngine;
 import cn.hutool.extra.template.TemplateException;
 import cn.hutool.log.StaticLog;
-import com.jfinal.template.Engine;
 
 /**
  * 简单模板工厂，用于根据用户引入的模板引擎jar，自动创建对应的模板引擎对象
@@ -20,7 +19,7 @@ public class TemplateFactory {
 	 * 推荐创建的引擎单例使用，此方法每次调用会返回新的引擎
 	 *
 	 * @param config 模板配置，包括编码、模板文件path等信息
-	 * @return {@link Engine}
+	 * @return {@link TemplateEngine}
 	 */
 	public static TemplateEngine create(TemplateConfig config) {
 		final TemplateEngine engine = doCreate(config);
@@ -33,7 +32,7 @@ public class TemplateFactory {
 	 * 推荐创建的引擎单例使用，此方法每次调用会返回新的引擎
 	 *
 	 * @param config 模板配置，包括编码、模板文件path等信息
-	 * @return {@link Engine}
+	 * @return {@link TemplateEngine}
 	 */
 	private static TemplateEngine doCreate(TemplateConfig config) {
 		final Class<? extends TemplateEngine> customEngineClass = config.getCustomEngine();
