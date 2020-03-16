faa4e54aa47bd954fcbf97eb4baa5a0e2f42bd6d
xielei <lei.xie@welian.com>
2020-03-16 15:54:25 +0800

    添加获取Class文件夹名称

--- hutool-core/src/test/java/cn/hutool/core/io/file/TailerTest.java
+++ hutool-core/src/test/java/cn/hutool/core/io/file/TailerTest.java
@@ -8,24 +8,24 @@ import org.junit.Test;
 
 public class TailerTest {
 
-    @Test
-    @Ignore
-    public void tailTest() {
-        FileUtil.tail(FileUtil.file("e:/tail.txt"), CharsetUtil.CHARSET_GBK);
-    }
+	@Test
+	@Ignore
+	public void tailTest() {
+		FileUtil.tail(FileUtil.file("e:/tail.txt"), CharsetUtil.CHARSET_GBK);
+	}
 
-    @Test
-    @Ignore
-    public void tailWithLinesTest() {
-        Tailer tailer = new Tailer(FileUtil.file("f:/test/test.log"), Tailer.CONSOLE_HANDLER, 2);
-        tailer.start();
-    }
+	@Test
+	@Ignore
+	public void tailWithLinesTest() {
+		Tailer tailer = new Tailer(FileUtil.file("f:/test/test.log"), Tailer.CONSOLE_HANDLER, 2);
+		tailer.start();
+	}
 
-    @Test
-    @Ignore
-    public void testDir() {
-        System.out.println(FileUtil.getCurrentDir(FileUtil.class));
-        System.out.println(FileUtil.getCurrentDir(Data.class));
-        System.out.println(FileUtil.getCurrentDir(Test.class));
-    }
+	@Test
+	@Ignore
+	public void testDir() {
+		System.out.println(FileUtil.getClassDir(FileUtil.class));
+		System.out.println(FileUtil.getClassDir(Data.class));
+		System.out.println(FileUtil.getClassDir(Test.class));
+	}
 }
