8319b009181b2d0a6299f576ef2557724cc178f0
Looly <loolly@gmail.com>
2020-06-05 10:10:02 +0800

    add test

---
+++ hutool-crypto/src/test/java/cn/hutool/crypto/test/AESTest.java
@@ -0,0 +1,20 @@
+package cn.hutool.crypto.test;
+
+import cn.hutool.crypto.Mode;
+import cn.hutool.crypto.Padding;
+import cn.hutool.crypto.symmetric.AES;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class AESTest {
+
+	@Test
+	public void encryptTest() {
+		// 构建
+		AES aes = new AES(Mode.CBC, Padding.PKCS5Padding,
+				"1234567890123456".getBytes(), "1234567890123456".getBytes());
+		String encryptHex = aes.encryptHex("123456");
+		Assert.assertEquals("d637735ae9e21ba50cb686b74fab8d2c", encryptHex);
+	}
+
+}
