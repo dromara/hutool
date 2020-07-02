1ef749bf38c9148e16b8aa82db0ced4fc48c10d1
Liang Long <54536224+GRain-long@users.noreply.github.com>
2020-07-02 16:45:13 +0800

    modify isEmpty(Object array)
    修复判空异常

--- hutool-core/src/main/java/cn/hutool/core/util/ArrayUtil.java
+++ hutool-core/src/main/java/cn/hutool/core/util/ArrayUtil.java
@@ -66,12 +66,13 @@ public class ArrayUtil {
 	 * @return 是否为空
 	 */
 	public static boolean isEmpty(Object array) {
-		if (null == array) {
-			return true;
-		} else if (isArray(array)) {
-			return 0 == Array.getLength(array);
+		if (array != null) {
+			if (isArray(array)) {
+				return 0 == Array.getLength(array);
+			}
+			return false;
 		}
-		throw new UtilException("Object to provide is not a Array !");
+		return true;		
 	}
 
 	/**
