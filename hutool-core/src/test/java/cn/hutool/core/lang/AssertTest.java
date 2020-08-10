ae26f64a84d97cbec36e11c2970f4f874d8f5ac7
lixiaohua <1124205446@qq.com>
2020-08-10 17:32:12 +0800

    断言增加支持自定义异常

--- hutool-core/src/test/java/cn/hutool/core/lang/AssertTest.java
+++ hutool-core/src/test/java/cn/hutool/core/lang/AssertTest.java
@@ -14,4 +14,16 @@ public class AssertTest {
 		String a = null;
 		cn.hutool.core.lang.Assert.isNull(a);
 	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void isTrueTest() {
+		int i = 0;
+		cn.hutool.core.lang.Assert.isTrue(i > 0, IllegalArgumentException::new);
+	}
+
+	@Test(expected = IndexOutOfBoundsException.class)
+	public void isTrueTest2() {
+		int i = -1;
+		cn.hutool.core.lang.Assert.isTrue(i >= 0, IndexOutOfBoundsException::new);
+	}
 }
