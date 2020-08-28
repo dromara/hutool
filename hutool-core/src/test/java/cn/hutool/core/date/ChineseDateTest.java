32ff87a682a86af1d046f4b9ecf808597d1b9177
Looly <loolly@gmail.com>
2020-08-28 17:37:58 +0800

    add test

--- hutool-core/src/test/java/cn/hutool/core/date/ChineseDateTest.java
+++ hutool-core/src/test/java/cn/hutool/core/date/ChineseDateTest.java
@@ -52,11 +52,20 @@ public class ChineseDateTest {
 		String cyclicalYMD = chineseDate.getCyclicalYMD();
 		Assert.assertEquals("壬申年癸丑月丁亥日",cyclicalYMD);
 	}
+
 	@Test
 	public void getCyclicalYMDTest2(){
 		//通过农历构建
 		ChineseDate chineseDate = new ChineseDate(1992,12,14);
 		String cyclicalYMD = chineseDate.getCyclicalYMD();
 		Assert.assertEquals("壬申年癸丑月丁亥日",cyclicalYMD);
+	}
+
+	@Test
+	public void getCyclicalYMDTest3(){
+		//通过公历构建
+		ChineseDate chineseDate = new ChineseDate(DateUtil.parseDate("2020-08-28"));
+		String cyclicalYMD = chineseDate.getCyclicalYMD();
+		Assert.assertEquals("庚子年甲申月癸卯日",cyclicalYMD);
 	}
 }
