4df1073e0158040f052a834c885dbb329f2a3b6a
neko <52202080+akiyamaneko@users.noreply.github.com>
2020-09-23 00:09:44 +0800

    RC4特性增强

--- hutool-crypto/src/test/java/cn/hutool/crypto/test/symmetric/RC4Test.java
+++ hutool-crypto/src/test/java/cn/hutool/crypto/test/symmetric/RC4Test.java
@@ -1,9 +1,13 @@
 package cn.hutool.crypto.test.symmetric;
 
+import cn.hutool.core.util.CharsetUtil;
 import org.junit.Assert;
 import org.junit.Test;
 
 import cn.hutool.crypto.symmetric.RC4;
+
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 
 public class RC4Test {
 	
@@ -34,6 +38,37 @@ public class RC4Test {
 		String message2 = "这是第二个中文消息！";
 		byte[] crypt2 = rc4.encrypt(message2);
 		String msg2 = rc4.decrypt(crypt2);
+		Assert.assertEquals(message2, msg2);
+	}
+
+	@Test
+	public void testDecryptWithHexMessage() {
+		String message = "这是第一个用来测试密文为十六进制字符串的消息！";
+		String key = "生成一个密钥";
+		RC4 rc4 = new RC4(key);
+		String encryptHex = rc4.encryptHex(message, CharsetUtil.CHARSET_UTF_8);
+		String msg = rc4.decrypt(encryptHex);
+		Assert.assertEquals(message, msg);
+
+		String message2 = "这是第二个用来测试密文为十六进制字符串的消息！";
+		String encryptHex2 = rc4.encryptHex(message2);
+		String msg2 = rc4.decrypt(encryptHex2);
+		Assert.assertEquals(message2, msg2);
+	}
+
+
+	@Test
+	public void testDecryptWithBase64Message() {
+		String message = "这是第一个用来测试密文为Base64编码的消息！";
+		String key = "生成一个密钥";
+		RC4 rc4 = new RC4(key);
+		String encryptHex = rc4.encryptBase64(message, CharsetUtil.CHARSET_UTF_8);
+		String msg = rc4.decrypt(encryptHex);
+		Assert.assertEquals(message, msg);
+
+		String message2 = "这是第一个用来测试密文为Base64编码的消息！";
+		String encryptHex2 = rc4.encryptBase64(message2);
+		String msg2 = rc4.decrypt(encryptHex2);
 		Assert.assertEquals(message2, msg2);
 	}
 }
