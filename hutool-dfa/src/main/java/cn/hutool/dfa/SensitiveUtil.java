f1ea1da90f7dcd407fb9419bdc715f28b9850770
neko <52202080+akiyamaneko@users.noreply.github.com>
2020-09-26 23:47:31 +0800

    敏感词工具类SensitiveUtil支持自定义字符过滤规则

--- hutool-dfa/src/main/java/cn/hutool/dfa/SensitiveUtil.java
+++ hutool-dfa/src/main/java/cn/hutool/dfa/SensitiveUtil.java
@@ -73,12 +73,15 @@ public final class SensitiveUtil {
 	}
 	
 	/**
-	 * 是否包含敏感词
-	 * @param text 文本
-	 * @return 是否包含
+	 * 设置字符过滤规则，通过定义字符串过滤规则，过滤不需要的字符<br>
+	 * 当accept为false时，此字符不参与匹配
+	 *
+	 * @param charFilter 过滤函数
 	 */
-	public static boolean containsSensitive(String text){
-		return sensitiveTree.isMatch(text);
+	public static void setCharFilter(Filter<Character> charFilter) {
+		if(charFilter != null) {
+			sensitiveTree.setCharFilter(charFilter);
+		}
 	}
 	
 	/**
