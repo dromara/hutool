36588f7e07c5774b9aaa333ff0d90f304cc6f7c3
Looly <loolly@gmail.com>
2020-12-06 07:41:54 +0800

    add page of sql support

--- hutool-db/src/main/java/cn/hutool/db/AbstractDb.java
+++ hutool-db/src/main/java/cn/hutool/db/AbstractDb.java
@@ -12,6 +12,7 @@ import cn.hutool.db.sql.Condition;
 import cn.hutool.db.sql.Condition.LikeType;
 import cn.hutool.db.sql.LogicalOperator;
 import cn.hutool.db.sql.Query;
+import cn.hutool.db.sql.SqlBuilder;
 import cn.hutool.db.sql.SqlExecutor;
 import cn.hutool.db.sql.SqlUtil;
 import cn.hutool.db.sql.Wrapper;
@@ -660,11 +661,28 @@ public abstract class AbstractDb implements Serializable {
 	 * @return 复合条件的结果数
 	 * @throws SQLException SQL执行异常
 	 */
-	public int count(Entity where) throws SQLException {
+	public long count(Entity where) throws SQLException {
 		Connection conn = null;
 		try {
 			conn = this.getConnection();
 			return runner.count(conn, where);
+		} finally {
+			this.closeConnection(conn);
+		}
+	}
+
+	/**
+	 * 结果的条目数
+	 *
+	 * @param selectSql 查询SQL语句
+	 * @return 复合条件的结果数
+	 * @throws SQLException SQL执行异常
+	 */
+	public long count(CharSequence selectSql) throws SQLException {
+		Connection conn = null;
+		try {
+			conn = this.getConnection();
+			return runner.count(conn, selectSql);
 		} finally {
 			this.closeConnection(conn);
 		}
@@ -779,23 +797,57 @@ public abstract class AbstractDb implements Serializable {
 
 	/**
 	 * 分页查询<br>
+	 *
+	 * @param <T>  结果对象类型
+	 * @param sql  SQL构建器，可以使用{@link SqlBuilder#of(CharSequence)} 包装普通SQL
+	 * @param page 分页对象
+	 * @param rsh  结果集处理对象
+	 * @return 结果对象
+	 * @throws SQLException SQL执行异常
+	 * @since 5.5.3
+	 */
+	public <T> T page(CharSequence sql, Page page, RsHandler<T> rsh) throws SQLException {
+		Connection conn = null;
+		try {
+			conn = this.getConnection();
+			return runner.page(conn, SqlBuilder.of(sql), page, rsh);
+		} finally {
+			this.closeConnection(conn);
+		}
+	}
+
+	/**
+	 * 分页查询
+	 *
+	 * @param sql  SQL语句字符串
+	 * @param page 分页对象
+	 * @return 结果对象
+	 * @throws SQLException SQL执行异常
+	 * @since 5.5.3
+	 */
+	public PageResult<Entity> page(CharSequence sql, Page page) throws SQLException {
+		Connection conn = null;
+		try {
+			conn = this.getConnection();
+			return runner.page(conn, SqlBuilder.of(sql), page);
+		} finally {
+			this.closeConnection(conn);
+		}
+	}
+
+	/**
+	 * 分页查询<br>
 	 * 查询条件为多个key value对表示，默认key = value，如果使用其它条件可以使用：where.put("key", " &gt; 1")，value也可以传Condition对象，key被忽略
 	 *
 	 * @param fields     返回的字段列表，null则返回所有字段
 	 * @param where      条件实体类（包含表名）
-	 * @param page       分页对象
-	 * @param numPerPage 每页条目数
+	 * @param pageNumber 页码
+	 * @param pageSize   每页结果数
 	 * @return 结果对象
 	 * @throws SQLException SQL执行异常
 	 */
-	public PageResult<Entity> page(Collection<String> fields, Entity where, int page, int numPerPage) throws SQLException {
-		Connection conn = null;
-		try {
-			conn = this.getConnection();
-			return runner.page(conn, fields, where, page, numPerPage);
-		} finally {
-			this.closeConnection(conn);
-		}
+	public PageResult<Entity> page(Collection<String> fields, Entity where, int pageNumber, int pageSize) throws SQLException {
+		return page(fields, where, new Page(pageNumber, pageSize));
 	}
 
 	/**
