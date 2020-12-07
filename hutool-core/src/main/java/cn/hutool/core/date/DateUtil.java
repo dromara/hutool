04a3250708f70383c054da9ddabccf0664c23b21
Looly <loolly@gmail.com>
2020-12-07 19:13:46 +0800

    fix code

--- hutool-core/src/main/java/cn/hutool/core/date/DateUtil.java
+++ hutool-core/src/main/java/cn/hutool/core/date/DateUtil.java
@@ -1339,8 +1339,8 @@ public class DateUtil extends CalendarUtil {
 	 * <pre>
 	 * 有时候我们计算相差天数的时候需要忽略时分秒。
 	 * 比如：2016-02-01 23:59:59和2016-02-02 00:00:00相差一秒
-	 * 如果isReset为<code>false</code>相差天数为0。
-	 * 如果isReset为<code>true</code>相差天数将被计算为1
+	 * 如果isReset为{@code false}相差天数为0。
+	 * 如果isReset为{@code true}相差天数将被计算为1
 	 * </pre>
 	 *
 	 * @param beginDate 起始日期
