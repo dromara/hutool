ed520134eece522a192b73ff58ab1902fa02c934
totalo <totalo@qq.com>
2021-02-19 17:35:36 +0800

    feat:增加数字缩写转换

--- hutool-core/src/test/java/cn/hutool/core/convert/NumberWordFormatTest.java
+++ hutool-core/src/test/java/cn/hutool/core/convert/NumberWordFormatTest.java
@@ -12,5 +12,14 @@ public class NumberWordFormatTest {
 		
 		String format2 = NumberWordFormatter.format("2100.00");
 		Assert.assertEquals("TWO THOUSAND ONE HUNDRED AND CENTS  ONLY", format2);
+
+		String format3 = NumberWordFormatter.formatValue(4384324, false);
+		Assert.assertEquals("4.38m", format3);
+
+		String format4 = NumberWordFormatter.formatValue(4384324);
+		Assert.assertEquals("438.43w", format4);
+
+		String format5 = NumberWordFormatter.formatValue(438);
+		Assert.assertEquals("438", format5);
 	}
 }
