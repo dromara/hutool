8097cc710e2905ba056ac0cd215d0203fe7e0b87
Looly <loolly@gmail.com>
2021-03-05 15:50:19 +0800

    fix comment

--- hutool-json/src/main/java/cn/hutool/json/JSONNull.java
+++ hutool-json/src/main/java/cn/hutool/json/JSONNull.java
@@ -5,8 +5,8 @@ import cn.hutool.core.util.StrUtil;
 import java.io.Serializable;
 
 /**
- * 用于定义<code>null</code>，与Javascript中null相对应<br>
- * Java中的<code>null</code>值在js中表示为undefined。
+ * 用于定义{@code null}，与Javascript中null相对应<br>
+ * Java中的{@code null}值在js中表示为undefined。
  * @author Looly
  *
  */
@@ -14,15 +14,15 @@ public class JSONNull implements Serializable{
 	private static final long serialVersionUID = 2633815155870764938L;
 
 	/**
-	 * <code>NULL</code> 对象用于减少歧义来表示Java 中的<code>null</code> <br>
-	 * <code>NULL.equals(null)</code> 返回 <code>true</code>. <br>
-	 * <code>NULL.toString()</code> 返回 <code>"null"</code>.
+	 * {@code NULL} 对象用于减少歧义来表示Java 中的{@code null} <br>
+	 * {@code NULL.equals(null)} 返回 {@code true}. <br>
+	 * {@code NULL.toString()} 返回 {@code "null"}.
 	 */
 	public static final JSONNull NULL = new JSONNull();
 
 	/**
 	 * A Null object is equal to the null value and to itself.
-	 * 对象与其本身和<code>null</code>值相等
+	 * 对象与其本身和{@code null}值相等
 	 *
 	 * @param object An object to test for nullness.
 	 * @return true if the object parameter is the JSONObject.NULL object or null.
@@ -32,7 +32,7 @@ public class JSONNull implements Serializable{
 	public boolean equals(Object object) {
 		return object == null || (object == this);
 	}
-	
+
 	/**
 	 * Get the "null" string value.
 	 *获得“null”字符串
