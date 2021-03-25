fc4a644e35d37b71a5aa7ef6828e9e35b137e5fe
Looly <loolly@gmail.com>
2021-03-25 17:14:54 +0800

    fix comment

--- hutool-extra/src/main/java/cn/hutool/extra/qrcode/QrCodeUtil.java
+++ hutool-extra/src/main/java/cn/hutool/extra/qrcode/QrCodeUtil.java
@@ -320,7 +320,7 @@ public class QrCodeUtil {
 	// ------------------------------------------------------------------------------------------------------------------- decode
 
 	/**
-	 * 解码二维码图片为文本
+	 * 解码二维码或条形码图片为文本
 	 *
 	 * @param qrCodeInputstream 二维码输入流
 	 * @return 解码文本
@@ -330,7 +330,7 @@ public class QrCodeUtil {
 	}
 
 	/**
-	 * 解码二维码图片为文本
+	 * 解码二维码或条形码图片为文本
 	 *
 	 * @param qrCodeFile 二维码文件
 	 * @return 解码文本
@@ -340,7 +340,7 @@ public class QrCodeUtil {
 	}
 
 	/**
-	 * 将二维码图片解码为文本
+	 * 将二维码或条形码图片解码为文本
 	 *
 	 * @param image {@link Image} 二维码图片
 	 * @return 解码后的文本
@@ -350,7 +350,7 @@ public class QrCodeUtil {
 	}
 
 	/**
-	 * 将二维码图片解码为文本
+	 * 将二维码或条形码图片解码为文本
 	 *
 	 * @param image         {@link Image} 二维码图片
 	 * @param isTryHarder   是否优化精度
