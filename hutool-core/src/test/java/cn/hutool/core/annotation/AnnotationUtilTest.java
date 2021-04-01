1542b0277190c77b4b7d81de8ed59db38a9caab5
Looly <loolly@gmail.com>
2021-04-01 09:51:10 +0800

    add test

--- hutool-core/src/test/java/cn/hutool/core/annotation/AnnotationUtilTest.java
+++ hutool-core/src/test/java/cn/hutool/core/annotation/AnnotationUtilTest.java
@@ -4,15 +4,18 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class AnnotationUtilTest {
-	
+
 	@Test
 	public void getAnnotationValueTest() {
 		Object value = AnnotationUtil.getAnnotationValue(ClassWithAnnotation.class, AnnotationForTest.class);
 		Assert.assertEquals("测试", value);
+
 	}
-	
+
 	@AnnotationForTest("测试")
 	static class ClassWithAnnotation{
-		
+		public void test(){
+
+		}
 	}
 }
