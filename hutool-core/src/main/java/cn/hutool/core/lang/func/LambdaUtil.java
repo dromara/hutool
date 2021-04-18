401a6dfeba0f72f051bfee4c328451ef25e2602e
zak <lanjingl@outlook.com>
2021-04-18 15:53:37 +0800

    1. 增加从lambda表达式方法中获取字段名 2. 增加通过泛型使受检异常绕过编译检查

--- hutool-core/src/main/java/cn/hutool/core/lang/func/LambdaUtil.java
+++ hutool-core/src/main/java/cn/hutool/core/lang/func/LambdaUtil.java
@@ -3,13 +3,14 @@ package cn.hutool.core.lang.func;
 import cn.hutool.core.lang.SimpleCache;
 import cn.hutool.core.util.ReflectUtil;
 
+import java.beans.Introspector;
 import java.io.Serializable;
 import java.lang.invoke.SerializedLambda;
 
 /**
  * Lambda相关工具类
  *
- * @author looly, Scen
+ * @author looly, Scen, zak
  * @since 5.6.3
  */
 public class LambdaUtil {
@@ -40,6 +41,24 @@ public class LambdaUtil {
 	}
 
 	/**
+	 * 当lambda表达式函数（方法）是一个get方法时，获取字段名称
+	 *
+	 * @param <T> Lambda类型
+	 * @param func 函数（无参方法）
+	 * @return 字段名称
+	 */
+	public static <T> String getFieldName(Func1<T, ?> func) {
+		String methodName = getMethodName(func);
+		if (methodName.startsWith("get")) {
+			return Introspector.decapitalize(methodName.substring(3));
+		}
+		if (methodName.startsWith("is")) {
+			return Introspector.decapitalize(methodName.substring(2));
+		}
+		throw new IllegalArgumentException("lambda表达式方法不是一个get方法");
+	}
+
+	/**
 	 * 解析lambda表达式,加了缓存。
 	 * 该缓存可能会在任意不定的时间被清除
 	 *
@@ -48,5 +67,60 @@ public class LambdaUtil {
 	 */
 	private static <T> SerializedLambda _resolve(Serializable func) {
 		return cache.get(func.getClass().getName(), () -> ReflectUtil.invoke(func, "writeReplace"));
+	}
+
+
+	/**
+	 * 将受检异常通过泛型绕过编译检测
+	 */
+	public static <R, E extends Throwable> R raise(Throwable e) throws E {
+		throw (E) e;
+	}
+
+
+	/**
+	 * 将受检异常通过泛型绕过编译检测
+	 *
+	 * @param raiseWrapper 异常代码包装
+	 * @param <T> 返回值类型
+	 * @return 返回值
+	 */
+	public static <T> T raise(RaiseWrapper<T> raiseWrapper) {
+		try {
+			return raiseWrapper.get();
+		} catch (Throwable throwable) {
+			return raise(throwable);
+		}
+	}
+
+	/**
+	 * 将受检异常通过泛型绕过编译检测
+	 *
+	 * @param raiseWrapper 异常代码包装
+	 */
+	public static void raise(RaiseWrapperNoReturn raiseWrapper) {
+		try {
+			raiseWrapper.get();
+		} catch (Throwable throwable) {
+			raise(throwable);
+		}
+	}
+
+
+
+	/**
+	 * 异常代码包装，有返回值
+	 */
+	@FunctionalInterface
+	public interface RaiseWrapper<T> {
+		T get() throws Throwable;
+	}
+
+	/**
+	 * 异常代码包装，没有返回值
+	 */
+	@FunctionalInterface
+	public interface RaiseWrapperNoReturn {
+		void get() throws Throwable;
 	}
 }
--- hutool-core/src/test/java/cn/hutool/core/lang/func/LambdaUtilTest.java
+++ hutool-core/src/test/java/cn/hutool/core/lang/func/LambdaUtilTest.java
@@ -4,6 +4,13 @@ import lombok.Data;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class LambdaUtilTest {
 
 	@Test
@@ -12,8 +19,64 @@ public class LambdaUtilTest {
 		Assert.assertEquals("getAge", methodName);
 	}
 
+	@Test
+	public void getFieldNameTest() {
+		String fieldName1 = LambdaUtil.getFieldName(MyTeacher::getAge);
+		Assert.assertEquals("age", fieldName1);
+
+		String fieldName2 = LambdaUtil.getFieldName(MyTeacher::isVip);
+		Assert.assertEquals("vip", fieldName2);
+	}
+
+	@Test
+	public void raiseTest() {
+		Assert.assertThrows("受检异常", Exception.class, () -> LambdaUtil.raise(new Exception("受检异常")));
+	}
+
+
+	@Test
+	public void raiseWrapperTest() {
+		File[] files = new File[]{new File("/test.txt"), new File("/test2.txt")};
+
+		Assert.assertThrows("文件不存在", FileNotFoundException.class, () -> {
+			// 在lambda表达式中绕过了对受检异常的处理
+			List<String> contents = Arrays.stream(files)
+					.map(file -> LambdaUtil.raise(() -> LambdaUtilTest.this.readFile(file)))
+					.collect(Collectors.toList());
+
+		});
+	}
+
+
+	@Test
+	public void raiseWrapperNoReturnTest() {
+		File[] files = new File[]{new File("/test.txt"), new File("/test2.txt")};
+
+		Assert.assertThrows("文件不存在", FileNotFoundException.class, () -> {
+			// 在lambda表达式中绕过了对受检异常的处理
+			Arrays.stream(files).forEach(file -> LambdaUtil.raise(() -> LambdaUtilTest.this.saveFile(file)));
+		});
+	}
+
+
+
 	@Data
 	static class MyTeacher{
 		public String age;
+		public boolean vip;
+	}
+
+	/**
+	 * 有返回值的会抛受检异常的方法
+	 */
+	private String readFile(File file) throws IOException {
+		throw new FileNotFoundException("文件不存在");
+	}
+
+	/**
+	 * 没有返回值的会抛受检异常的方法
+	 */
+	private void saveFile(File file) throws IOException {
+		throw new FileNotFoundException("文件不存在");
 	}
 }
