405208a5dec0742ab946424d6fa11b6e15d9c16b
Looly <loolly@gmail.com>
2021-05-08 11:07:28 +0800

    fix Bcrypt bug

--- hutool-crypto/src/main/java/cn/hutool/crypto/digest/BCrypt.java
+++ hutool-crypto/src/main/java/cn/hutool/crypto/digest/BCrypt.java
@@ -424,7 +424,9 @@ public class BCrypt {
 			off = 3;
 		else {
 			minor = salt.charAt(2);
-			if (minor != 'a' || salt.charAt(3) != '$')
+			// pr#1560@Github
+			// 修正一个在Blowfish实现上的安全风险
+			if ((minor != 'a' && minor != 'x' && minor != 'y' && minor != 'b') || salt.charAt(3) != '$')
 				throw new IllegalArgumentException("Invalid salt revision");
 			off = 4;
 		}
