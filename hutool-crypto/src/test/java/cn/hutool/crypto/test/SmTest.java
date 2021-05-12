389ce1d27ee1f3a7f737d50c4cdfc107b570e46e
Looly <loolly@gmail.com>
2021-05-13 07:34:29 +0800

    fix test

--- hutool-crypto/src/test/java/cn/hutool/crypto/test/SmTest.java
+++ hutool-crypto/src/test/java/cn/hutool/crypto/test/SmTest.java
@@ -14,18 +14,18 @@ import javax.crypto.SecretKey;
 
 /**
  * SM单元测试
- * 
+ *
  * @author looly
  *
  */
 public class SmTest {
-	
+
 	@Test
 	public void sm3Test() {
 		String digestHex = SmUtil.sm3("aaaaa");
 		Assert.assertEquals("136ce3c86e4ed909b76082055a61586af20b4dab674732ebd4b599eef080c9be", digestHex);
 	}
-	
+
 	@Test
 	public void sm4Test() {
 		String content = "test中文";
@@ -73,7 +73,7 @@ public class SmTest {
 		String decryptStr = sm4.decryptStr(encryptHex, CharsetUtil.CHARSET_UTF_8);
 		Assert.assertEquals(content, decryptStr);
 	}
-	
+
 	@Test
 	public void hmacSm3Test() {
 		String content = "test中文";
