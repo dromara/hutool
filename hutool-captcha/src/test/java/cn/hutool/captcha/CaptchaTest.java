6aba52fda8e913ee3a10295be308fbdc4d3ede5b
Looly <loolly@gmail.com>
2021-06-15 16:28:24 +0800

    fix test

--- hutool-captcha/src/test/java/cn/hutool/captcha/CaptchaTest.java
+++ hutool-captcha/src/test/java/cn/hutool/captcha/CaptchaTest.java
@@ -77,13 +77,25 @@ public class CaptchaTest {
 
 	@Test
 	@Ignore
-	public void ShearCaptchaTest() {
+	public void shearCaptchaTest() {
 
 		// 定义图形验证码的长和宽
 		ShearCaptcha captcha = CaptchaUtil.createShearCaptcha(203, 100, 4, 4);
 		// ShearCaptcha captcha = new ShearCaptcha(200, 100, 4, 4);
 		// 图形验证码写出，可以写出到文件，也可以写出到流
 		captcha.write("f:/captcha/shear.png");
+		// 验证图形验证码的有效性，返回boolean值
+		captcha.verify("1234");
+	}
+
+	@Test
+	@Ignore
+	public void shearCaptchaTest2() {
+
+		// 定义图形验证码的长和宽
+		ShearCaptcha captcha = new ShearCaptcha(200, 100, 4, 4);
+		// 图形验证码写出，可以写出到文件，也可以写出到流
+		captcha.write("d:/test/shear.png");
 		// 验证图形验证码的有效性，返回boolean值
 		captcha.verify("1234");
 	}
