185764ec75cbe198044eb8a1fe74224575829539
Looly <loolly@gmail.com>
2021-06-18 01:56:06 +0800

    add test

--- hutool-core/src/test/java/cn/hutool/core/text/StrJoinerTest.java
+++ hutool-core/src/test/java/cn/hutool/core/text/StrJoinerTest.java
@@ -1,5 +1,7 @@
 package cn.hutool.core.text;
 
+import cn.hutool.core.collection.CollUtil;
+import cn.hutool.core.collection.ListUtil;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -26,5 +28,54 @@ public class StrJoinerTest {
 	public void noJoinTest(){
 		final StrJoiner append = StrJoiner.of(",");
 		Assert.assertEquals("", append.toString());
+	}
+
+	@Test
+	public void joinMultiArrayTest(){
+		final StrJoiner append = StrJoiner.of(",");
+		append.append(new Object[]{ListUtil.of("1", "2"),
+				CollUtil.newHashSet("3", "4")
+		});
+		Assert.assertEquals("1,2,3,4", append.toString());
+	}
+
+	@Test
+	public void joinNullModeTest(){
+		StrJoiner append = StrJoiner.of(",")
+				.setNullMode(StrJoiner.NullMode.IGNORE)
+				.append("1")
+				.append((Object)null)
+				.append("3");
+		Assert.assertEquals("1,3", append.toString());
+
+		append = StrJoiner.of(",")
+				.setNullMode(StrJoiner.NullMode.TO_EMPTY)
+				.append("1")
+				.append((Object)null)
+				.append("3");
+		Assert.assertEquals("1,,3", append.toString());
+
+		append = StrJoiner.of(",")
+				.setNullMode(StrJoiner.NullMode.NULL_STRING)
+				.append("1")
+				.append((Object)null)
+				.append("3");
+		Assert.assertEquals("1,null,3", append.toString());
+	}
+
+	@Test
+	public void joinWrapTest(){
+		StrJoiner append = StrJoiner.of(",", "[", "]")
+				.append("1")
+				.append("2")
+				.append("3");
+		Assert.assertEquals("[1,2,3]", append.toString());
+
+		append = StrJoiner.of(",", "[", "]")
+				.setWrapElement(true)
+				.append("1")
+				.append("2")
+				.append("3");
+		Assert.assertEquals("[1],[2],[3]", append.toString());
 	}
 }
