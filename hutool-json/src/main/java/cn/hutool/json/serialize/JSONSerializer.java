8b0357da189681a5fde0db36d434a7cdc5e360df
Looly <loolly@gmail.com>
2021-06-26 09:37:16 +0800

    change code

--- hutool-json/src/main/java/cn/hutool/json/serialize/JSONSerializer.java
+++ hutool-json/src/main/java/cn/hutool/json/serialize/JSONSerializer.java
@@ -4,7 +4,7 @@ import cn.hutool.json.JSON;
 
 /**
  * 序列化接口，通过实现此接口，实现自定义的对象转换为JSON的操作
- * 
+ *
  * @param <T> JSON类型，可以是JSONObject或者JSONArray
  * @param <V> 对象类型
  * @author Looly
@@ -15,7 +15,7 @@ public interface JSONSerializer<T extends JSON, V> {
 	/**
 	 * 序列化实现，通过实现此方法，将指定类型的对象转换为{@link JSON}对象<br>
 	 * 转换后的对象可以为JSONObject也可以为JSONArray，首先new一个空的JSON，然后将需要的数据字段put到JSON对象中去即可。
-	 * 
+	 *
 	 * @param json JSON，可以为JSONObject或者JSONArray
 	 * @param bean 指定类型对象
 	 */
