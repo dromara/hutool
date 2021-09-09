6b8adc8a909308682108aacc14da9abae1ce8c05
Looly <loolly@gmail.com>
2021-09-09 18:13:29 +0800

    add chacha20

--- hutool-crypto/src/test/java/cn/hutool/crypto/test/symmetric/ChaCha20Test.java
+++ hutool-crypto/src/test/java/cn/hutool/crypto/test/symmetric/ChaCha20Test.java
@@ -2,12 +2,9 @@ package cn.hutool.crypto.test.symmetric;
 
 import cn.hutool.core.util.CharsetUtil;
 import cn.hutool.core.util.RandomUtil;
-import cn.hutool.crypto.KeyUtil;
-import cn.hutool.crypto.symmetric.SymmetricCrypto;
+import cn.hutool.crypto.symmetric.ChaCha20;
 import org.junit.Assert;
 import org.junit.Test;
-
-import javax.crypto.spec.IvParameterSpec;
 
 /**
  * 见：https://stackoverflow.com/questions/32672241/using-bouncycastles-chacha-for-file-encryption
@@ -16,18 +13,14 @@ public class ChaCha20Test {
 
 	@Test
 	public void encryptAndDecryptTest() {
-		String content = "test中文";
-
 		// 32 for 256 bit key or 16 for 128 bit
 		byte[] key = RandomUtil.randomBytes(32);
 		// 64 bit IV required by ChaCha20
 		byte[] iv = RandomUtil.randomBytes(12);
 
-		final SymmetricCrypto chacha = new SymmetricCrypto("ChaCha20",
-				KeyUtil.generateKey("ChaCha20", key),
-				new IvParameterSpec(iv)
-		);
+		final ChaCha20 chacha = new ChaCha20(key, iv);
 
+		String content = "test中文";
 		// 加密为16进制表示
 		String encryptHex = chacha.encryptHex(content);
 		// 解密为字符串
