ed85f81cea2e6eed31678f995866734c5cecfb8e
Looly <loolly@gmail.com>
2021-09-27 22:36:14 +0800

    fix test

--- hutool-core/src/test/java/cn/hutool/core/util/JAXBUtilTest.java
+++ hutool-core/src/test/java/cn/hutool/core/util/JAXBUtilTest.java
@@ -50,7 +50,7 @@ public class JAXBUtilTest {
 
 	@XmlRootElement(name = "school")
 	@XmlAccessorType(XmlAccessType.FIELD)
-	@XmlType(propOrder={"schoolName", "school_address", "room"})
+	@XmlType(propOrder={"schoolName", "schoolAddress", "room"})
 	public static class SchoolVo {
 		@XmlElement(name = "school_name", required = true)
 		private String schoolName;
@@ -87,7 +87,7 @@ public class JAXBUtilTest {
 		}
 
 		@XmlAccessorType(XmlAccessType.FIELD)
-		@XmlType(propOrder={"room_no", "room_name"})
+		@XmlType(propOrder={"roomNo", "roomName"})
 		public static final class RoomVo {
 			@XmlElement(name = "room_no", required = true)
 			private String roomNo;
