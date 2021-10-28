ccc9109197050f5ba2fe4c184fe4db37bbebfaf9
Jice Gao <jice.gao@jiahui.com>
2021-10-28 09:46:12 +0800

    修正cidr判定问题

--- hutool-core/src/main/java/cn/hutool/core/net/NetUtil.java
+++ hutool-core/src/main/java/cn/hutool/core/net/NetUtil.java
@@ -702,7 +702,7 @@ public class NetUtil {
 		String[] ips = StrUtil.splitToArray(ip, '.');
 		int ipAddr = (Integer.parseInt(ips[0]) << 24) | (Integer.parseInt(ips[1]) << 16) | (Integer.parseInt(ips[2]) << 8) | Integer.parseInt(ips[3]);
 		int type = Integer.parseInt(cidr.replaceAll(".*/", ""));
-		int mask = 0xFFFFFFFF << (32 - type);
+		int mask = (int)((-1L << 32 - type) & 0XFFFFFFFF);
 		String cidrIp = cidr.replaceAll("/.*", "");
 		String[] cidrIps = cidrIp.split("\\.");
 		int cidrIpAddr = (Integer.parseInt(cidrIps[0]) << 24) | (Integer.parseInt(cidrIps[1]) << 16) | (Integer.parseInt(cidrIps[2]) << 8) | Integer.parseInt(cidrIps[3]);
