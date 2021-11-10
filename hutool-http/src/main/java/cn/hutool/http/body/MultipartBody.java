f9e5625744433805a8417c40b27c892c953a0627
Looly <loolly@gmail.com>
2021-11-11 01:19:53 +0800

    add HttpResource

--- hutool-http/src/main/java/cn/hutool/http/body/MultipartBody.java
+++ hutool-http/src/main/java/cn/hutool/http/body/MultipartBody.java
@@ -5,31 +5,33 @@ import cn.hutool.core.io.IoUtil;
 import cn.hutool.core.io.resource.MultiResource;
 import cn.hutool.core.io.resource.Resource;
 import cn.hutool.core.map.MapUtil;
-import cn.hutool.core.util.ObjectUtil;
 import cn.hutool.core.util.RandomUtil;
 import cn.hutool.core.util.StrUtil;
 import cn.hutool.http.ContentType;
+import cn.hutool.http.HttpResource;
 import cn.hutool.http.HttpUtil;
 
+import java.io.ByteArrayOutputStream;
 import java.io.OutputStream;
 import java.nio.charset.Charset;
 import java.util.Map;
 
 /**
- * Multipart/form-data数据的请求体封装
+ * Multipart/form-data数据的请求体封装<br>
+ * 遵循RFC2388规范
  *
  * @author looly
  * @since 5.3.5
  */
-public class MultipartBody implements RequestBody{
+public class MultipartBody implements RequestBody {
 
 	private static final String BOUNDARY = "--------------------Hutool_" + RandomUtil.randomString(16);
 	private static final String BOUNDARY_END = StrUtil.format("--{}--\r\n", BOUNDARY);
-	private static final String CONTENT_DISPOSITION_TEMPLATE = "Content-Disposition: form-data; name=\"{}\"\r\n\r\n";
+	private static final String CONTENT_DISPOSITION_TEMPLATE = "Content-Disposition: form-data; name=\"{}\"\r\n";
 	private static final String CONTENT_DISPOSITION_FILE_TEMPLATE = "Content-Disposition: form-data; name=\"{}\"; filename=\"{}\"\r\n";
 
 	private static final String CONTENT_TYPE_MULTIPART_PREFIX = ContentType.MULTIPART.getValue() + "; boundary=";
-	private static final String CONTENT_TYPE_FILE_TEMPLATE = "Content-Type: {}\r\n\r\n";
+	private static final String CONTENT_TYPE_FILE_TEMPLATE = "Content-Type: {}\r\n";
 
 	/**
 	 * 存储表单数据
@@ -42,11 +44,12 @@ public class MultipartBody implements RequestBody{
 
 	/**
 	 * 根据已有表单内容，构建MultipartBody
-	 * @param form 表单
+	 *
+	 * @param form    表单
 	 * @param charset 编码
 	 * @return MultipartBody
 	 */
-	public static MultipartBody create(Map<String, Object> form, Charset charset){
+	public static MultipartBody create(Map<String, Object> form, Charset charset) {
 		return new MultipartBody(form, charset);
 	}
 
@@ -55,15 +58,15 @@ public class MultipartBody implements RequestBody{
 	 *
 	 * @return Multipart的Content-Type类型
 	 */
-	public static String getContentType(){
+	public static String getContentType() {
 		return CONTENT_TYPE_MULTIPART_PREFIX + BOUNDARY;
 	}
 
 	/**
 	 * 构造
 	 *
-	 * @param form     表单
-	 * @param charset  编码
+	 * @param form    表单
+	 * @param charset 编码
 	 */
 	public MultipartBody(Map<String, Object> form, Charset charset) {
 		this.form = form;
@@ -79,6 +82,13 @@ public class MultipartBody implements RequestBody{
 	public void write(OutputStream out) {
 		writeForm(out);
 		formEnd(out);
+	}
+
+	@Override
+	public String toString() {
+		final ByteArrayOutputStream out = new ByteArrayOutputStream();
+		write(out);
+		return IoUtil.toStr(out, this.charset);
 	}
 
 	// 普通字符串数据
@@ -97,10 +107,26 @@ public class MultipartBody implements RequestBody{
 	}
 
 	/**
-	 * 添加Multipart表单的数据项
+	 * 添加Multipart表单的数据项<br>
+	 * <pre>
+	 *     --分隔符(boundary)[换行]
+	 *     Content-Disposition: form-data; name="参数名"[换行]
+	 *     [换行]
+	 *     参数值[换行]
+	 * </pre>
+	 *
+	 * 或者：
+	 *
+	 * <pre>
+	 *     --分隔符(boundary)[换行]
+	 *     Content-Disposition: form-data; name="表单名"; filename="文件名"[换行]
+	 *     Content-Type: MIME类型[换行]
+	 *     [换行]
+	 *     文件的二进制内容[换行]
+	 * </pre>
 	 *
 	 * @param formFieldName 表单名
-	 * @param value      值，可以是普通值、资源（如文件等）
+	 * @param value         值，可以是普通值、资源（如文件等）
 	 * @param out           Http流
 	 * @throws IORuntimeException IO异常
 	 */
@@ -113,23 +139,61 @@ public class MultipartBody implements RequestBody{
 			return;
 		}
 
+		// --分隔符(boundary)[换行]
 		write(out, "--", BOUNDARY, StrUtil.CRLF);
 
-		if(value instanceof Resource){
-			// 文件资源（二进制资源）
-			final Resource resource = (Resource)value;
-			final String fileName = resource.getName();
-			write(out, StrUtil.format(CONTENT_DISPOSITION_FILE_TEMPLATE, formFieldName, ObjectUtil.defaultIfNull(fileName, formFieldName)));
-			// 根据name的扩展名指定互联网媒体类型，默认二进制流数据
-			write(out, StrUtil.format(CONTENT_TYPE_FILE_TEMPLATE, HttpUtil.getMimeType(fileName, "application/octet-stream")));
-			resource.writeTo(out);
-		} else{
-			// 普通数据
+		if (value instanceof Resource) {
+			appendResource(formFieldName, (Resource) value, out);
+		} else {
+			/*
+			 * Content-Disposition: form-data; name="参数名"[换行]
+			 * [换行]
+			 * 参数值
+			 */
 			write(out, StrUtil.format(CONTENT_DISPOSITION_TEMPLATE, formFieldName));
+			write(out, StrUtil.CRLF);
 			write(out, value);
 		}
 
 		write(out, StrUtil.CRLF);
+	}
+
+	/**
+	 * 添加Multipart表单的Resource数据项，支持包括{@link HttpResource}资源格式
+	 *
+	 * @param formFieldName 表单名
+	 * @param resource      资源
+	 * @param out           Http流
+	 * @throws IORuntimeException IO异常
+	 */
+	private void appendResource(String formFieldName, Resource resource, OutputStream out) throws IORuntimeException {
+		final String fileName = resource.getName();
+
+		// Content-Disposition
+		if (null == fileName) {
+			// Content-Disposition: form-data; name="参数名"[换行]
+			write(out, StrUtil.format(CONTENT_DISPOSITION_TEMPLATE, formFieldName));
+		} else {
+			// Content-Disposition: form-data; name="参数名"; filename="文件名"[换行]
+			write(out, StrUtil.format(CONTENT_DISPOSITION_FILE_TEMPLATE, formFieldName, fileName));
+		}
+
+		// Content-Type
+		if (resource instanceof HttpResource) {
+			final String contentType = ((HttpResource) resource).getContentType();
+			if (StrUtil.isNotBlank(contentType)) {
+				// Content-Type: 类型[换行]
+				write(out, StrUtil.format(CONTENT_TYPE_FILE_TEMPLATE, contentType));
+			}
+		} else {
+			// 根据name的扩展名指定互联网媒体类型，默认二进制流数据
+			write(out, StrUtil.format(CONTENT_TYPE_FILE_TEMPLATE,
+					HttpUtil.getMimeType(fileName, ContentType.OCTET_STREAM.getValue())));
+		}
+
+		// 内容
+		write(out, "\r\n");
+		resource.writeTo(out);
 	}
 
 	/**
