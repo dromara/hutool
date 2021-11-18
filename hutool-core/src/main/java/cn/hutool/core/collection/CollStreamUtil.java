8b3f34674288db51d6b7462e53ebe562a0457388
VampireAchao <achao1441470436@gmail.com>
2021-11-18 17:33:46 +0800

    添加CollStreamUtil并行流相关实现

--- hutool-core/src/main/java/cn/hutool/core/collection/CollStreamUtil.java
+++ hutool-core/src/main/java/cn/hutool/core/collection/CollStreamUtil.java
@@ -33,6 +33,29 @@ public class CollStreamUtil {
 		return collection.stream().collect(Collectors.toMap(key, Function.identity(), (l, r) -> l));
 	}
 
+
+	/**
+	 * 将collection转化为类型不变的map<br>
+	 * <B>{@code Collection<V>  ---->  Map<K,V>}</B>
+	 *
+	 * @param collection 需要转化的集合
+	 * @param key        V类型转化为K类型的lambda方法
+	 * @param isParallel 是否并行流
+	 * @param <V>        collection中的泛型
+	 * @param <K>        map中的key类型
+	 * @return 转化后的map
+	 * @see #toIdentityMap toIdentityMap的并行流实现
+	 */
+	public static <V, K> Map<K, V> toIdentityMap(Collection<V> collection, Function<V, K> key, boolean isParallel) {
+		if (false == isParallel) {
+			return toIdentityMap(collection, key);
+		}
+		if (CollUtil.isEmpty(collection)) {
+			return Collections.emptyMap();
+		}
+		return collection.parallelStream().collect(Collectors.toMap(key, Function.identity(), (l, r) -> l));
+	}
+
 	/**
 	 * 将Collection转化为map(value类型与collection的泛型不同)<br>
 	 * <B>{@code Collection<E> -----> Map<K,V>  }</B>
@@ -51,6 +74,30 @@ public class CollStreamUtil {
 		}
 		return collection.stream().collect(Collectors.toMap(key, value, (l, r) -> l));
 	}
+
+	/**
+	 * @param collection 需要转化的集合
+	 * @param key        E类型转化为K类型的lambda方法
+	 * @param value      E类型转化为V类型的lambda方法
+	 * @param isParallel 是否并行流
+	 * @param <E>        collection中的泛型
+	 * @param <K>        map中的key类型
+	 * @param <V>        map中的value类型
+	 * @return 转化后的map
+	 * @see #toMap toMap的并行流实现
+	 * 将Collection转化为map(value类型与collection的泛型不同)<br>
+	 * <B>{@code Collection<E> -----> Map<K,V>  }</B>
+	 */
+	public static <E, K, V> Map<K, V> toMap(Collection<E> collection, Function<E, K> key, Function<E, V> value, boolean isParallel) {
+		if (false == isParallel) {
+			return toMap(collection, key, value);
+		}
+		if (CollUtil.isEmpty(collection)) {
+			return Collections.emptyMap();
+		}
+		return collection.parallelStream().collect(Collectors.toMap(key, value, (l, r) -> l));
+	}
+
 
 	/**
 	 * 将collection按照规则(比如有相同的班级id)分类成map<br>
@@ -72,6 +119,30 @@ public class CollStreamUtil {
 	}
 
 	/**
+	 * 将collection按照规则(比如有相同的班级id)分类成map<br>
+	 * <B>{@code Collection<E> -------> Map<K,List<E>> } </B>
+	 *
+	 * @param collection 需要分类的集合
+	 * @param key        分类的规则
+	 * @param isParallel 是否并行流
+	 * @param <E>        collection中的泛型
+	 * @param <K>        map中的key类型
+	 * @return 分类后的map
+	 * @see #groupByKey groupByKey的并行流实现
+	 */
+	public static <E, K> Map<K, List<E>> groupByKey(Collection<E> collection, Function<E, K> key, boolean isParallel) {
+		if (false == isParallel) {
+			return groupByKey(collection, key);
+		}
+		if (CollUtil.isEmpty(collection)) {
+			return Collections.emptyMap();
+		}
+		return collection
+				.parallelStream()
+				.collect(Collectors.groupingBy(key, Collectors.toList()));
+	}
+
+	/**
 	 * 将collection按照两个规则(比如有相同的年级id,班级id)分类成双层map<br>
 	 * <B>{@code Collection<E>  --->  Map<T,Map<U,List<E>>> } </B>
 	 *
@@ -89,6 +160,33 @@ public class CollStreamUtil {
 		}
 		return collection
 				.stream()
+				.collect(Collectors.groupingBy(key1, Collectors.groupingBy(key2, Collectors.toList())));
+	}
+
+
+	/**
+	 * 将collection按照两个规则(比如有相同的年级id,班级id)分类成双层map<br>
+	 * <B>{@code Collection<E>  --->  Map<T,Map<U,List<E>>> } </B>
+	 *
+	 * @param collection 需要分类的集合
+	 * @param key1       第一个分类的规则
+	 * @param key2       第二个分类的规则
+	 * @param isParallel 是否并行流
+	 * @param <E>        集合元素类型
+	 * @param <K>        第一个map中的key类型
+	 * @param <U>        第二个map中的key类型
+	 * @return 分类后的map
+	 * @see #groupBy2Key    groupBy2Key的并行实现
+	 */
+	public static <E, K, U> Map<K, Map<U, List<E>>> groupBy2Key(Collection<E> collection, Function<E, K> key1, Function<E, U> key2, boolean isParallel) {
+		if (false == isParallel) {
+			return groupBy2Key(collection, key1, key2);
+		}
+		if (CollUtil.isEmpty(collection)) {
+			return Collections.emptyMap();
+		}
+		return collection
+				.parallelStream()
 				.collect(Collectors.groupingBy(key1, Collectors.groupingBy(key2, Collectors.toList())));
 	}
 
@@ -114,6 +212,32 @@ public class CollStreamUtil {
 	}
 
 	/**
+	 * 将collection按照两个规则(比如有相同的年级id,班级id)分类成双层map<br>
+	 * <B>{@code Collection<E>  --->  Map<T,Map<U,E>> } </B>
+	 *
+	 * @param collection 需要分类的集合
+	 * @param key1       第一个分类的规则
+	 * @param key2       第二个分类的规则
+	 * @param isParallel 是否并行流
+	 * @param <T>        第一个map中的key类型
+	 * @param <U>        第二个map中的key类型
+	 * @param <E>        collection中的泛型
+	 * @return 分类后的map
+	 * @see #group2Map 的并行实现
+	 */
+	public static <E, T, U> Map<T, Map<U, E>> group2Map(Collection<E> collection, Function<E, T> key1, Function<E, U> key2, boolean isParallel) {
+		if (false == isParallel) {
+			return group2Map(collection, key1, key2);
+		}
+		if (CollUtil.isEmpty(collection) || key1 == null || key2 == null) {
+			return Collections.emptyMap();
+		}
+		return collection
+				.parallelStream()
+				.collect(Collectors.groupingBy(key1, Collectors.toMap(key2, Function.identity(), (l, r) -> l)));
+	}
+
+	/**
 	 * 将collection转化为List集合，但是两者的泛型不同<br>
 	 * <B>{@code Collection<E>  ------>  List<T> } </B>
 	 *
@@ -135,6 +259,32 @@ public class CollStreamUtil {
 	}
 
 	/**
+	 * 将collection转化为List集合，但是两者的泛型不同<br>
+	 * <B>{@code Collection<E>  ------>  List<T> } </B>
+	 *
+	 * @param collection 需要转化的集合
+	 * @param function   collection中的泛型转化为list泛型的lambda表达式
+	 * @param isParallel 是否并行流
+	 * @param <E>        collection中的泛型
+	 * @param <T>        List中的泛型
+	 * @return 转化后的list
+	 * @see #toList 的并行实现
+	 */
+	public static <E, T> List<T> toList(Collection<E> collection, Function<E, T> function, boolean isParallel) {
+		if (false == isParallel) {
+			return toList(collection, function);
+		}
+		if (CollUtil.isEmpty(collection)) {
+			return Collections.emptyList();
+		}
+		return collection
+				.parallelStream()
+				.map(function)
+				.filter(Objects::nonNull)
+				.collect(Collectors.toList());
+	}
+
+	/**
 	 * 将collection转化为Set集合，但是两者的泛型不同<br>
 	 * <B>{@code Collection<E>  ------>  Set<T> } </B>
 	 *
@@ -150,6 +300,32 @@ public class CollStreamUtil {
 		}
 		return collection
 				.stream()
+				.map(function)
+				.filter(Objects::nonNull)
+				.collect(Collectors.toSet());
+	}
+
+	/**
+	 * 将collection转化为Set集合，但是两者的泛型不同<br>
+	 * <B>{@code Collection<E>  ------>  Set<T> } </B>
+	 *
+	 * @param collection 需要转化的集合
+	 * @param function   collection中的泛型转化为set泛型的lambda表达式
+	 * @param isParallel 是否并行流
+	 * @param <E>        collection中的泛型
+	 * @param <T>        Set中的泛型
+	 * @return 转化后的Set
+	 * @see #toSet 的并行实现
+	 */
+	public static <E, T> Set<T> toSet(Collection<E> collection, Function<E, T> function, boolean isParallel) {
+		if (false == isParallel) {
+			return toSet(collection, function);
+		}
+		if (CollUtil.isEmpty(collection) || function == null) {
+			return Collections.emptySet();
+		}
+		return collection
+				.parallelStream()
 				.map(function)
 				.filter(Objects::nonNull)
 				.collect(Collectors.toSet());
