899f1384f6b3bcd62b626e4fdc46289b3d94dc43
Looly <loolly@gmail.com>
2022-03-05 22:47:07 +0800

    add test

---
+++ hutool-crypto/src/test/java/cn/hutool/crypto/test/symmetric/TEATest.java
@@ -0,0 +1,39 @@
+package cn.hutool.crypto.test.symmetric;
+
+import cn.hutool.crypto.symmetric.SymmetricCrypto;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * TEA（Tiny Encryption Algorithm）和 XTEA算法单元测试
+ */
+public class TEATest {
+
+	@Test
+	public void teaTest(){
+		String data = "测试的加密数据 by Hutool";
+
+		// 密钥必须为128bit
+		final SymmetricCrypto tea = new SymmetricCrypto("TEA", "MyPassword123456".getBytes());
+		final byte[] encrypt = tea.encrypt(data);
+
+		// 解密
+		final String decryptStr = tea.decryptStr(encrypt);
+
+		Assert.assertEquals(data, decryptStr);
+	}
+
+	@Test
+	public void xteaTest(){
+		String data = "测试的加密数据 by Hutool";
+
+		// 密钥必须为128bit
+		final SymmetricCrypto tea = new SymmetricCrypto("XTEA", "MyPassword123456".getBytes());
+		final byte[] encrypt = tea.encrypt(data);
+
+		// 解密
+		final String decryptStr = tea.decryptStr(encrypt);
+
+		Assert.assertEquals(data, decryptStr);
+	}
+}
