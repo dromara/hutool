1c5e9787339ffb141f6fbe1a626c37ba55c61ddc
Looly <loolly@gmail.com>
2022-03-14 23:23:18 +0800

    fix test

--- hutool-core/src/test/java/cn/hutool/core/io/FileCopierTest.java
+++ hutool-core/src/test/java/cn/hutool/core/io/FileCopierTest.java
@@ -7,35 +7,35 @@ import cn.hutool.core.io.file.FileCopier;
 
 /**
  * 文件拷贝单元测试
- * @author Looly
  *
+ * @author Looly
  */
 public class FileCopierTest {
-	
+
 	@Test
 	@Ignore
 	public void dirCopyTest() {
 		FileCopier copier = FileCopier.create("D:\\Java", "e:/eclipse/eclipse2.zip");
 		copier.copy();
 	}
-	
+
 	@Test
 	@Ignore
 	public void dirCopyTest2() {
 		//测试带.的文件夹复制
 		FileCopier copier = FileCopier.create("D:\\workspace\\java\\.metadata", "D:\\workspace\\java\\.metadata\\temp");
 		copier.copy();
-		
+
 		FileUtil.copy("D:\\workspace\\java\\looly\\hutool\\.git", "D:\\workspace\\java\\temp", true);
 	}
-	
+
 	@Test(expected = IORuntimeException.class)
 	public void dirCopySubTest() {
 		//测试父目录复制到子目录报错
 		FileCopier copier = FileCopier.create("D:\\workspace\\java\\.metadata", "D:\\workspace\\java\\.metadata\\temp");
 		copier.copy();
 	}
-	
+
 	@Test
 	@Ignore
 	public void copyFileToDirTest() {
