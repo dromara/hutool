163dff00bdc5204f8c536c01529daa0fce95f994
Looly <loolly@gmail.com>
2022-03-24 09:37:26 +0800

    fix re

--- hutool-http/src/main/java/cn/hutool/http/HtmlUtil.java
+++ hutool-http/src/main/java/cn/hutool/http/HtmlUtil.java
@@ -127,10 +127,10 @@ public class HtmlUtil {
 			// (?i)表示其后面的表达式忽略大小写
 			if (withTagContent) {
 				// 标签及其包含内容
-				regex = StrUtil.format("(?i)<{}(\\s+[^>]*)?/?>(.*?</{}>)?", tagName, tagName);
+				regex = StrUtil.format("(?i)<{}(\\s+[^>]*?)?/?>(.*?</{}>)?", tagName, tagName);
 			} else {
 				// 标签不包含内容
-				regex = StrUtil.format("(?i)<{}(\\s+[^>]*)?/?>|</?{}>", tagName, tagName);
+				regex = StrUtil.format("(?i)<{}(\\s+[^>]*?)?/?>|</?{}>", tagName, tagName);
 			}
 
 			content = ReUtil.delAll(regex, content); // 非自闭标签小写
