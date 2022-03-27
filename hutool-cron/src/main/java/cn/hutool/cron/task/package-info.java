bdf9eacb4536e745d907dfe5196bd68e55880547
Looly <loolly@gmail.com>
2022-03-27 20:19:02 +0800

    fix code

--- hutool-cron/src/main/java/cn/hutool/cron/task/package-info.java
+++ hutool-cron/src/main/java/cn/hutool/cron/task/package-info.java
@@ -1,6 +1,7 @@
 /**
- * 定时任务中作业的抽象封装和实现，包括Runnable实现和反射实现
- * 
+ * 定时任务中作业的抽象封装和实现，包括Runnable实现和反射实现<br>
+ * {@link cn.hutool.cron.task.Task}表示一个具体的任务，当满足时间匹配要求时，会执行{@link cn.hutool.cron.task.Task#execute()}方法。
+ *
  * @author looly
  *
  */
