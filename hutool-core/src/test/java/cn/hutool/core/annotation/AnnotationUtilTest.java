562e77e438bd71934b49b84df8f82e4282e0af44
李鸿达 <hongda.li.dlmu@qq.com>
2022-04-26 17:35:43 +0800

    fixed 7dbc9e9 from https://gitee.com/yixi-dlmu/hutool/pulls/603 新增可获取重复性注解方法

--- hutool-core/src/test/java/cn/hutool/core/annotation/AnnotationUtilTest.java
+++ hutool-core/src/test/java/cn/hutool/core/annotation/AnnotationUtilTest.java
@@ -3,7 +3,14 @@ package cn.hutool.core.annotation;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.Set;
+
 public class AnnotationUtilTest {
+	@Test
+	public void getRepeatAnnotationValueTest(){
+		Set<AnnotationForTest> annotations = AnnotationUtil.getRepeatedAnnotations(ClassWithAnnotation.class, AnnotationForTest.class);
+		Assert.assertTrue(annotations != null && annotations.size() == 2);
+	}
 
 	@Test
 	public void getAnnotationValueTest() {
@@ -23,6 +30,7 @@ public class AnnotationUtilTest {
 	}
 
 	@AnnotationForTest("测试")
+	@RepeatAnnotationForTest
 	static class ClassWithAnnotation{
 		public void test(){
 
