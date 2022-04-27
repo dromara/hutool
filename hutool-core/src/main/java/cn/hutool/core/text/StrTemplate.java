416c73e05b6f6fdd759b96cdb6847ca8a82bae2d
Looly <loolly@gmail.com>
2022-04-27 15:59:07 +0800

    remove

--- hutool-core/src/main/java/cn/hutool/core/text/StrTemplate.java
+++
@@ -1,8 +0,0 @@
-package cn.hutool.core.text;
-
-public class StrTemplate {
-	public static final String DEFAULT_MACRO_PREFIX = "$";
-	public static final String DEFAULT_MACRO_START = "${";
-	public static final String DEFAULT_MACRO_END = "}";
-
-}
