e0ac5e9961cfe0f2e7102eb669bf01d9d233d668
Looly <loolly@gmail.com>
2022-04-27 23:51:14 +0800

    prepare v6

--- bin/push_dev.sh
+++ bin/push_dev.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-echo -e "\033[32mCheckout to v5-dev\033[0m"
-git checkout v5-dev
+echo -e "\033[32mCheckout to v6-dev\033[0m"
+git checkout v6-dev
 
-echo -e "\033[32mPush to origin v5-dev\033[0m"
-git push origin v5-dev
-echo -e "\033[32mPush to osc v5-dev\033[0m"
-git push osc v5-dev
+echo -e "\033[32mPush to origin v6-dev\033[0m"
+git push origin v6-dev
+echo -e "\033[32mPush to osc v6-dev\033[0m"
+git push osc v6-dev
--- bin/push_master.sh
+++ bin/push_master.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-echo -e "\033[32mCheckout to v5-master\033[0m"
-git checkout v5-master
+echo -e "\033[32mCheckout to v6-master\033[0m"
+git checkout v6-master
 
-echo -e "\033[32mMerge v5-dev branch\033[0m"
-git merge v5-dev -m 'Prepare release'
+echo -e "\033[32mMerge v6-dev branch\033[0m"
+git merge v6-dev -m 'Prepare release'
 
-echo -e "\033[32mPush to origin v5-master\033[0m"
-git push origin v5-master
-echo -e "\033[32mPush to osc v5-master\033[0m"
-git push osc v5-master
+echo -e "\033[32mPush to origin v6-master\033[0m"
+git push origin v6-master
+echo -e "\033[32mPush to osc v6-master\033[0m"
+git push osc v6-master
