835e450c3cdb5ea983f49187ab0e1373e4f03707
Looly <loolly@gmail.com>
2022-04-27 09:31:29 +0800

    add StrTemplate

---
+++ hutool-core/src/main/java/cn/hutool/core/text/StrTemplate.java
@@ -0,0 +1,8 @@
+package cn.hutool.core.text;
+
+public class StrTemplate {
+	public static final String DEFAULT_MACRO_PREFIX = "$";
+	public static final String DEFAULT_MACRO_START = "${";
+	public static final String DEFAULT_MACRO_END = "}";
+
+}
