e4737f0a5d59223a20c4e57c3fb1fa9201f45e4e
duandazhi <ab601026460@163.com>
2022-06-28 17:29:14 +0800

    add more StrUtilTest

--- hutool-core/src/test/java/cn/hutool/core/text/StrUtilTest.java
+++ hutool-core/src/test/java/cn/hutool/core/text/StrUtilTest.java
@@ -8,6 +8,44 @@ import java.util.List;
 
 public class StrUtilTest {
 	@Test
+	public void testReplace2() {
+		// https://gitee.com/dromara/hutool/issues/I4M16G
+		final String replace = "#{A}";
+		final String result = StrUtil.replace(replace, "#{AAAAAAA}", "1");
+		Assert.assertEquals(replace, result);
+	}
+
+	@Test
+	public void testReplaceByStr() {
+		final String replace = "SSM15930297701BeryAllen";
+		final String result = StrUtil.replace(replace, 5, 12, "***");
+		Assert.assertEquals("SSM15***01BeryAllen", result);
+	}
+
+	@Test
+	public void testAddPrefixIfNot() {
+		final String str = "hutool";
+		String result = StrUtil.addPrefixIfNot(str, "hu");
+		Assert.assertEquals(str, result);
+
+		result = StrUtil.addPrefixIfNot(str, "Good");
+		Assert.assertEquals("Good" + str, result);
+	}
+
+	@Test
+	public void testAddSuffixIfNot() {
+		final String str = "hutool";
+		String result = StrUtil.addSuffixIfNot(str, "tool");
+		Assert.assertEquals(str, result);
+
+		result = StrUtil.addSuffixIfNot(str, " is Good");
+		Assert.assertEquals(str + " is Good", result);
+
+		result = StrUtil.addSuffixIfNot("", "/");
+		Assert.assertEquals("/", result);
+	}
+
+	@Test
 	public void testIssAllBlank() {
 		List<String> queue = new LinkedList<>();
 		queue.add("apple");
@@ -41,5 +79,13 @@ public class StrUtilTest {
 		Assert.assertTrue(CharSequenceUtil.isAllBlank("\u3000"));
 		Assert.assertTrue(CharSequenceUtil.isAllBlank("\uFEFF"));
 
+		// 其他空白字符
+		Assert.assertTrue(CharSequenceUtil.isAllBlank("\u000B"));
+		Assert.assertTrue(CharSequenceUtil.isAllBlank("\u000C"));
+		Assert.assertTrue(CharSequenceUtil.isAllBlank("\u00A0"));
+		Assert.assertTrue(CharSequenceUtil.isAllBlank("\u1680"));
+		Assert.assertTrue(CharSequenceUtil.isAllBlank("\u180E"));
+		Assert.assertTrue(CharSequenceUtil.isAllBlank("\u2000"));
+		Assert.assertTrue(CharSequenceUtil.isAllBlank("\u2001"));
 	}
 }
