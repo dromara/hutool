ab79193c7efebbe4dce094235c9906f2e2253bbd
Looly <loolly@gmail.com>
2022-07-16 12:18:21 +0800

    fix code

--- hutool-core/src/main/java/cn/hutool/core/collection/Partition.java
+++ hutool-core/src/main/java/cn/hutool/core/collection/Partition.java
@@ -1,5 +1,7 @@
 package cn.hutool.core.collection;
 
+import cn.hutool.core.lang.Assert;
+
 import java.util.AbstractList;
 import java.util.List;
 
@@ -21,19 +23,18 @@ public class Partition<T> extends AbstractList<List<T>> {
 	/**
 	 * 列表分区
 	 *
-	 * @param list 被分区的列表
-	 * @param size 每个分区的长度
+	 * @param list 被分区的列表，非空
+	 * @param size 每个分区的长度，必须>0
 	 */
 	public Partition(List<T> list, int size) {
-		this.list = list;
-		this.size = size;
+		this.list = Assert.notNull(list);
+		this.size = Math.min(list.size(), size);
 	}
 
 	@Override
 	public List<T> get(int index) {
-		int listSize = list.size();
-		int start = Math.min(index * size, listSize);
-		int end = Math.min(start + size, listSize);
+		final int start = index * size;
+		final int end = Math.min(start + size, list.size());
 		return list.subList(start, end);
 	}
 
@@ -42,8 +43,9 @@ public class Partition<T> extends AbstractList<List<T>> {
 		// 此处采用动态计算，以应对list变
 		final int size = this.size;
 		final int total = list.size();
-		int length = (total + size - 1) / size;
-		return length;
+		// 类似于判断余数，当总数非整份size时，多余的数>=1，则相当于被除数多一个size，做到+1目的
+		// 类似于：if(total % size > 0){length += 1;}
+		return (total + size - 1) / size;
 	}
 
 	@Override
