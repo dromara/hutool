f2bec2a9b951ecf4e9e59821282bd7d92b86fbfa
yujr <yujr@asiainfo.com>
2022-07-26 18:24:56 +0800

    删除测试文件

--- hutool-dfa/src/main/java/cn/hutool/dfa/Test.java
+++
@@ -1,8 +0,0 @@
-package cn.hutool.dfa;
-
-public class Test {
-	public static void main(String[] args) {
-		int initialCapacity = (int) (0 / 0.75) + 1;
-		System.out.println(initialCapacity);
-	}
-}
