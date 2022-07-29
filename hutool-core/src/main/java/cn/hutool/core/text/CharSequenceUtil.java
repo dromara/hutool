3608cbb20ff0cc84daa87d177e2d9bfd81e9bb02
Looly <loolly@gmail.com>
2022-07-29 21:29:08 +0800

    fix comment

--- hutool-core/src/main/java/cn/hutool/core/text/CharSequenceUtil.java
+++ hutool-core/src/main/java/cn/hutool/core/text/CharSequenceUtil.java
@@ -3722,7 +3722,6 @@ public class CharSequenceUtil {
 	/**
 	 * 替换所有正则匹配的文本，并使用自定义函数决定如何替换<br>
 	 * replaceFun可以通过{@link Matcher}提取出匹配到的内容的不同部分，然后经过重新处理、组装变成新的内容放回原位。
-	 *
 	 * <pre class="code">
 	 *     replace(this.content, "(\\d+)", parameters -&gt; "-" + parameters.group(1) + "-")
 	 *     // 结果为："ZZZaaabbbccc中文-1234-"
