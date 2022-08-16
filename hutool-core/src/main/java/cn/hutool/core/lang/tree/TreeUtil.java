6196f83caa6e74c0896f3fd60a2aeb0adbbc7be2
duhanmin <hanmin.du@longbridge.sg>
2022-08-16 14:45:32 +0800

    Tree add 类型校验

--- hutool-core/src/main/java/cn/hutool/core/lang/tree/TreeUtil.java
+++ hutool-core/src/main/java/cn/hutool/core/lang/tree/TreeUtil.java
@@ -123,7 +123,7 @@ public class TreeUtil {
 	 */
 	public static <T, E> Tree<E> buildSingle(List<T> list, E rootId, TreeNodeConfig treeNodeConfig, NodeParser<T, E> nodeParser) {
 		return TreeBuilder.of(rootId, treeNodeConfig)
-				.append(list, nodeParser).build();
+				.append(list, rootId, nodeParser).build();
 	}
 
 	/**
