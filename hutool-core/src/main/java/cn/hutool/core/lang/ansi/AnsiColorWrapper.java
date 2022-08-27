4e53873d814172c4cdb6d540b4acd833df3fd513
TomXin <766781886@qq.com>
2022-08-27 15:59:03 +0800

    新增AnsiColors、AnsiColorWrapper

---
+++ hutool-core/src/main/java/cn/hutool/core/lang/ansi/AnsiColorWrapper.java
@@ -0,0 +1,83 @@
+package cn.hutool.core.lang.ansi;
+
+import cn.hutool.core.lang.Assert;
+
+import java.util.Objects;
+
+/**
+ * ANSI 颜色包装类
+ *
+ * @author TomXin
+ * @since 5.8.6
+ */
+public class AnsiColorWrapper {
+
+	private final int code;
+
+	private final AnsiColors.BitDepth bitDepth;
+
+	/**
+	 * 创建指定位深度的 {@link AnsiColorWrapper} 实例
+	 * @param code 颜色编码，位深度为4bit时，code取值范围[30~37]，[90~97]。位深度为8bit时，code取值范围[0~255]
+	 * @param bitDepth 位深度
+	 */
+	public AnsiColorWrapper(int code, AnsiColors.BitDepth bitDepth) {
+		if (bitDepth==AnsiColors.BitDepth.FOUR){
+			Assert.isTrue((30<=code&&code<=37)||(90<=code&&code<=97),"The value of 4 bit color only supported [30~37],[90~97].");
+		}
+		Assert.isTrue((0<=code&&code<=255),"The value of 8 bit color only supported [0~255].");
+		this.code = code;
+		this.bitDepth = bitDepth;
+	}
+
+	/**
+	 * 转换为 {@link AnsiElement} 实例
+	 *
+	 * @param foreOrBack 区分前景还是背景
+	 * @return {@link AnsiElement} 实例
+	 */
+	public AnsiElement toAnsiElement(ForeOrBack foreOrBack){
+		if (bitDepth== AnsiColors.BitDepth.FOUR){
+			if (foreOrBack == ForeOrBack.FORE){
+				return AnsiColor.valueOf(String.valueOf(code));
+			}
+			return AnsiBackground.valueOf(String.valueOf(code + 10));
+		}
+		if (foreOrBack == ForeOrBack.FORE){
+			return Ansi8BitColor.foreground(code);
+		}
+		return Ansi8BitColor.background(code);
+	}
+
+	/**
+	 * 区分前景还是背景
+	 */
+	public enum ForeOrBack{
+
+		/**
+		 * 前景
+		 */
+		FORE,
+		/**
+		 * 背景
+		 */
+		BACK,
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) {
+			return true;
+		}
+		if (o == null || getClass() != o.getClass()) {
+			return false;
+		}
+		AnsiColorWrapper that = (AnsiColorWrapper) o;
+		return code == that.code && bitDepth == that.bitDepth;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(code, bitDepth);
+	}
+}
--- hutool-core/src/test/java/cn/hutool/core/lang/ansi/AnsiEncoderTest.java
+++ hutool-core/src/test/java/cn/hutool/core/lang/ansi/AnsiEncoderTest.java
@@ -1,7 +1,11 @@
 package cn.hutool.core.lang.ansi;
 
+import cn.hutool.core.lang.Console;
 import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
+
+import java.awt.*;
 
 public class AnsiEncoderTest {
 
@@ -9,5 +13,28 @@ public class AnsiEncoderTest {
 	public void encodeTest(){
 		final String encode = AnsiEncoder.encode(AnsiColor.GREEN, "Hutool test");
 		Assert.assertEquals("\u001B[32mHutool test\u001B[0;39m", encode);
+	}
+
+	@Test
+	@Ignore
+	public void colorfulEncodeTest(){
+		String text = "Hutool▀████▀";
+		final AnsiColors ansiColors = new AnsiColors(AnsiColors.BitDepth.EIGHT);
+		Color[] colorArray = new Color[]{
+				Color.BLACK,			Color.BLUE,
+				Color.CYAN,				Color.DARK_GRAY,
+				Color.GRAY,				Color.GREEN,
+				Color.LIGHT_GRAY,		Color.MAGENTA,
+				Color.ORANGE,			Color.PINK,
+				Color.RED,				Color.WHITE,
+				Color.YELLOW
+		};
+		for (Color foreColor : colorArray) {
+			AnsiElement foreElement = ansiColors.findClosest(foreColor).toAnsiElement(AnsiColorWrapper.ForeOrBack.FORE);
+			Color backColor = new Color(255 - foreColor.getRed(), 255 - foreColor.getGreen(), 255 - foreColor.getBlue());
+			AnsiElement backElement = ansiColors.findClosest(backColor).toAnsiElement(AnsiColorWrapper.ForeOrBack.BACK);
+			String encode = AnsiEncoder.encode(foreElement, backElement, text);
+			//Console.print( i%2==1?encode+"\n":encode);
+		}
 	}
 }
