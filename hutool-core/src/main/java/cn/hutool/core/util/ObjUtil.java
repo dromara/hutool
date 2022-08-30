90772979668cfca8be8dc9c80429b35ee02cd01d
huangchengxing <841396397@qq.com>
2022-08-30 16:45:17 +0800

    将defaultIfNull检验参数类型改为泛型，并补充重载方法

--- hutool-core/src/main/java/cn/hutool/core/util/ObjUtil.java
+++ hutool-core/src/main/java/cn/hutool/core/util/ObjUtil.java
@@ -13,11 +13,7 @@ import cn.hutool.core.text.StrUtil;
 
 import java.lang.reflect.Array;
 import java.math.BigDecimal;
-import java.util.Collection;
-import java.util.Enumeration;
-import java.util.Iterator;
-import java.util.Map;
-import java.util.Objects;
+import java.util.*;
 import java.util.function.Function;
 import java.util.function.Supplier;
 
@@ -301,6 +297,7 @@ public class ObjUtil {
 	/**
 	 * 如果给定对象为{@code null} 返回默认值, 如果不为null 返回自定义handle处理后的返回值
 	 *
+	 * @param <R>          返回值类型
 	 * @param <T>          被检查对象类型
 	 * @param source       Object 类型对象
 	 * @param handler      非空时自定义的处理方法
@@ -308,11 +305,28 @@ public class ObjUtil {
 	 * @return 处理后的返回值
 	 * @since 6.0.0
 	 */
-	public static <T> T defaultIfNull(final Object source, final Function<Object, T> handler, final Supplier<? extends T> defaultSupplier) {
+	public static <T, R> R defaultIfNull(
+		final T source, final Function<? super T, ? extends R> handler, final Supplier<? extends R> defaultSupplier) {
 		if (isNotNull(source)) {
 			return handler.apply(source);
 		}
 		return defaultSupplier.get();
+	}
+
+	/**
+	 * 如果给定对象为{@code null}返回默认值, 如果不为null返回自定义handle处理后的返回值
+	 *
+	 * @param <R>          返回值类型
+	 * @param <T>          被检查对象类型
+	 * @param source       Object 类型对象
+	 * @param handler      非空时自定义的处理方法
+	 * @param defaultValue 默认为空的返回值
+	 * @return 处理后的返回值
+	 * @since 6.0.0
+	 */
+	public static <T, R> R defaultIfNull(
+		final T source, final Function<? super T, ? extends R> handler, final R defaultValue) {
+		return isNull(source) ? defaultValue : handler.apply(source);
 	}
 
 	/**
