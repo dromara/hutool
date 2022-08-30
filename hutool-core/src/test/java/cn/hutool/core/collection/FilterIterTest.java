87a1cec9edc96439f94dedda470fec38ebb637ec
Looly <loolly@gmail.com>
2022-08-30 22:27:30 +0800

    add test

--- hutool-core/src/test/java/cn/hutool/core/collection/FilterIterTest.java
+++ hutool-core/src/test/java/cn/hutool/core/collection/FilterIterTest.java
@@ -1,5 +1,6 @@
 package cn.hutool.core.collection;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.Iterator;
@@ -15,17 +16,25 @@ public class FilterIterTest {
 		Iterator<String> it = ListUtil.of("1", "2").iterator();
 		// filter 为null
 		FilterIter<String> filterIter = new FilterIter<>(it, null);
-		while (filterIter.hasNext()) {
-			System.out.println(filterIter.next());
-		}
 
-		System.out.println();
+		int count = 0;
+		while (filterIter.hasNext()) {
+			if(filterIter.next() != null){
+				count++;
+			}
+		}
+		Assert.assertEquals(2, count);
+
 		it = ListUtil.of("1", "2").iterator();
 		// filter 不为空
 		filterIter = new FilterIter<>(it, (key) -> key.equals("1"));
+		count = 0;
 		while (filterIter.hasNext()) {
-			System.out.println(filterIter.next());
+			if(filterIter.next() != null){
+				count++;
+			}
 		}
+		Assert.assertEquals(1, count);
 	}
 
 }
