6d3d7d491335d7a54c21bb37dba960db6865e862
Looly <loolly@gmail.com>
2022-09-15 19:24:48 +0800

    add test

--- hutool-core/src/test/java/cn/hutool/core/util/ClassLoaderUtilTest.java
+++ hutool-core/src/test/java/cn/hutool/core/util/ClassLoaderUtilTest.java
@@ -1,6 +1,7 @@
 package cn.hutool.core.util;
 
 import cn.hutool.core.classloader.ClassLoaderUtil;
+import cn.hutool.core.map.Dict;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -13,5 +14,13 @@ public class ClassLoaderUtilTest {
 
 		name = ClassLoaderUtil.loadClass("java.lang.Thread$State").getName();
 		Assert.assertEquals("java.lang.Thread$State", name);
+	}
+
+	@Test
+	public void loadArrayClassTest(){
+		final String s = Dict[].class.getName();
+
+		final Class<Object> objectClass = ClassLoaderUtil.loadClass(s);
+		Assert.assertEquals(Dict[].class, objectClass);
 	}
 }
