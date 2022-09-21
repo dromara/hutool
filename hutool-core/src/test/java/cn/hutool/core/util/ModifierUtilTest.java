400a7b7e040a7471ab37ed526ef4f9767108802f
Looly <loolly@gmail.com>
2022-09-21 21:48:06 +0800

    add method

--- hutool-core/src/test/java/cn/hutool/core/util/ModifierUtilTest.java
+++ hutool-core/src/test/java/cn/hutool/core/util/ModifierUtilTest.java
@@ -1,10 +1,14 @@
 package cn.hutool.core.util;
 
+import cn.hutool.core.reflect.FieldUtil;
 import cn.hutool.core.reflect.ModifierUtil;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.List;
 
 public class ModifierUtilTest {
 
@@ -19,5 +23,28 @@ public class ModifierUtilTest {
 	}
 
 	private static void ddd() {
+	}
+
+	@Test
+	public void setFinalFieldValueTest() {
+		final String fieldName = "DIALECTS";
+		final List<Number> dialects =
+				Arrays.asList(
+						1,
+						2,
+						3,
+						99
+				);
+		final Field field = FieldUtil.getField(JdbcDialects.class, fieldName);
+		ModifierUtil.removeFinalModify(field);
+		FieldUtil.setFieldValue(JdbcDialects.class, fieldName, dialects);
+
+		Assert.assertEquals(dialects, FieldUtil.getFieldValue(JdbcDialects.class, fieldName));
+	}
+
+	@SuppressWarnings("unused")
+	public static class JdbcDialects {
+		private static final List<Number> DIALECTS =
+				Arrays.asList(1L, 2L, 3L);
 	}
 }
