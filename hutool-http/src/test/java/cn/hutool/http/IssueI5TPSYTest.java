6bde21ff4ab5171d1d36e8631d75131937230b8c
Looly <loolly@aliyun.com>
2022-09-30 18:11:20 +0800

    add test

--- hutool-http/src/test/java/cn/hutool/http/IssueI5TPSYTest.java
+++ hutool-http/src/test/java/cn/hutool/http/IssueI5TPSYTest.java
@@ -1,19 +1,17 @@
 package cn.hutool.http;
 
 import cn.hutool.core.lang.Console;
-import org.junit.Ignore;
 import org.junit.Test;
-
-import java.net.HttpCookie;
 
 public class IssueI5TPSYTest {
 
 	@Test
-	@Ignore
+	//@Ignore
 	public void redirectTest() {
 		final String url = "https://bsxt.gdzwfw.gov.cn/UnifiedReporting/auth/newIndex";
 		final HttpResponse res = HttpUtil.createGet(url).setFollowRedirects(true)
-				.cookie(new HttpCookie("iPlanetDirectoryPro", "123"))
+				.header(Header.USER_AGENT, "PostmanRuntime/7.29.2")
+				.cookie("jsessionid=s%3ANq6YTcIHQWrHkEqOSxiQNijDMhoFNV4_.h2MVD1CkW7sOZ60OSnPs7m4K%2FhENfYy%2FdzjKvSiZF4E")
 				.execute();
 		Console.log(res.body());
 	}
