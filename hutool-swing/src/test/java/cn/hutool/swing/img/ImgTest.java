bcdbf5537cbcdae5ea29bf4a71ee2031ef0fb0e1
Looly <loolly@gmail.com>
2022-10-08 21:25:52 +0800

    fix code

--- hutool-swing/src/test/java/cn/hutool/swing/img/ImgTest.java
+++ hutool-swing/src/test/java/cn/hutool/swing/img/ImgTest.java
@@ -2,7 +2,7 @@ package cn.hutool.swing.img;
 
 import cn.hutool.core.io.FileTypeUtil;
 import cn.hutool.core.io.FileUtil;
-import cn.hutool.core.net.URLUtil;
+import cn.hutool.core.net.url.URLUtil;
 import org.junit.Ignore;
 import org.junit.Test;
 
