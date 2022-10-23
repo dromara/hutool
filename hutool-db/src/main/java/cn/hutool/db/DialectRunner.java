5c835ef888cbdce9e7116da245199af12ee9ab6c
Looly <loolly@aliyun.com>
2022-10-23 13:25:11 +0800

    修复分页时order by截断问题

--- hutool-db/src/main/java/cn/hutool/db/DialectRunner.java
+++ hutool-db/src/main/java/cn/hutool/db/DialectRunner.java
@@ -272,7 +272,7 @@ public class DialectRunner implements Serializable {
 
 		String selectSql = sqlBuilder.build();
 		// 去除order by 子句
-		final int orderByIndex = StrUtil.indexOfIgnoreCase(selectSql, " order by");
+		final int orderByIndex = StrUtil.lastIndexOfIgnoreCase(selectSql, " order by");
 		if (orderByIndex > 0) {
 			selectSql = StrUtil.subPre(selectSql, orderByIndex);
 		}
