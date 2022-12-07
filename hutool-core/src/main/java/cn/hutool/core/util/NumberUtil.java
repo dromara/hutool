fdf698066fcdab691d72b68d05122662dfe88a8a
Dingxindong <dingxindong@kezaihui.com>
2022-12-07 10:42:56 +0800

    将形参判断提前，并且包装类型的判断 改用equals 而不是 ==

--- hutool-core/src/main/java/cn/hutool/core/util/NumberUtil.java
+++ hutool-core/src/main/java/cn/hutool/core/util/NumberUtil.java
@@ -1881,12 +1881,12 @@ public class NumberUtil {
 	 */
 	public static boolean equals(BigDecimal bigNum1, BigDecimal bigNum2) {
 		//noinspection NumberEquality
-		if (bigNum1 == bigNum2) {
-			// 如果用户传入同一对象，省略compareTo以提高性能。
-			return true;
-		}
 		if (bigNum1 == null || bigNum2 == null) {
 			return false;
+		}
+		if (bigNum1.equals(bigNum2)) {
+			// 如果用户传入同一对象，省略compareTo以提高性能。
+			return true;
 		}
 		return 0 == bigNum1.compareTo(bigNum2);
 	}
