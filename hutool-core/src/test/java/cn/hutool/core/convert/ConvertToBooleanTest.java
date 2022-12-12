cfc72cc2a98937a6953e863d9e9f9dcc740e640a
Looly <loolly@gmail.com>
2022-12-12 13:09:52 +0800

    add test

--- hutool-core/src/test/java/cn/hutool/core/convert/ConvertToBooleanTest.java
+++ hutool-core/src/test/java/cn/hutool/core/convert/ConvertToBooleanTest.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 public class ConvertToBooleanTest {
 
 	@Test
-	public void intToBooleanTest(){
+	public void intToBooleanTest() {
 		final int a = 100;
 		final Boolean aBoolean = Convert.toBool(a);
 		Assert.assertTrue(aBoolean);
@@ -15,4 +15,11 @@ public class ConvertToBooleanTest {
 		final Boolean bBoolean = Convert.toBool(b);
 		Assert.assertFalse(bBoolean);
 	}
+
+	@Test
+	public void issueI65P8ATest() {
+		final Boolean bool = Convert.toBool("", Boolean.TRUE);
+		Assert.assertFalse(bool);
+	}
+
 }
