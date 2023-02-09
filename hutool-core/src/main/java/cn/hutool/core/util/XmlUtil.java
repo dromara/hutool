02e85a80e566b212ad8cf87bc943a567dcc0b9ee
Looly <loolly@gmail.com>
2023-02-09 18:31:54 +0800

    【重要】删除XmlUtil.readObjectFromXml方法，避免漏洞

--- hutool-core/src/main/java/cn/hutool/core/util/XmlUtil.java
+++ hutool-core/src/main/java/cn/hutool/core/util/XmlUtil.java
@@ -39,7 +39,6 @@ import javax.xml.xpath.XPath;
 import javax.xml.xpath.XPathConstants;
 import javax.xml.xpath.XPathExpressionException;
 import javax.xml.xpath.XPathFactory;
-import java.beans.XMLDecoder;
 import java.beans.XMLEncoder;
 import java.io.BufferedInputStream;
 import java.io.BufferedWriter;
@@ -328,62 +327,6 @@ public class XmlUtil {
 		}
 		xmlStr = cleanInvalid(xmlStr);
 		return readXML(StrUtil.getReader(xmlStr));
-	}
-
-	/**
-	 * 从XML中读取对象 Reads serialized object from the XML file.<br>
-	 * 注意，使用此方法解析不受信任 XML 字符串时，可能容易受到远程代码执行攻击！<br>
-	 * 见：https://gitee.com/dromara/hutool/issues/I6AEX2
-	 *
-	 * @param <T>    对象类型
-	 * @param source XML文件
-	 * @return 对象
-	 * @deprecated 使用此方法解析不受信任 XML 字符串时，可能容易受到远程代码执行攻击，废弃
-	 */
-	@Deprecated
-	public static <T> T readObjectFromXml(File source) {
-		return readObjectFromXml(new InputSource(FileUtil.getInputStream(source)));
-	}
-
-	/**
-	 * 从XML中读取对象 Reads serialized object from the XML file.<br>
-	 * 注意，使用此方法解析不受信任 XML 字符串时，可能容易受到远程代码执行攻击！<br>
-	 * 见：https://gitee.com/dromara/hutool/issues/I6AEX2
-	 *
-	 * @param <T>    对象类型
-	 * @param xmlStr XML内容
-	 * @return 对象
-	 * @since 3.2.0
-	 * @deprecated 使用此方法解析不受信任 XML 字符串时，可能容易受到远程代码执行攻击，废弃
-	 */
-	@Deprecated
-	public static <T> T readObjectFromXml(String xmlStr) {
-		return readObjectFromXml(new InputSource(StrUtil.getReader(xmlStr)));
-	}
-
-	/**
-	 * 从XML中读取对象 Reads serialized object from the XML file.<br>
-	 * 注意，使用此方法解析不受信任 XML 字符串时，可能容易受到远程代码执行攻击！<br>
-	 * 见：https://gitee.com/dromara/hutool/issues/I6AEX2
-	 *
-	 * @param <T>    对象类型
-	 * @param source {@link InputSource}
-	 * @return 对象
-	 * @since 3.2.0
-	 * @deprecated 使用此方法解析不受信任 XML 字符串时，可能容易受到远程代码执行攻击，废弃
-	 */
-	@Deprecated
-	@SuppressWarnings("unchecked")
-	public static <T> T readObjectFromXml(InputSource source) {
-		Object result;
-		XMLDecoder xmldec = null;
-		try {
-			xmldec = new XMLDecoder(source);
-			result = xmldec.readObject();
-		} finally {
-			IoUtil.close(xmldec);
-		}
-		return (T) result;
 	}
 
 	// -------------------------------------------------------------------------------------- Write
