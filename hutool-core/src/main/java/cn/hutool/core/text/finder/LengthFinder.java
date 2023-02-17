6cd85b7f1627e245998c61a104100708c6c82553
Zjp <1215582715@qq.com>
2023-02-17 10:50:31 +0800

    fix StrUtil.split切分长度为0时的bug;

--- hutool-core/src/main/java/cn/hutool/core/text/finder/LengthFinder.java
+++ hutool-core/src/main/java/cn/hutool/core/text/finder/LengthFinder.java
@@ -19,6 +19,7 @@ public class LengthFinder extends TextFinder {
 	 * @param length 长度
 	 */
 	public LengthFinder(int length) {
+		Assert.isTrue(length > 0, "Length must be great than 0");
 		this.length = length;
 	}
 
