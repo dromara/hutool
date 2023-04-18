ce1c0bab452482621d102ab3154df10870a436d7
Looly <loolly@aliyun.com>
2023-04-18 16:35:29 +0800

    fix decoder bug

--- hutool-core/src/main/java/cn/hutool/core/net/URLDecoder.java
+++ hutool-core/src/main/java/cn/hutool/core/net/URLDecoder.java
@@ -88,11 +88,6 @@ public class URLDecoder implements Serializable {
 		char c;
 		for (int i = 0; i < length; i++) {
 			c = str.charAt(i);
-			if('+' == c){
-				result.append(isPlusToSpace ? CharUtil.SPACE : c);
-				begin++;
-				continue;
-			}
 			if(ESCAPE_CHAR == c || CharUtil.isHexChar(c)){
 				continue;
 			}
@@ -101,6 +96,11 @@ public class URLDecoder implements Serializable {
 			// 处理之前的hex字符
 			if(i > begin){
 				result.append(decodeSub(str, begin, i, charset, isPlusToSpace));
+			}
+
+			// 非Hex字符，忽略本字符
+			if('+' == c && isPlusToSpace){
+				c = CharUtil.SPACE;
 			}
 
 			// 非Hex字符，忽略本字符
