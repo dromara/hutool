049086aa791fcd9f5630c65a7b1ee0b0d622da36
Looly <loolly@gmail.com>
2023-05-04 03:14:12 +0800

    优化MD5性能

--- hutool-crypto/src/main/java/cn/hutool/crypto/digest/MD5.java
+++ hutool-crypto/src/main/java/cn/hutool/crypto/digest/MD5.java
@@ -6,33 +6,37 @@ import java.nio.charset.Charset;
 
 /**
  * MD5算法
- * 
+ *
  * @author looly
  * @since 4.4.3
  */
 public class MD5 extends Digester {
 	private static final long serialVersionUID = 1L;
-	
+
+	// issue#I6ZIQH
+	// MD5算法不使用BC库，使用JDK默认以提高初始性能
+	private static final DigesterFactory FACTORY = DigesterFactory.ofJdk(DigestAlgorithm.MD5.getValue());
+
 	/**
 	 * 创建MD5实例
-	 * 
+	 *
 	 * @return MD5
 	 * @since 4.6.0
 	 */
 	public static MD5 create() {
 		return new MD5();
 	}
-	
+
 	/**
 	 * 构造
 	 */
 	public MD5() {
-		super(DigestAlgorithm.MD5);
+		super(FACTORY.createMessageDigester());
 	}
 
 	/**
 	 * 构造
-	 * 
+	 *
 	 * @param salt 盐值
 	 */
 	public MD5(byte[] salt) {
@@ -41,7 +45,7 @@ public class MD5 extends Digester {
 
 	/**
 	 * 构造
-	 * 
+	 *
 	 * @param salt 盐值
 	 * @param digestCount 摘要次数，当此值小于等于1,默认为1。
 	 */
@@ -51,7 +55,7 @@ public class MD5 extends Digester {
 
 	/**
 	 * 构造
-	 * 
+	 *
 	 * @param salt 盐值
 	 * @param saltPosition 加盐位置，即将盐值字符串放置在数据的index数，默认0
 	 * @param digestCount 摘要次数，当此值小于等于1,默认为1。
@@ -62,10 +66,10 @@ public class MD5 extends Digester {
 		this.saltPosition = saltPosition;
 		this.digestCount = digestCount;
 	}
-	
+
 	/**
 	 * 生成16位MD5摘要
-	 * 
+	 *
 	 * @param data 数据
 	 * @param charset 编码
 	 * @return 16位MD5摘要
@@ -77,7 +81,7 @@ public class MD5 extends Digester {
 
 	/**
 	 * 生成16位MD5摘要
-	 * 
+	 *
 	 * @param data 数据
 	 * @return 16位MD5摘要
 	 * @since 4.5.1
@@ -88,7 +92,7 @@ public class MD5 extends Digester {
 
 	/**
 	 * 生成16位MD5摘要
-	 * 
+	 *
 	 * @param data 数据
 	 * @return 16位MD5摘要
 	 * @since 4.5.1
@@ -99,7 +103,7 @@ public class MD5 extends Digester {
 
 	/**
 	 * 生成16位MD5摘要
-	 * 
+	 *
 	 * @param data 数据
 	 * @return 16位MD5摘要
 	 */
@@ -109,7 +113,7 @@ public class MD5 extends Digester {
 
 	/**
 	 * 生成16位MD5摘要
-	 * 
+	 *
 	 * @param data 数据
 	 * @return 16位MD5摘要
 	 * @since 4.5.1
