6acaae7b1127b8c0799366c51d9c3dd1717bfcfd
VampireAchao <achao1441470436@gmail.com>
2023-05-11 22:59:28 +0800

    fix issue I72C6E

--- hutool-core/src/main/java/cn/hutool/core/lang/Opt.java
+++ hutool-core/src/main/java/cn/hutool/core/lang/Opt.java
@@ -1,27 +1,3 @@
-/*
- * Copyright (c) 2012, 2020, Oracle and/or its affiliates. All rights reserved.
- * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
- *
- * This code is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License version 2 only, as
- * published by the Free Software Foundation.  Oracle designates this
- * particular file as subject to the "Classpath" exception as provided
- * by Oracle in the LICENSE file that accompanied this code.
- *
- * This code is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
- * version 2 for more details (a copy is included in the LICENSE file that
- * accompanied this code).
- *
- * You should have received a copy of the GNU General Public License version
- * 2 along with this work; if not, write to the Free Software Foundation,
- * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
- *
- * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
- * or visit www.oracle.com if you need additional information or have any
- * questions.
- */
 package cn.hutool.core.lang;
 
 import cn.hutool.core.collection.CollectionUtil;
@@ -59,9 +35,9 @@ public class Opt<T> {
 	 * @param <T> 包裹里元素的类型
 	 * @return Opt
 	 */
+	@SuppressWarnings("unchecked")
 	public static <T> Opt<T> empty() {
-		@SuppressWarnings("unchecked") final Opt<T> t = (Opt<T>) EMPTY;
-		return t;
+		return (Opt<T>) EMPTY;
 	}
 
 	/**
