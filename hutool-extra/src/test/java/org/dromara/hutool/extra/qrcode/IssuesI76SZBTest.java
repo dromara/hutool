8f299f10aa6156d01433bccd0e294847f05ba165
Looly <loolly@aliyun.com>
2023-05-25 17:55:51 +0800

    add test

---
+++ hutool-extra/src/test/java/org/dromara/hutool/extra/qrcode/IssuesI76SZBTest.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2023 looly(loolly@aliyun.com)
+ * Hutool is licensed under Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan PSL v2.
+ * You may obtain a copy of Mulan PSL v2 at:
+ *          http://license.coscl.org.cn/MulanPSL2
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+ * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ * See the Mulan PSL v2 for more details.
+ */
+
+package org.dromara.hutool.extra.qrcode;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+
+public class IssuesI76SZBTest {
+	@Test
+	@Disabled
+	void generateTest() {
+		final QrConfig qrConfig = new QrConfig(300, 300);
+		final File file = new File("d:/test/out.png");
+		qrConfig.setImg(new File("d:/test/b2dd3614_868440.png"));
+		QrCodeUtil.generate("111", qrConfig, file);
+	}
+}
