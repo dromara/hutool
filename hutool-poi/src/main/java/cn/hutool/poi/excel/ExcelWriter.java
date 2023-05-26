c07a3d94bfb20863cce5bf0e36e7005d05826ffd
hower <whoishower@gmail.com>
2023-05-26 12:49:14 +0800

    添加writeCellValue的重载，以支持isHeader

--- hutool-poi/src/main/java/cn/hutool/poi/excel/ExcelWriter.java
+++ hutool-poi/src/main/java/cn/hutool/poi/excel/ExcelWriter.java
@@ -889,7 +889,7 @@ public class ExcelWriter extends ExcelBase<ExcelWriter> {
 	public ExcelWriter writeImg(File imgFile, int imgType, int dx1, int dy1, int dx2,
 								int dy2, int col1, int row1, int col2, int row2) {
 		return writeImg(FileUtil.readBytes(imgFile), imgType, dx1,
-			dy1, dx2, dy2, col1, row1, col2, row2);
+				dy1, dx2, dy2, col1, row1, col2, row2);
 	}
 
 	/**
@@ -1105,22 +1105,49 @@ public class ExcelWriter extends ExcelBase<ExcelWriter> {
 	 * @since 5.1.4
 	 */
 	public ExcelWriter writeCellValue(String locationRef, Object value) {
-		final CellLocation cellLocation = ExcelUtil.toLocation(locationRef);
-		return writeCellValue(cellLocation.getX(), cellLocation.getY(), value);
+		return writeCellValue(locationRef, value, false);
 	}
 
 	/**
 	 * 给指定单元格赋值，使用默认单元格样式
 	 *
-	 * @param x     X坐标，从0计数，即列号
-	 * @param y     Y坐标，从0计数，即行号
-	 * @param value 值
+	 * @param locationRef 单元格地址标识符，例如A11，B5
+	 * @param value       值
+	 * @param isHeader    是否为Header
+	 * @return this
+	 * @since 5.1.4
+	 */
+	public ExcelWriter writeCellValue(String locationRef, Object value, boolean isHeader) {
+		final CellLocation cellLocation = ExcelUtil.toLocation(locationRef);
+		return writeCellValue(cellLocation.getX(), cellLocation.getY(), value, isHeader);
+	}
+
+	/**
+	 * 给指定单元格赋值，使用默认单元格样式
+	 *
+	 * @param x        X坐标，从0计数，即列号
+	 * @param y        Y坐标，从0计数，即行号
+	 * @param value    值
 	 * @return this
 	 * @since 4.0.2
 	 */
 	public ExcelWriter writeCellValue(int x, int y, Object value) {
+		return writeCellValue(x, y, value, false);
+	}
+
+	/**
+	 * 给指定单元格赋值，使用默认单元格样式
+	 *
+	 * @param x        X坐标，从0计数，即列号
+	 * @param y        Y坐标，从0计数，即行号
+	 * @param value    值
+	 * @param isHeader 是否为Header
+	 * @return this
+	 * @since 4.0.2
+	 */
+	public ExcelWriter writeCellValue(int x, int y, Object value, boolean isHeader) {
 		final Cell cell = getOrCreateCell(x, y);
-		CellUtil.setCellValue(cell, value, this.styleSet, false);
+		CellUtil.setCellValue(cell, value, this.styleSet, isHeader);
 		return this;
 	}
 
