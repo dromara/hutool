7fb0529cd36348d0c8bb8bcac8ef860ccd57d8c6
kite <drgonroot@gamil.com>
2023-07-12 21:13:04 +0800

    fix 调整注释样式

--- hutool-extra/src/main/java/cn/hutool/extra/spring/requestjson/RequestJsonAutoConfiguration.java
+++ hutool-extra/src/main/java/cn/hutool/extra/spring/requestjson/RequestJsonAutoConfiguration.java
@@ -19,16 +19,23 @@ import java.text.SimpleDateFormat;
 import java.util.List;
 
 /**
- * @RequestJson 和 @RequestJsonParam 注解使用
+ * {@code @RequestJson}和{@code @RequestJsonParam}注解使用
  * 对于body content-type:application/x-www-form-urlencoded 可以通过 @RequestParam来获取对应值。
  * 但是对于body content-type:application/json 目前需要声明对应的类和@ReqeustBody来获取对应值。
  * 对于不同接口参数需要声明不同类来解决，且粒度类很大。
  * 因此RequestJsonAutoConfiguration旨在解决对应问题，可以通过使用@RequestJsonParam获取指定key-value，粒度更小，类似RequestParam获取对应值。
  * example:
  * json内容：{"a":"a", "b":"{c:1, "d":2}", "f":"{"g":2, "h":"1"}"}
- * public void test(@RequestJson JsonNode jsonNode, @RequestJsonParam(value="a", required=true) String a, @RequestJsonParam(value="b.c", required=false) Integer b, @RequestJsonParam(value="f") Object f1, @RequestJsonParam(value="f") Map<String, Object> f2, @RequestJsonParam(value="f") F f3) {
- *     return;
+ * <blockquote><pre>
+ * public void test({@code @RequestJson} JsonNode jsonNode,
+ * 					{@code @RequestJsonParam}(value="a", required=true) String a,
+ * 					{@code @RequestJsonParam}(value="b.c", required=false) Integer b,
+ * 					{@code @RequestJsonParam}(value="f") Object f1,
+ * 					{@code @RequestJsonParam}(value="f") {@code Map<String, Object>} f2,
+ * 					{@code @RequestJsonParam}(value="f") F f3) {
+ * 					return;
  * }
+ * </pre></blockquote><p>
  * Created by useheart on 2023/7/11
  * @author useheart
  */
@@ -45,6 +52,7 @@ public class RequestJsonAutoConfiguration implements WebMvcConfigurer, Applicati
 
 	/**
 	 * 如果需要提高ObjectMapper反序列化的能力，可以在外部配置类重新定义
+	 * @return objectmapper
 	 * */
 	@Bean(name = RequestJsonAutoConfiguration.OBJECT_MAPPER_BEAN_NAME)
 	@ConditionalOnMissingBean(name = RequestJsonAutoConfiguration.OBJECT_MAPPER_BEAN_NAME)
