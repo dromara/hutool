5889435e0998a4159f4c219ac001a44d50070b8c
Looly <loolly@aliyun.com>
2023-07-29 14:01:49 +0800

    fix test

--- hutool-extra/src/test/java/cn/hutool/extra/expression/AviatorTest.java
+++ hutool-extra/src/test/java/cn/hutool/extra/expression/AviatorTest.java
@@ -21,17 +21,17 @@ public class AviatorTest {
 		ExpressionEngine engine = new AviatorEngine();
 		String exp =
 				"\"[foo i=\"+ foo.i + \", f=\" + foo.f + \", date.year=\" + (foo.date.year+1900) + \", date.month=\" + foo.date.month + \", bars[0].name=\" + #foo.bars[0].name + \"]\"";
-		String result = (String) engine.eval(exp, Dict.create().set("foo", foo));
+		String result = (String) engine.eval(exp, Dict.create().set("foo", foo), null);
 		Assert.assertEquals("[foo i=100, f=3.14, date.year=2020, date.month=10, bars[0].name=bar]", result);
 
 		// Assignment.
 		exp = "#foo.bars[0].name='hello aviator' ; #foo.bars[0].name";
-		result = (String) engine.eval(exp, Dict.create().set("foo", foo));
+		result = (String) engine.eval(exp, Dict.create().set("foo", foo), null);
 		Assert.assertEquals("hello aviator", result);
 		Assert.assertEquals("hello aviator", foo.bars[0].getName());
 
 		exp = "foo.bars[0] = nil ; foo.bars[0]";
-		result = (String) engine.eval(exp, Dict.create().set("foo", foo));
+		result = (String) engine.eval(exp, Dict.create().set("foo", foo), null);
 		Console.log("Execute expression: " + exp);
 		Assert.assertNull(result);
 		Assert.assertNull(foo.bars[0]);
