a42c8dbd9957034a2d3a0bce12b10e8bbacaf989
Looly <loolly@aliyun.com>
2023-08-04 15:41:19 +0800

    修复SqlUtil.formatSql 格式化的sql换行异常

--- hutool-db/src/test/java/cn/hutool/db/sql/SqlFormatterTest.java
+++ hutool-db/src/test/java/cn/hutool/db/sql/SqlFormatterTest.java
@@ -1,5 +1,6 @@
 package cn.hutool.db.sql;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class SqlFormatterTest {
@@ -8,21 +9,23 @@ public class SqlFormatterTest {
 	public void formatTest(){
 		// issue#I3XS44@Gitee
 		// 测试是否空指针错误
-		String sql = "(select 1 from dual) union all (select 1 from dual)";
+		final String sql = "(select 1 from dual) union all (select 1 from dual)";
 		SqlFormatter.format(sql);
 	}
 
 	@Test
+	@Ignore
 	public void testKeyword() {
-		String sql = "select * from `order`";
-		String format = SqlFormatter.format(sql);
+		final String sql = "select * from `order`";
+		final String format = SqlFormatter.format(sql);
 		System.out.println(format);
 	}
 
 	@Test
+	@Ignore
 	public void testSqlBuilderFormat() {
-		String sql = "SELECT `link_table_a`.`value_a` AS `link_table_a.value_a`,`link_table_a`.`id` AS `link_table_a.id`,`link_table_b`.`value_b` AS `link_table_b.value_b`,`link_table_c`.`id` AS `link_table_c.id`,`link_table_b`.`id` AS `link_table_b.id`,`link_table_c`.`value_c` AS `link_table_c.value_c` FROM `link_table_a` INNER JOIN `link_table_b` ON `link_table_a`.`table_b_id` = `link_table_b`.`id` INNER JOIN `link_table_c` ON `link_table_b`.`table_c_id` = `link_table_c`.`id`";
-		String format = SqlBuilder.of(sql).format().build();
+		final String sql = "SELECT `link_table_a`.`value_a` AS `link_table_a.value_a`,`link_table_a`.`id` AS `link_table_a.id`,`link_table_b`.`value_b` AS `link_table_b.value_b`,`link_table_c`.`id` AS `link_table_c.id`,`link_table_b`.`id` AS `link_table_b.id`,`link_table_c`.`value_c` AS `link_table_c.value_c` FROM `link_table_a` INNER JOIN `link_table_b` ON `link_table_a`.`table_b_id` = `link_table_b`.`id` INNER JOIN `link_table_c` ON `link_table_b`.`table_c_id` = `link_table_c`.`id`";
+		final String format = SqlBuilder.of(sql).format().build();
 		System.out.println(format);
 	}
 }
