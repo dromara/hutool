d5ac761f089ea85bfb79694237d206b077f74649
Looly <loolly@aliyun.com>
2023-08-07 18:26:12 +0800

    add test

---
+++ hutool-core/src/test/java/org/dromara/hutool/core/math/IssueI7R2B6Test.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2023 looly(loolly@aliyun.com)
+ * Hutool is licensed under Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan PSL v2.
+ * You may obtain a copy of Mulan PSL v2 at:
+ *          http://license.coscl.org.cn/MulanPSL2
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+ * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ * See the Mulan PSL v2 for more details.
+ */
+
+package org.dromara.hutool.core.math;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+public class IssueI7R2B6Test {
+	@Test
+	void divTest() {
+		Assertions.assertEquals(new BigDecimal("61.67"),
+			NumberUtil.div(NumberUtil.mul(15858155520D, 100), 25715638272D, 2));
+	}
+}
