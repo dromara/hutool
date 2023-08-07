be3312d2e095dc084530fe8d76a587b414a7c12f
Looly <loolly@aliyun.com>
2023-08-07 11:13:29 +0800

    修复DateUtil.parse 给定一个时间解析错误问题

---
+++ hutool-core/src/test/java/cn/hutool/core/date/IssueI7QI6RTest.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2023 looly(loolly@aliyun.com)
+ * Hutool is licensed under Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan PSL v2.
+ * You may obtain a copy of Mulan PSL v2 at:
+ *          http://license.coscl.org.cn/MulanPSL2
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+ * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ * See the Mulan PSL v2 for more details.
+ */
+
+package cn.hutool.core.date;
+
+import org.junit.Test;
+
+public class IssueI7QI6RTest {
+	@Test(expected = DateException.class)
+	public void parseTest() {
+		DateUtil.parse("2023-08-04 1");
+	}
+
+	@Test(expected = DateException.class)
+	public void parseTest2() {
+		DateUtil.parse("2023-08-04-1");
+	}
+}
