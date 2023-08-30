a3efec9c52e0a39a2a5ff89cab0373bbd072e31f
Looly <loolly@aliyun.com>
2023-08-30 21:57:28 +0800

    fix bug

--- hutool-json/src/main/java/org/dromara/hutool/json/JSONArray.java
+++ hutool-json/src/main/java/org/dromara/hutool/json/JSONArray.java
@@ -456,12 +456,13 @@ public class JSONArray implements JSON, JSONGetter<Integer>, List<Object>, Rando
 			}
 			this.rawList.add(index, InternalJSONUtil.wrap(element, this.config));
 		} else {
-			if (!config.isIgnoreNullValue()) {
-				while (index != this.size()) {
-					// 非末尾，则填充null
-					this.add(null);
-				}
-			}
+			// issue#3286, 如果用户指定的index太大，容易造成Java heap space错误。
+//			if (!config.isIgnoreNullValue()) {
+//				while (index != this.size()) {
+//					// 非末尾，则填充null
+//					this.add(null);
+//				}
+//			}
 			this.add(element);
 		}
 
