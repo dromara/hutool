01cb9bdd26de80752e980704852f4515597745a5
Looly <loolly@aliyun.com>
2023-09-21 11:37:56 +0800

    add test

--- hutool-http/src/test/java/cn/hutool/http/Issue3314Test.java
+++ hutool-http/src/test/java/cn/hutool/http/Issue3314Test.java
@@ -1,10 +1,21 @@
 package cn.hutool.http;
 
+import cn.hutool.core.io.FileUtil;
+import cn.hutool.core.lang.Console;
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class Issue3314Test {
 	@Test
+	@Ignore
 	public void postTest() {
 		String url = "https://hutool.cn/test/getList";
+		final String body = HttpRequest.get(url)
+			.setRest(true)
+			.body(FileUtil.readBytes("d:/test/3314.xlsx"))
+			.execute()
+			.body();
+
+		Console.log(body);
 	}
 }
