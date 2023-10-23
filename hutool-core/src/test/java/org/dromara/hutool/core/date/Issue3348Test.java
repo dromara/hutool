33ca89ce7d426d5900573626905b334688ef521a
Looly <loolly@aliyun.com>
2023-10-23 20:09:40 +0800

    add test

---
+++ hutool-core/src/test/java/org/dromara/hutool/core/date/Issue3348Test.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2023. looly(loolly@aliyun.com)
+ * Hutool is licensed under Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan PSL v2.
+ * You may obtain a copy of Mulan PSL v2 at:
+ *          https://license.coscl.org.cn/MulanPSL2
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+ * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ * See the Mulan PSL v2 for more details.
+ */
+
+package org.dromara.hutool.core.date;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class Issue3348Test {
+	@Test
+	void formatChineseDateTest() {
+		final String formatChineseDate = DateUtil.formatChineseDate(
+			DateUtil.parse("2023-10-23"), true, false);
+		Assertions.assertEquals("二〇二三年十月二十三日", formatChineseDate);
+	}
+}
