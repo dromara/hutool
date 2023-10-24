7d80f1aa1494a2f2a0427aff2890d57e86218f09
Looly <loolly@aliyun.com>
2023-10-25 00:21:35 +0800

    修复SimpleServer在添加的HttpFilter中有获取请求参数时报错问题

--- hutool-http/src/main/java/cn/hutool/http/server/handler/ActionHandler.java
+++ hutool-http/src/main/java/cn/hutool/http/server/handler/ActionHandler.java
@@ -1,5 +1,6 @@
 package cn.hutool.http.server.handler;
 
+import cn.hutool.http.server.HttpExchangeWrapper;
 import cn.hutool.http.server.HttpServerRequest;
 import cn.hutool.http.server.HttpServerResponse;
 import cn.hutool.http.server.action.Action;
@@ -29,10 +30,18 @@ public class ActionHandler implements HttpHandler {
 
 	@Override
 	public void handle(HttpExchange httpExchange) throws IOException {
-		action.doAction(
-				new HttpServerRequest(httpExchange),
-				new HttpServerResponse(httpExchange)
-		);
+		final HttpServerRequest request;
+		final HttpServerResponse response;
+		if (httpExchange instanceof HttpExchangeWrapper) {
+			// issue#3343 当使用Filter时，可能读取了请求参数，此时使用共享的req和res，可复用缓存
+			final HttpExchangeWrapper wrapper = (HttpExchangeWrapper) httpExchange;
+			request = wrapper.getRequest();
+			response = wrapper.getResponse();
+		} else {
+			request = new HttpServerRequest(httpExchange);
+			response = new HttpServerResponse(httpExchange);
+		}
+		action.doAction(request, response);
 		httpExchange.close();
 	}
 }
