c529fa129fd45b6d22ab2fee22fdbb6aad073f14
Looly <loolly@aliyun.com>
2023-12-06 17:26:51 +0800

    fix test

--- hutool-core/src/test/java/org/dromara/hutool/core/bean/IssueI8M38TTest.java
+++ hutool-core/src/test/java/org/dromara/hutool/core/bean/IssueI8M38TTest.java
@@ -13,6 +13,7 @@
 package org.dromara.hutool.core.bean;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import org.dromara.hutool.core.bean.copier.CopyOptions;
 import org.dromara.hutool.core.collection.ListUtil;
 import org.dromara.hutool.core.map.MapUtil;
@@ -34,15 +35,32 @@ public class IssueI8M38TTest {
 
 		final HashMap<String, String> mapping = MapUtil.of("subName", "name");
 		final List<Person> people = BeanUtil.copyToList(subPeople, Person.class,
+			// 不覆盖模式下，当第一次subName已经拷贝到值，后续不再覆盖
+			CopyOptions.of().setFieldMapping(mapping).setOverride(false));
+
+		Assertions.assertEquals(subPerson.getSubName(), people.get(0).getName());
+	}
+
+	@Test
+	void copyListByMappingTest2() {
+		final SubPerson subPerson = new SubPerson();
+		subPerson.setAge(14);
+		subPerson.setOpenid(112233);
+		subPerson.setSubName("sub名字");
+		final ArrayList<SubPerson> subPeople = ListUtil.of(subPerson);
+
+		final HashMap<String, String> mapping = MapUtil.of("subName", "name");
+		// subName复制到name后，name字段继续拷贝，覆盖了subName的值
+		mapping.put("name", "aaa");
+		final List<Person> people = BeanUtil.copyToList(subPeople, Person.class,
 			CopyOptions.of().setFieldMapping(mapping));
 
 		Assertions.assertEquals(subPerson.getSubName(), people.get(0).getName());
 	}
 
+	@EqualsAndHashCode(callSuper = true)
 	@Data
-	static class SubPerson {
-		private int age;
-		private int openid;
+	static class SubPerson extends Person{
 		private String subName;
 	}
 
