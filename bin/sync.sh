8addd4a652d3b03bdf718de49f09143dab8d941f
Looly <loolly@aliyun.com>
2023-12-26 00:03:30 +0800

    add sync sh

---
+++ bin/sync.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#
+# Copyright (c) 2023 looly(loolly@aliyun.com)
+# Hutool is licensed under Mulan PSL v2.
+# You can use this software according to the terms and conditions of the Mulan PSL v2.
+# You may obtain a copy of Mulan PSL v2 at:
+#          https://license.coscl.org.cn/MulanPSL2
+# THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+# EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+# MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+# See the Mulan PSL v2 for more details.
+#
+
+# show Hutool logo
+"$(dirname ${BASH_SOURCE[0]})"/logo.sh
+
+git checkout v6-dev
+git pull osc v6-dev
