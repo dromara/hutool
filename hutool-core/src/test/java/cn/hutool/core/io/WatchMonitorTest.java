c68b9df0dfb131bb3e5a243abe75ac5b50663f5e
Looly <loolly@aliyun.com>
2023-12-29 00:19:48 +0800

    WatchServer新增通过Path获取WatchKey方法

--- hutool-core/src/test/java/cn/hutool/core/io/WatchMonitorTest.java
+++ hutool-core/src/test/java/cn/hutool/core/io/WatchMonitorTest.java
@@ -1,16 +1,16 @@
 package cn.hutool.core.io;
 
-import java.nio.file.Path;
-import java.nio.file.WatchEvent;
-import java.nio.file.WatchKey;
-import java.nio.file.Watchable;
-
 import cn.hutool.core.io.watch.SimpleWatcher;
 import cn.hutool.core.io.watch.WatchMonitor;
 import cn.hutool.core.io.watch.Watcher;
 import cn.hutool.core.io.watch.watchers.DelayWatcher;
 import cn.hutool.core.lang.Console;
+import org.junit.Ignore;
 import org.junit.Test;
+
+import java.nio.file.Path;
+import java.nio.file.WatchEvent;
+import java.nio.file.WatchKey;
 
 /**
  * 文件监听单元测试
@@ -21,36 +21,37 @@ public class WatchMonitorTest {
 	WatchMonitor monitor;
 	Watcher watcher = new SimpleWatcher() {
 		@Override
-		public void onCreate(WatchEvent<?> event, Path currentPath) {
-			Object obj = event.context();
+		public void onCreate(final WatchEvent<?> event, final Path currentPath) {
+			final Object obj = event.context();
 			Console.log("创建：{}-> {}", currentPath, obj);
-			WatchKey watchKey = monitor.getWatchKey(currentPath);
-			Path watchable = (Path) watchKey.watchable();
-			Path fullPath = watchable.resolve((Path) event.context());
+			final WatchKey watchKey = monitor.getWatchKey(currentPath);
+			final Path watchable = (Path) watchKey.watchable();
+			final Path fullPath = watchable.resolve((Path) event.context());
 			Console.log("Path 完整对象：{}", fullPath);
 		}
 
 		@Override
-		public void onModify(WatchEvent<?> event, Path currentPath) {
-			Object obj = event.context();
+		public void onModify(final WatchEvent<?> event, final Path currentPath) {
+			final Object obj = event.context();
 			Console.log("修改：{}-> {}", currentPath, obj);
 		}
 
 		@Override
-		public void onDelete(WatchEvent<?> event, Path currentPath) {
-			Object obj = event.context();
+		public void onDelete(final WatchEvent<?> event, final Path currentPath) {
+			final Object obj = event.context();
 			Console.log("删除：{}-> {}", currentPath, obj);
 		}
 
 		@Override
-		public void onOverflow(WatchEvent<?> event, Path currentPath) {
-			Object obj = event.context();
+		public void onOverflow(final WatchEvent<?> event, final Path currentPath) {
+			final Object obj = event.context();
 			Console.log("Overflow：{}-> {}", currentPath, obj);
 		}
 	};
 
 
 	@Test
+	@Ignore
 	public void testFile() {
 
 		monitor = WatchMonitor.createAll("d:/test/aaa.txt", new DelayWatcher(watcher, 500));
@@ -60,9 +61,9 @@ public class WatchMonitorTest {
 	}
 
 	@Test
+	@Ignore
 	public void testDir() {
 		monitor = WatchMonitor.createAll("d:/", new DelayWatcher(watcher, 500));
-
 		monitor.run();
 	}
 
