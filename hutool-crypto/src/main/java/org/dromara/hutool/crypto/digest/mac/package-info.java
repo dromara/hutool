94dc35a2e0dac725dc2d9c54dba2c3f62435c42e
Looly <loolly@aliyun.com>
2024-01-08 23:52:58 +0800

    fix code

--- hutool-crypto/src/main/java/org/dromara/hutool/crypto/digest/mac/package-info.java
+++ hutool-crypto/src/main/java/org/dromara/hutool/crypto/digest/mac/package-info.java
@@ -22,7 +22,7 @@
  * HMAC 可以与任何迭代散列函数捆绑使用。MD5 和 SHA-1 就是这种散列函数。HMAC 还可以使用一个用于计算和确认消息鉴别值的密钥。
  * </p>
  *
- * <pre>
+ * <pre>{@code
  *         MacEngineFactory
  *               ||(创建)
  *           MacEngine----------------（包装）-----------------> Mac
@@ -33,7 +33,7 @@
  *                   BCHMacEngine  CBCBlockCipherMacEngine
  *                                          |
  *                                     SM4MacEngine
- * </pre>
+ * }</pre>
  *
  * 通过MacEngine，封装支持了BouncyCastle和JCE实现的一些MAC算法，通过MacEngineFactory自动根据算法名称创建对应对象。
  *
