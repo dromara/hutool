ce6e9bd931b444c05c43d865ed0ee1fe51647011
Looly <loolly@aliyun.com>
2024-02-03 11:37:44 +0800

    fix test

--- hutool-poi/src/test/java/org/dromara/hutool/poi/excel/IssueI8ZIQCTest.java
+++ hutool-poi/src/test/java/org/dromara/hutool/poi/excel/IssueI8ZIQCTest.java
@@ -1,9 +1,11 @@
 package org.dromara.hutool.poi.excel;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 public class IssueI8ZIQCTest {
 	@Test
+	@Disabled
 	void readTest() {
 		final ExcelReader reader = ExcelUtil.getReader("d:/test/test3.xlsx", "aaa");
 	}
