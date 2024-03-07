fbb97f0c9add332c7a65d0fe9430da15c6c58142
Looly <loolly@aliyun.com>
2024-03-07 12:20:34 +0800

    fix code

--- hutool-core/src/main/java/org/dromara/hutool/core/func/LambdaFactory.java
+++ hutool-core/src/main/java/org/dromara/hutool/core/func/LambdaFactory.java
@@ -27,7 +27,7 @@ import java.lang.reflect.Method;
 import java.util.Map;
 
 /**
- * 以类似反射的方式动态创建Lambda，在性能上有一定优势，同时避免每次调用Lambda时创建匿名内部类<br>
+ * 以类似反射的方式动态创建Lambda，在性能上有一定优势，同时避免每次调用Lambda时创建匿名内部类
  * TODO JDK9+存在兼容问题，当参数为原始类型时报错
  *
  * @author nasodaengineer
