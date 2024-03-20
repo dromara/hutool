6ec135486c0aab03a2fab6c7f6c4c34412aa6a8c
Looly <loolly@aliyun.com>
2024-03-20 16:41:35 +0800

    fix code

--- hutool-core/src/main/java/cn/hutool/core/map/ReferenceConcurrentMap.java
+++ hutool-core/src/main/java/cn/hutool/core/map/ReferenceConcurrentMap.java
@@ -177,6 +177,7 @@ public class ReferenceConcurrentMap<K, V> implements ConcurrentMap<K, V>, Iterab
 
 	@Override
 	public Set<K> keySet() {
+		this.purgeStaleKeys();
 		// TODO 非高效方式的set转换，应该返回一个view
 		final Collection<K> trans = CollUtil.trans(this.raw.keySet(), (reference) -> null == reference ? null : reference.get());
 		return new HashSet<>(trans);
