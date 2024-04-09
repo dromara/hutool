008dcb3f21cf0a6e9eb1bd47febcd726d06a1621
Looly <loolly@aliyun.com>
2024-04-09 18:06:31 +0800

    fix test

--- hutool-http/src/test/java/org/dromara/hutool/http/Issue3536Test.java
+++ hutool-http/src/test/java/org/dromara/hutool/http/Issue3536Test.java
@@ -1,5 +1,7 @@
 package org.dromara.hutool.http;
 
+import org.dromara.hutool.core.net.url.UrlQuery;
+import org.dromara.hutool.core.net.url.UrlQueryUtil;
 import org.dromara.hutool.core.util.CharsetUtil;
 import org.junit.Assert;
 import org.junit.Test;
@@ -20,4 +22,13 @@ public class Issue3536Test {
 		Assert.assertEquals("https://hutool.cn/test?scope=a,b,c%E4%BD%A0&redirect_uri=https://api.hutool.cn/v1/test", s);
 	}
 
+	@Test
+	public void toQueryTest() {
+		final Map<String, Object> paramMap = new HashMap<>();
+		paramMap.put("redirect_uri", "https://api.hutool.cn/v1/test");
+		paramMap.put("scope", "a,b,c你");
+
+		final String params = UrlQueryUtil.toQuery(paramMap, CharsetUtil.UTF_8, UrlQuery.EncodeMode.STRICT);
+		Assert.assertEquals("scope=a%2Cb%2Cc%E4%BD%A0&redirect_uri=https%3A%2F%2Fapi.hutool.cn%2Fv1%2Ftest", params);
+	}
 }
