1af2366c2e2a116d5cc84497a2c019fb62d124e3
Looly <loolly@aliyun.com>
2024-04-22 13:14:57 +0800

    add test

--- hutool-db/src/test/java/org/dromara/hutool/db/IssueI9BANETest.java
+++ hutool-db/src/test/java/org/dromara/hutool/db/IssueI9BANETest.java
@@ -1,5 +1,10 @@
 package org.dromara.hutool.db;
 
+import org.dromara.hutool.core.lang.Console;
+import org.dromara.hutool.db.ds.DSUtil;
+import org.dromara.hutool.db.ds.DSWrapper;
+import org.dromara.hutool.db.meta.MetaUtil;
+import org.dromara.hutool.db.meta.Table;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
@@ -8,5 +13,10 @@ public class IssueI9BANETest {
 	@Disabled
 	void metaTest() {
 		final Db db = Db.of("orcl");
+		db.find(Entity.of("\"1234\""));
+
+		final DSWrapper ds = DSUtil.getDS("orcl");
+		final Table tableMeta = MetaUtil.getTableMeta(ds, null, null, "\"1234\"");
+		Console.log(tableMeta.getIndexInfoList());
 	}
 }
