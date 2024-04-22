f1aab33dc111c55da0de3699060057ecc2ec3887
Looly <loolly@aliyun.com>
2024-04-22 13:15:28 +0800

    add test

---
+++ hutool-db/src/test/java/cn/hutool/db/IssueI9BANETest.java
@@ -0,0 +1,24 @@
+package cn.hutool.db;
+
+import cn.hutool.core.lang.Console;
+import cn.hutool.db.ds.DSFactory;
+import cn.hutool.db.meta.MetaUtil;
+import cn.hutool.db.meta.Table;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
+
+public class IssueI9BANETest {
+	@Test
+	@Ignore
+	public void metaTest() throws SQLException {
+		final Db db = Db.use("orcl");
+		db.find(Entity.create("\"1234\""));
+
+		final DataSource ds = DSFactory.get("orcl");
+		final Table tableMeta = MetaUtil.getTableMeta(ds, null, null, "\"1234\"");
+		Console.log(tableMeta.getIndexInfoList());
+	}
+}
