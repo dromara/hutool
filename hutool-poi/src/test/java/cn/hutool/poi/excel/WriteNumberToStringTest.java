829fecdc38223a974fba55836179fa28931483c9
Looly <loolly@aliyun.com>
2024-05-08 17:17:28 +0800

    fix test

--- hutool-poi/src/test/java/cn/hutool/poi/excel/WriteNumberToStringTest.java
+++ hutool-poi/src/test/java/cn/hutool/poi/excel/WriteNumberToStringTest.java
@@ -1,11 +1,7 @@
 package cn.hutool.poi.excel;
 
 import cn.hutool.core.collection.ListUtil;
-import org.apache.poi.ss.usermodel.BuiltinFormats;
-import org.apache.poi.ss.usermodel.CellStyle;
-import org.apache.poi.ss.usermodel.IgnoredErrorType;
-import org.apache.poi.ss.util.CellRangeAddress;
-import org.apache.poi.xssf.usermodel.XSSFSheet;
+import cn.hutool.poi.excel.cell.FormulaCellValue;
 import org.junit.Ignore;
 import org.junit.Test;
 
@@ -13,15 +9,11 @@ public class WriteNumberToStringTest {
 	@Test
 	@Ignore
 	public void writeNumberTest() {
-		final ExcelWriter writer = ExcelUtil.getWriter("d:/test/dataWithNumber.xlsx");
-		final XSSFSheet sheet = (XSSFSheet) writer.getSheet();
-		sheet.addIgnoredErrors(new CellRangeAddress(0, 100, 0, 100), IgnoredErrorType.NUMBER_STORED_AS_TEXT);
-		final CellStyle cellStyle = writer.getStyleSet().getCellStyle();
-		cellStyle.setDataFormat((short) BuiltinFormats.getBuiltinFormat("TEXT"));
+		final ExcelWriter writer = ExcelUtil.getBigWriter("d:/test/dataWithNumber.xlsx");
 
 		writer.writeRow(ListUtil.of("姓名", "编号"));
-		writer.writeRow(ListUtil.of("张三", "010001"));
-		writer.writeRow(ListUtil.of("李四", "120001"));
+		writer.writeRow(ListUtil.of("张三", new FormulaCellValue("010001")));
+		writer.writeRow(ListUtil.of("李四", new FormulaCellValue("120001")));
 		writer.writeRow(ListUtil.of("王五", 123456));
 
 		writer.close();
