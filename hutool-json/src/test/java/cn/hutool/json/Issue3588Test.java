fe01021c72949a2b13ecb54364f5ce0181edde09
Looly <loolly@aliyun.com>
2024-05-17 16:46:01 +0800

    add test

---
+++ hutool-json/src/test/java/cn/hutool/json/Issue3588Test.java
@@ -0,0 +1,35 @@
+package cn.hutool.json;
+
+import cn.hutool.core.lang.Console;
+import lombok.Data;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Issue3588Test {
+
+	@Test
+	public void toBeanIgnoreCaseTest() {
+		String json = "{id: 1, code: 1122, tsemaphores: [{type: 1, status: 12}]}";
+		AttrData attrData = JSONUtil.toBean(json, JSONConfig.create().setIgnoreCase(true), AttrData.class);
+		Assert.assertEquals("1", attrData.getId());
+		Assert.assertEquals("1122", attrData.getCode());
+		Assert.assertEquals("1", attrData.getTSemaphores().get(0).getType());
+		Assert.assertEquals("12", attrData.getTSemaphores().get(0).getStatus());
+	}
+
+	@Data
+	static class AttrData {
+		private String id;
+		private String code;
+		private List<TSemaphore> tSemaphores = new ArrayList<>();
+	}
+
+	@Data
+	static class TSemaphore{
+		private String type;
+		private String status;
+	}
+}
