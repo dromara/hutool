e117489d7aa94e96ae447ed6bad18e094adf94d0
Looly <loolly@aliyun.com>
2024-06-04 08:56:33 +0800

    add test

---
+++ hutool-core/src/test/java/cn/hutool/core/util/IssueI9UK5VTest.java
@@ -0,0 +1,19 @@
+package cn.hutool.core.util;
+
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class IssueI9UK5VTest {
+	@Test
+	public void splitTest() {
+		String str = "";
+		List<String> split = StrUtil.split(str, ',');
+		assertEquals(1, split.size());
+
+		split = StrUtil.splitTrim(str, ',');
+		assertEquals(0, split.size());
+	}
+}
