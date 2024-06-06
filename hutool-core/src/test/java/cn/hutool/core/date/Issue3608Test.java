99cf24267fc3ae0f0708f4feafd411c25c49cef8
Looly <loolly@aliyun.com>
2024-06-06 21:42:46 +0800

    add test

---
+++ hutool-core/src/test/java/cn/hutool/core/date/Issue3608Test.java
@@ -0,0 +1,22 @@
+package cn.hutool.core.date;
+
+import cn.hutool.core.lang.Console;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
+public class Issue3608Test {
+	@Test
+	@Ignore
+	public void parseTest() throws ParseException {
+		String dateTime = "1940-06-01 00:00:00";
+		final SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		simpleDateFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
+		final Date parse = simpleDateFormat.parse(dateTime);
+		Console.log(parse);
+	}
+}
