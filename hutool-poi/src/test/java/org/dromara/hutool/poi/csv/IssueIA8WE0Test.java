0662e77c0aaf172c84b894bcaea4d1ab7ddc45fa
Looly <loolly@aliyun.com>
2024-07-01 15:59:42 +0800

    修复\n#出现在双引号中解析错误问题

---
+++ hutool-poi/src/test/java/org/dromara/hutool/poi/csv/IssueIA8WE0Test.java
@@ -0,0 +1,29 @@
+package org.dromara.hutool.poi.csv;
+
+import org.dromara.hutool.core.io.file.FileUtil;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * https://gitee.com/dromara/hutool/issues/IA8WE0
+ */
+public class IssueIA8WE0Test {
+	@Test
+	void csvReadTest() {
+		final CsvReader csvReader = new CsvReader();
+		final CsvData read = csvReader.read(FileUtil.file("issueIA8WE0.csv"));
+		final List<CsvRow> rows = read.getRows();
+
+		assertEquals(1, rows.size());
+		assertEquals(3, rows.get(0).size());
+		assertEquals("c1_text1", rows.get(0).get(0));
+		// 如果\n#出现在双引号中，表示实际的文本内容，并不算注释
+		assertEquals("c1_text2\n#c1_text2_line2", rows.get(0).get(1));
+		assertEquals("c1_text3", rows.get(0).get(2));
+
+		csvReader.close();
+	}
+}
