b330e576714c0df3b7772b06189d9111f193d1be
mochouZero555 <kankankanfuke@gmail.com>
2024-07-24 20:53:58 +0800

    add benchMark

--- hutool-core/src/test/java/cn/hutool/core/comparator/IndexedComparatorTest.java
+++ hutool-core/src/test/java/cn/hutool/core/comparator/IndexedComparatorTest.java
@@ -1,6 +1,7 @@
 package cn.hutool.core.comparator;
 
 import cn.hutool.core.collection.CollectionUtil;
+import cn.hutool.core.date.StopWatch;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.junit.Assert;
@@ -17,7 +18,8 @@ public class IndexedComparatorTest {
 		final Object[] arr ={"a", "b", new User("9", null), "1",3,null,"2"};
 		final Collection<Object> set = new HashSet<>(Arrays.asList(arr));
 
-		final List<Object> sortSet = CollectionUtil.sort(set, new IndexedComparator<>(arr));
+
+		final List<Object> sortSet = CollectionUtil.sort(set, new ArrayIndexedComparator<>(arr));
 
 		Assert.assertEquals("a", sortSet.get(0));
 		Assert.assertEquals( new User("9", null), sortSet.get(2));
@@ -30,12 +32,35 @@ public class IndexedComparatorTest {
 		final Object[] arr ={"a", "b", new User("9", null), "1",3,null,"2"};
 		final Collection<Object> set = new HashSet<>(Arrays.asList(arr));
 
-		final List<Object> sortSet = CollectionUtil.sort(set, new IndexedComparator<>(arr).reversed());
+		final List<Object> sortSet = CollectionUtil.sort(set, new ArrayIndexedComparator<>(arr).reversed());
 
 		Assert.assertEquals("a", sortSet.get(6));
 		Assert.assertNull(sortSet.get(1));
 		Assert.assertEquals( new User("9", null), sortSet.get(4));
 		Assert.assertEquals(3, sortSet.get(2));
+	}
+
+	@Test
+	public void benchMarkSortTest() {
+		final Object[] arr ={"a", "b", new User("9", null), "1",3,null,"2"};
+		final Collection<Object> set = new HashSet<>(Arrays.asList(arr));
+
+		final StopWatch stopWatch = new StopWatch();
+
+		stopWatch.start();
+		for (int i = 0; i < 10_000_000; i++) {
+			final List<Object> sortSet = CollectionUtil.sort(set, new IndexedComparator<>(arr));
+		}
+		stopWatch.stop();
+		System.out.println(stopWatch.prettyPrint());
+
+
+		stopWatch.start();
+		for (int i = 0; i < 10_000_000; i++) {
+			final List<Object> sortSet = CollectionUtil.sort(set, new ArrayIndexedComparator<>(arr));
+		}
+		stopWatch.stop();
+		System.out.println(stopWatch.prettyPrint());
 	}
 
 	@Data
