8ae3a8717451d72cc92bad6eae36c37f30fa81c9
Looly <loolly@aliyun.com>
2024-07-31 00:05:58 +0800

    add test

--- hutool-core/src/test/java/cn/hutool/core/util/Issue3660Test.java
+++ hutool-core/src/test/java/cn/hutool/core/util/Issue3660Test.java
@@ -1,10 +1,17 @@
 package cn.hutool.core.util;
 
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.List;
 
 public class Issue3660Test {
 	@Test
 	public void splitTest() {
-		System.out.println(StrUtil.split("", ','));
+		List<String> split = StrUtil.split("", ',');
+		Assert.assertEquals(1, split.size());
+
+		split = StrUtil.splitTrim("", ',');
+		Assert.assertEquals(0, split.size());
 	}
 }
