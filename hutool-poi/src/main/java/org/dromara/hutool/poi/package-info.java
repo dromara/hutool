a90c6b5f0a883bc98bf143cfacf70b52925ab73e
Looly <loolly@aliyun.com>
2024-08-10 20:41:33 +0800

    add comment

--- hutool-poi/src/main/java/org/dromara/hutool/poi/package-info.java
+++ hutool-poi/src/main/java/org/dromara/hutool/poi/package-info.java
@@ -15,6 +15,17 @@
  * Java针对MS Office的操作的库屈指可数，比较有名的就是Apache的POI库。<br>
  * 这个库异常强大，但是使用起来也并不容易。Hutool针对POI封装一些常用工具，使Java操作Excel等文件变得异常简单。
  *
+ * <ul>
+ *     <li>HSSF － 提供读写Microsoft Excel XLS 格式文件的功能</li>
+ *     <li>XSSF － 提供读写Microsoft Excel OOXML XLSX 格式文件的功能</li>
+ *     <li>HWPF － 提供读写Microsoft Word DOC 格式文件的功能</li>
+ *     <li>XWPF － 提供读写Microsoft Word DOCX 格式文件的功能</li>
+ *     <li>HSLF － 提供读写Microsoft PowerPoint格式文件的功能</li>
+ *     <li>HDGF － 提供读Microsoft Visio格式文件的功能</li>
+ *     <li>HPBF － 提供读Microsoft Publisher格式文件的功能</li>
+ *     <li>HSMF － 提供读Microsoft Outlook格式文件的功能</li>
+ * </ul>
+ *
  * @author looly
  *
  */
