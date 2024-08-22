9163963d49f5cc55fbaab06119ae07d172057266
Looly <loolly@aliyun.com>
2024-08-22 20:40:54 +0800

    update copyright

--- hutool-swing/src/main/java/org/dromara/hutool/swing/img/color/AnsiLabMapping.java
+++ hutool-swing/src/main/java/org/dromara/hutool/swing/img/color/AnsiLabMapping.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013-2024 Hutool Team.
+ * Copyright (c) 2013-2024 Hutool Team and hutool.cn
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
