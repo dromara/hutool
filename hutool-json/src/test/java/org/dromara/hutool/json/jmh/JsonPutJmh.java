9a753e3fd6dbe9ea6028cee3e0e41221336a1a57
Looly <loolly@aliyun.com>
2024-09-30 19:24:29 +0800

    JsonPutJmh

--- hutool-json/src/test/java/org/dromara/hutool/json/jmh/JsonPutJmh.java
+++ hutool-json/src/test/java/org/dromara/hutool/json/jmh/JsonPutJmh.java
@@ -1,13 +1,12 @@
 package org.dromara.hutool.json.jmh;
 
-import com.alibaba.fastjson2.JSON;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonParser;
+import com.google.gson.JsonObject;
+import org.dromara.hutool.core.util.RandomUtil;
 import org.dromara.hutool.json.JSONObject;
-import org.dromara.hutool.json.JSONUtil;
-import org.junit.jupiter.api.Assertions;
 import org.openjdk.jmh.annotations.*;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -20,36 +19,38 @@ import java.util.concurrent.TimeUnit;
 @Fork(1) //
 @OutputTimeUnit(TimeUnit.NANOSECONDS) // 单位：纳秒
 @State(Scope.Benchmark) // 共享域
-public class JsonToStringJmh {
+public class JsonPutJmh {
 
+	Map<String, String> testData;
 	private JSONObject hutoolJSON;
-	private JsonElement gson;
+	private JsonObject gson;
 	private com.alibaba.fastjson2.JSONObject fastJSON;
 
 
 	@Setup
 	public void setup() {
-		final String jsonStr = "{\"name\":\"张三\",\"age\":18,\"birthday\":\"2020-01-01\"}";
-		hutoolJSON = JSONUtil.parseObj(jsonStr);
-		gson = JsonParser.parseString(jsonStr);
-		fastJSON = JSON.parseObject(jsonStr);
+		testData = new HashMap<>(100, 1);
+		for (int i = 0; i < 100; i++) {
+			testData.put(RandomUtil.randomString(10), RandomUtil.randomString(20));
+		}
+
+		hutoolJSON = new JSONObject();
+		gson = new JsonObject();
+		fastJSON = new com.alibaba.fastjson2.JSONObject();
 	}
 
 	@Benchmark
 	public void gsonJmh() {
-		final String jsonStr = gson.toString();
-		Assertions.assertNotNull(jsonStr);
+		testData.forEach(gson::addProperty);
 	}
 
 	@Benchmark
 	public void hutoolJmh() {
-		final String jsonStr = hutoolJSON.toString();
-		Assertions.assertNotNull(jsonStr);
+		testData.forEach(hutoolJSON::putObj);
 	}
 
 	@Benchmark
 	public void fastJSONJmh() {
-		final String jsonStr = fastJSON.toString();
-		Assertions.assertNotNull(jsonStr);
+		testData.forEach(fastJSON::put);
 	}
 }
