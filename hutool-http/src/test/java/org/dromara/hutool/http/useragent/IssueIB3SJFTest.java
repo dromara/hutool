d88eac0dace1b0046ba569b2430e8482bf64f1f1
Looly <loolly@aliyun.com>
2024-11-12 18:54:26 +0800

    增加Windows微信浏览器识别（issue#IB3SJF@Gitee）

---
+++ hutool-http/src/test/java/org/dromara/hutool/http/useragent/IssueIB3SJFTest.java
@@ -0,0 +1,16 @@
+package org.dromara.hutool.http.useragent;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class IssueIB3SJFTest {
+	@Test
+	void isMobileTest() {
+		final String str="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36 NetType/WIFI " +
+			"MicroMessenger/7.0.20.1781(0x6700143B) WindowsWechat(0x63090c11) XWEB/11275 Flue";
+		final UserAgent ua = UserAgentUtil.parse(str);
+
+		Assertions.assertFalse(ua.isMobile());
+		Assertions.assertEquals("7.0.20.1781", ua.getBrowser().getVersion(str));
+	}
+}
