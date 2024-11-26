5b129a1a8fd5a9d8ca8a78ebbe889a5738db87e0
Looly <loolly@aliyun.com>
2024-11-26 20:13:35 +0800

    add test

--- hutool-core/src/test/java/org/dromara/hutool/core/date/Issue3798Test.java
+++ hutool-core/src/test/java/org/dromara/hutool/core/date/Issue3798Test.java
@@ -3,6 +3,8 @@ package org.dromara.hutool.core.date;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.TimeZone;
+
 public class Issue3798Test {
 	@Test
 	void parseTest() {
@@ -13,6 +15,10 @@ public class Issue3798Test {
 		// 伦敦时间（Greenwich Mean Time, GMT）和北京时间（China Standard Time, CST）之间的时差是8小时。北京时间比伦敦时间快8小时
 		final String iso_datetime2 = "2000-01-01T12:00:00+00:00";
 		final DateTime parse2 = DateUtil.parse(iso_datetime2);
+
+		// 默认的，输出的是当地时间，即伦敦时间在北京是几点
 		Assertions.assertEquals("2000-01-01 20:00:00", parse2.toString());
+		// 如果想输出伦敦时间，则，需要指定时区
+		Assertions.assertEquals("2000-01-01 12:00:00", parse2.toString(TimeZone.getTimeZone("GMT+00:00")));
 	}
 }
